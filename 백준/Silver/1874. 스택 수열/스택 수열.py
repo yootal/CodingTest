c441c68948caa75482a5c23fcb14ea77938e6bd8
yootal <92325898+yootal@users.noreply.github.com>
2023-07-03 19:05:39 +0900

    [Silver II] Title: 스택 수열, Time: 220 ms, Memory: 36696 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,35 @@
+import sys
+input=sys.stdin.readline
+
+input_list = []
+n = int(input())
+index = 0
+for _ in range(n):
+      input_list.append(int(input()))
+stack_list=[]
+result_list=[]
+for i1 in input_list:
+      if index == 0:
+            index = i1
+            for i in range(1,index+1):
+                  stack_list.append(i)
+                  result_list.append("+")
+            stack_list.pop()
+            result_list.append("-")
+ 
+      elif i1 < index:
+            pop = stack_list.pop()
+            result_list.append("-")
+            if i1 != pop:
+                  print("NO")
+                  break
+      elif i1 > index:
+            for i in range(index+1,i1+1):
+                  stack_list.append(i)
+                  result_list.append("+") 
+            index = i1
+            stack_list.pop()
+            result_list.append("-")  
+if len(stack_list) == 0:
+      for r in result_list:
+            print(r)
