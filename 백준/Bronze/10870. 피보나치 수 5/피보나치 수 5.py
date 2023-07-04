9af99da2753ead96ded7ecb3ed27e00d459c77d3
yootal <92325898+yootal@users.noreply.github.com>
2023-07-04 17:52:21 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -0,0 +1,13 @@
+import sys
+
+input=sys.stdin.readline
+
+n = int(input())
+
+def fib(n):
+      if n==1 or n==0:
+            return n
+      else: 
+            return fib(n-1)+fib(n-2)
+      
+print(fib(n))
