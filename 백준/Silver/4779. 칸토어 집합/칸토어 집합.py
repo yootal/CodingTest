3898834752033fa4a11cbb2213e6fc04ecb10117
yootal <92325898+yootal@users.noreply.github.com>
2023-07-04 19:44:00 +0900

    [Silver III] Title: 칸토어 집합, Time: 52 ms, Memory: 32292 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,20 @@
+import sys
+input=sys.stdin.readline
+
+def change(n):
+      if n == 1:
+            return "-"
+      else:
+            r = n // 3
+            return change(r) + " "*r + change(r)            
+            
+while True:
+      try:
+            inp = input().rstrip()
+            if inp =="":
+                  break
+            else:
+                  p = pow(3,int(inp))
+            print(change(p))
+      except : 
+            break 
