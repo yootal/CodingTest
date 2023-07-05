15b82c6450fb3010397ab832d7417c855704a9f5
yootal <92325898+yootal@users.noreply.github.com>
2023-07-05 12:38:33 +0900

    [Silver III] Title: N과 M (1), Time: 204 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,16 @@
+import sys
+input=sys.stdin.readline
+
+def nm(ans):
+      if len(ans)==n2:
+            return print(*ans)
+      for i in range(1,n1+1):
+            if i not in ans:
+                  ans.append(i)
+                  nm(ans)
+                  ans.pop()
+
+
+n1,n2 = map(int,input().split(" "))
+
+nm([])
