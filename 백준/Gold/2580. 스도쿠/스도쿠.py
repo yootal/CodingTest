351ff079512e3e396674e14636c42f33786fadae
yootal <92325898+yootal@users.noreply.github.com>
2023-07-10 17:13:03 +0900

    [Gold IV] Title: 스도쿠, Time: 4424 ms, Memory: 214344 KB -BaekjoonHub

--- 백준/Gold/2580. 스도쿠/스도쿠.py
+++ 백준/Gold/2580. 스도쿠/스도쿠.py
@@ -1,44 +1,46 @@
-sudoku = [list(map(int, input().split())) for _ in range(9)]
-zeros = [(i, j) for i in range(9) for j in range(9) if sudoku[i][j] == 0]
+board = [list(map(int, input().split())) for _ in range(9)]
+empty_space = [(i, j) for i in range(9) for j in range(9) if board[i][j] == 0]
+
 def is_promising(i, j):
     promising = [1,2,3,4,5,6,7,8,9]  
     
     for k in range(9):
-        if sudoku[i][k] in promising:#가로 방향
-            promising.remove(sudoku[i][k])
-        if sudoku[k][j] in promising:#세로방향
-            promising.remove(sudoku[k][j])
+        if board[i][k] in promising: # 가로
+            promising.remove(board[i][k])
+        if board[k][j] in promising: # 세로
+            promising.remove(board[k][j])
             
-    #3*3 박스 검사
     i //= 3
     j //= 3
+    
     for p in range(i*3, (i+1)*3):
         for q in range(j*3, (j+1)*3):
-            if sudoku[p][q] in promising:
-                promising.remove(sudoku[p][q])
+            if board[p][q] in promising:
+                promising.remove(board[p][q])
     
     return promising
 
-flag = False #답이 출력되었는가?
+check = False 
 
-def dfs(x):
-    global flag
+def sudoku(x):
+    global check
     
-    if flag: #이미 답이 출력된 경우
+    if check: # 이미 답이 출력된 경우
         return
         
-    if x == len(zeros): #마지막 0까지 다 채웠을 경우
-        for row in sudoku:
+    if x == len(empty_space): # 마지막 0까지 다 채웠을 경우
+        for row in board:
             print(*row)
-        flag = True #답 출력
+        check = True # 답 출력
         return
         
     else:    
-        (i, j) = zeros[x]
-        promising = is_promising(i, j) #유망한 숫자들을 받음
+        (i, j) = empty_space[x]
+        promising = is_promising(i, j) # 유망한 숫자들을 받음
         
         for num in promising:
-            sudoku[i][j] = num #유망한 숫자 중 하나를 넣어줌
-            dfs(x + 1) #다음 0으로 넘어감
-            sudoku[i][j] = 0 #초기화 (정답이 없을 경우를 대비)
-dfs(0)
+            board[i][j] = num #유망한 숫자 중 하나를 넣어줌
+            sudoku(x + 1) # 다음 0으로 넘어감
+            board[i][j] = 0 # 초기화 (정답이 없을 경우를 대비)
+            
+sudoku(0)
