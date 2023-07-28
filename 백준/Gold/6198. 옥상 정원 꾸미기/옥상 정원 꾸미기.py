363f0930adc5bb792f368130f5aa5277b5f0064c
yootal <92325898+yootal@users.noreply.github.com>
2023-07-28 16:47:20 +0900

    [Gold V] Title: 옥상 정원 꾸미기, Time: 120 ms, Memory: 35932 KB -BaekjoonHub

---
+++ 백준/Gold/6198. 옥상 정원 꾸미기/옥상 정원 꾸미기.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+towers = []
+stack = []
+n = int(input())
+for _ in range(n):
+    towers.append(int(input()))
+    
+ans = 0
+
+for t in towers:
+    while stack and stack[-1] <= t:
+        stack.pop()
+    stack.append(t)
+
+    ans += len(stack)-1
+    
+print(ans)
