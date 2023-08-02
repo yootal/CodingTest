a4d41a5f286a27624e2e577aea1f64f711a03644
yootal <92325898+yootal@users.noreply.github.com>
2023-08-02 15:28:34 +0900

    [Silver I] Title: Z, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1074. Z/Z.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n,r,c = map(int,input().split())
+
+def z(n,r,c):
+    if n == 0:
+        return 0
+    half = 2**(n-1)
+    if r < half and c < half:
+        return z(n-1,r,c)
+    elif r < half and c >= half:
+        return half*half + z(n-1,r,c-half)
+    elif r >= half and c < half:
+        return 2*half*half + z(n-1,r-half,c)
+    else: 
+        return 3*half*half + z(n-1,r-half,c-half)
+    
+print(z(n,r,c))
+
+    
