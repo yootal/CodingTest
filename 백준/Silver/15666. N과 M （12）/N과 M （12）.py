a5747be158e8165428a7173cbdb601ed094eb47d
yootal <92325898+yootal@users.noreply.github.com>
2023-08-03 15:53:09 +0900

    [Silver II] Title: N과 M (12), Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -0,0 +1,23 @@
+import sys
+input=sys.stdin.readline
+
+def dfs():
+    if len(temp) == m:
+        print(*temp)
+        return
+    remember = 0
+    for i in range(n):
+        if remember != num_list[i]:
+            if len(temp) > 0 and temp[-1] <= num_list[i] or not temp:
+                visited[i] = True
+                temp.append(num_list[i])
+                remember = num_list[i]
+                dfs()
+                visited[i] = False
+                temp.pop()
+
+n,m = map(int,input().split())
+num_list = sorted(list(map(int,input().split())))
+visited = [False] * n
+temp = []
+dfs()
