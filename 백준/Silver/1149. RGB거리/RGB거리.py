dd539f3e9e3c214d2e90615e250ae2d3b7e02ead
yootal <92325898+yootal@users.noreply.github.com>
2023-08-16 17:41:30 +0900

    [Silver I] Title: RGB거리, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.py
+++ 백준/Silver/1149. RGB거리/RGB거리.py
@@ -2,30 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-
-rgb_list = [[0 for _ in range(3)] for _ in range(1000)]
-r_list = []
-g_list = []
-b_list = []
+dp = [[0] * 3 for _ in range(n+1)]
+rgb = [[]]
 
 for _ in range(n):
-    r,g,b = map(int,input().split())
-    r_list.append(r)
-    g_list.append(g)
-    b_list.append(b)
+    rgb.append(list(map(int,input().split())))
     
-    
-rgb_list[0][0] = r_list[0]
-rgb_list[0][1] = g_list[0]
-rgb_list[0][2] = b_list[0]
-    
-for i in range(1,n):
-    rgb_list[i][0] = min(rgb_list[i-1][1],rgb_list[i-1][2]) + r_list[i]
-    rgb_list[i][1] = min(rgb_list[i-1][0],rgb_list[i-1][2]) + g_list[i]
-    rgb_list[i][2] = min(rgb_list[i-1][0],rgb_list[i-1][1]) + b_list[i]
+dp[1][0] = rgb[1][0]
+dp[1][1] = rgb[1][1]
+dp[1][2] = rgb[1][2]
 
-print(min(rgb_list[n-1][0],rgb_list[n-1][1],rgb_list[n-1][2]))
-    
 
-            
-        
+for i in range(2,n+1):
+    dp[i][0] = min(dp[i-1][1],dp[i-1][2]) + rgb[i][0]
+    dp[i][1] = min(dp[i-1][0],dp[i-1][2]) + rgb[i][1]
+    dp[i][2] = min(dp[i-1][0],dp[i-1][1]) + rgb[i][2]
+
+print(min(dp[n]))
