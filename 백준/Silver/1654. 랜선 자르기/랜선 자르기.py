c83bebc582cb2612dacc540d6a181ba5a40db389
yootal <92325898+yootal@users.noreply.github.com>
2023-08-23 16:12:20 +0900

    [Silver II] Title: 랜선 자르기, Time: 84 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -3,22 +3,19 @@ input = sys.stdin.readline
 
 k,n = map(int,input().split())
 
-num = []
-
-for _ in range(k):
-    num.append(int(input()))
+num = [int(input()) for _ in range(k)]
     
 max_num = max(num)
 left,right = 1,max_num
 
-while left <= right:
+while left < right:
     count = 0
-    mid = (left+right)//2
+    mid = (left+right+1)//2
     for num1 in num:
         count += (num1//mid)
     if count >= n:
-        left = mid + 1
-    else: right = mid -1 
+        left = mid
+    else: 
+        right = mid -1 
 
-print(right)
-    
+print(left)
