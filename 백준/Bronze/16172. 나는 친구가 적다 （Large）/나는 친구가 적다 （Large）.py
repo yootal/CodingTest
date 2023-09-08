834f812ec66d94bcd71b9ece972f8d9cce405b48
yootal <92325898+yootal@users.noreply.github.com>
2023-09-08 15:07:53 +0900

    [Bronze II] Title: 나는 친구가 적다 (Large), Time: 196 ms, Memory: 35184 KB -BaekjoonHub

---
+++ 백준/Bronze/16172. 나는 친구가 적다 （Large）/나는 친구가 적다 （Large）.py
@@ -0,0 +1,35 @@
+import sys
+input = sys.stdin.readline
+
+def exclude(s):
+    ns = ""
+    for i in range(len(s)):
+        if not s[i].isdigit():
+            ns += s[i]
+    return ns
+
+def failure(s):
+    f = [0] * len(s)
+    j = 0
+    for i in range(1,len(s)):
+        while j > 0 and s[i] != s[j]:
+            j = f[j-1]
+        if s[i] == s[j]:
+            j += 1 
+            f[i] = j
+    return f
+
+a = input().rstrip()
+a = exclude(a)
+b = input().rstrip()
+f = failure(b)
+j = 0
+for i in range(len(a)):
+    while j > 0 and a[i] != b[j]:
+        j = f[j-1]
+    if a[i] == b[j]:
+        j += 1
+    if j == len(b):
+        print(1)
+        exit()
+print(0)
