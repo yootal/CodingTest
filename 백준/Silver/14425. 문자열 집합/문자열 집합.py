182db661679873653d45593dfae8e8eff1f4796f
yootal <92325898+yootal@users.noreply.github.com>
2023-09-09 23:01:23 +0900

    [Silver III] Title: 문자열 집합, Time: 4588 ms, Memory: 1610036 KB -BaekjoonHub

---
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.py
@@ -0,0 +1,42 @@
+import sys
+input = sys.stdin.readline
+
+root = 1
+unused = 2
+mx = 10000 * 500 # 최대 등장 가능
+check = [False] * mx
+nxt = [[-1] * 26 for _ in range(mx)]
+
+def cc(c):
+    return ord(c)- ord('a')
+
+def insert(s):
+    global unused
+    cur = root
+    for c in s:
+        if nxt[cur][cc(c)] == -1:
+            unused += 1
+            nxt[cur][cc(c)] = unused
+        cur = nxt[cur][cc(c)]
+    check[cur] = True
+    
+def find(s):
+    cur = root
+    for c in s:
+        if nxt[cur][cc(c)] == -1:
+            return False
+        cur = nxt[cur][cc(c)]
+    return check[cur]
+
+n,m = map(int,input().split())
+for _ in range(n):
+    inp = input().rstrip()
+    insert(inp)
+
+ans = 0
+for _ in range(m):
+    inp = input().rstrip()
+    if find(inp):
+        ans += 1
+
+print(ans)
