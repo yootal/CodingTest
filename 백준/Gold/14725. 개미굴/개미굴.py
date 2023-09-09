c9dbe816ea08886ddb3eb7b1c797f8baba60c069
yootal <92325898+yootal@users.noreply.github.com>
2023-09-10 01:01:31 +0900

    [Gold III] Title: 개미굴, Time: 68 ms, Memory: 34184 KB -BaekjoonHub

---
+++ 백준/Gold/14725. 개미굴/개미굴.py
@@ -0,0 +1,42 @@
+import sys
+from collections import defaultdict
+input = sys.stdin.readline
+
+def insert(route):
+    global unused
+    v = root
+    for r in route:
+        if not nxt[v][r]:
+            nv = nxt[v][r] = unused
+            unused += 1
+        else:
+            nv = nxt[v][r]
+        depth[nv] = depth[v] + 1
+        name[nv] = r
+        v = nv
+        
+def dfs(v):
+    if v != root:
+        for _ in range(depth[v]):
+            print("--",end = "")
+        print(name[v])
+    sorted_nxt = sorted(nxt[v].items(), key=lambda x: x[0])
+    for sub in sorted_nxt:
+        dfs(sub[1])
+    
+root = 1
+unused = 2
+mx = 1000 * 15 # 최대 등장 가능
+check = [False] * mx
+nxt = [defaultdict(int) for _ in range(mx)]   
+depth = defaultdict(int)
+name = defaultdict(str)
+
+n = int(input())
+depth[root] = -1
+for _ in range(n):
+    inp = input().rstrip().split()
+    route = inp[1:]
+    insert(route)
+    
+dfs(root)
