25b1f5260e1e72c0ce6b4bd624b4b311e5a8e74e
yootal <92325898+yootal@users.noreply.github.com>
2023-09-10 21:08:45 +0900

    [Gold V] Title: 한글 LCS, Time: 1320 ms, Memory: 39364 KB -BaekjoonHub

---
+++ 백준/Gold/15482. 한글 LCS/한글 LCS.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+a = input().strip()
+b = input().strip()
+len_a = len(a)
+len_b = len(b)
+
+dp = [[""] * (len_b+1) for _ in range(len_a+1)]
+
+for i in range(1, len_a+1):
+    for j in range(1, len_b+1):
+        if a[i-1] == b[j-1]:
+            dp[i][j] = dp[i-1][j-1] + a[i-1]
+        else:
+            dp[i][j] = max(dp[i][j-1], dp[i-1][j])
+            if len(dp[i-1][j]) >= len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i][j-1]
+                
+print(len(dp[-1][-1]))
