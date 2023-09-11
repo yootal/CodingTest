830926071f0378abaf1c033eac973e75a8531ea8
yootal <92325898+yootal@users.noreply.github.com>
2023-09-11 13:55:27 +0900

    [Silver II] Title: 스택 수열, Time: 204 ms, Memory: 36696 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.py
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -1,19 +1,22 @@
 import sys
 input=sys.stdin.readline
 
-input_list = []
 n = int(input())
-index = 0
-for _ in range(n):
-      input_list.append(int(input()))
+
+input_list = []
 stack_list=[]
 result_list=[]
+
+for _ in range(n):
+      input_list.append(int(input()))
+
+index = 0      
 for i1 in input_list:
       if index == 0:
-            index = i1
-            for i in range(1,index+1):
+            for i in range(1,i1+1):
                   stack_list.append(i)
                   result_list.append("+")
+            index = i1
             stack_list.pop()
             result_list.append("-")
  
@@ -23,6 +26,7 @@ for i1 in input_list:
             if i1 != pop:
                   print("NO")
                   break
+              
       elif i1 > index:
             for i in range(index+1,i1+1):
                   stack_list.append(i)
@@ -30,6 +34,7 @@ for i1 in input_list:
             index = i1
             stack_list.pop()
             result_list.append("-")  
+            
 if len(stack_list) == 0:
       for r in result_list:
             print(r)
