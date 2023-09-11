600719852afb65380effcaf59030e4eea399a37c
yootal <92325898+yootal@users.noreply.github.com>
2023-09-11 10:44:21 +0900

    [Silver I] Title: IOIOI, Time: 132 ms, Memory: 115136 KB -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -1,6 +1,17 @@
 import sys
 input = sys.stdin.readline
 
+def failure(s):
+    f = [0] * add_v
+    j = 0
+    for i in range(1,add_v):
+        while j > 0 and s[i] != s[j]:
+            j = f[j-1]
+        if s[i] == s[j]:
+            j += 1 
+            f[i] = j
+    return f
+
 n = int(input())
 m = int(input())
 s = input().rstrip()
@@ -13,9 +24,16 @@ for i in range(add_v):
     else:
         p += "I"
 
+f = failure(p)
 ans = 0
-for i in range(m-add_v+1):
-    if s[i] == 'I':
-        if s[i:i+(add_v)] == p:
-            ans += 1
+j = 0
+for i in range(m):
+    while j > 0 and s[i] != p[j]:
+        j = f[j-1]
+    if s[i] == p[j]:
+        j += 1
+    if j == add_v:
+        ans += 1
+        j = f[j-1]
+        
 print(ans)
