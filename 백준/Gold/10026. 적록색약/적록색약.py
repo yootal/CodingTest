cd292c645c858feff56027982fd2c7075fe1d343
yootal <92325898+yootal@users.noreply.github.com>
2023-09-12 00:05:18 +0900

    [Gold V] Title: 적록색약, Time: 68 ms, Memory: 31604 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.py
+++ 백준/Gold/10026. 적록색약/적록색약.py
@@ -1,60 +1,46 @@
-from collections import deque
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-
-n = int(input())
-
-graph = []
-for _ in range(n):
-    graph.append(list(input().rstrip()))
-
-visited = [[0]*n for _ in range(n)] 
-visited2 = [[0]*n for _ in range(n)] 
 
 dy = [1,-1,0,0]
 dx = [0,0,1,-1]
 
-count1 = 0
-count2 = 0
+n = int(input())
+board = [input().rstrip() for _ in range(n)]
+vis1 = [[False] * n for _ in range(n)]
+vis2 = [[False] * n for _ in range(n)]
 
-def bfs(row,col,color):
-    q = deque([(row,col)])
-    while q:
-        r1,c1 = q.popleft()
-        for d in range(4):
-            ny = r1 + dy[d]
-            nx = c1 + dx[d]
-            if nx < 0 or ny < 0 or nx > n - 1 or ny > n - 1:
-                continue
-            if graph[ny][nx] == color and visited[ny][nx] == 0:
-                visited[ny][nx] = 1
-                q.append((ny,nx))
-
-def bfs2(row,col,color):
-    q = deque([(row,col)])
-    while q:
-        r1,c1 = q.popleft()
-        for d in range(4):
-            ny = r1 + dy[d]
-            nx = c1 + dx[d]
-            if nx < 0 or ny < 0 or nx > n - 1 or ny > n - 1:
-                continue
-            if color == 'R' or color == 'G':
-                if graph[ny][nx] == 'R' and visited2[ny][nx] == 0 or graph[ny][nx] == 'G' and visited2[ny][nx] == 0: 
-                    visited2[ny][nx] = 1
-                    q.append((ny,nx))
-            else:
-                if graph[ny][nx] == color and visited2[ny][nx] == 0:
-                    visited2[ny][nx] = 1
-                    q.append((ny,nx))
+def dfs(x,y,c):
+    vis1[x][y] = True
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx <= n-1 and 0 <= ny <= n-1:
+            if board[nx][ny] == c and not vis1[nx][ny]:
+                dfs(nx,ny,c)
                 
-for r in range(n):
-    for c in range(n):
-        if visited[r][c] == 0:
-            bfs(r,c,graph[r][c])
-            count1 += 1
-        if visited2[r][c] == 0:
-            bfs2(r,c,graph[r][c])
-            count2 += 1
-        
-print(count1,count2)
+def dfs_cw(x,y,c):
+    vis2[x][y] = True
+    for i in range(4):
+        nx = x + dx[i]
+        ny = y + dy[i]
+        if 0 <= nx <= n-1 and 0 <= ny <= n-1:
+            if c == 'R' or c == 'G':
+                if (board[nx][ny] == 'R' or board[nx][ny] == 'G') and not vis2[nx][ny]:
+                    dfs_cw(nx,ny,c)
+            else:
+                if board[nx][ny] == c and not vis2[nx][ny]:
+                    dfs_cw(nx,ny,c)
+                
+cnt = 0                
+cnt_cw = 0
+for i in range(n):
+    for j in range(n):
+        if not vis1[i][j]:
+            dfs(i,j,board[i][j])
+            cnt += 1
+        if not vis2[i][j]:
+            dfs_cw(i,j,board[i][j])
+            cnt_cw += 1
+            
+print(cnt,cnt_cw)
