e8ce15ccbfbeb9876d8a13ebcac070c735bf6f9f
yootal <92325898+yootal@users.noreply.github.com>
2023-09-11 15:48:08 +0900

    [Silver I] Title: 미로 탐색, Time: 72 ms, Memory: 34160 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,6 +1,5 @@
-from collections import deque
 import sys
-sys.setrecursionlimit(10**9)
+from collections import deque
 input=sys.stdin.readline
 
 dy = [1,0,-1,0]
@@ -9,8 +8,6 @@ dx = [0,1,0,-1]
 n,m = map(int,input().split())
 board = [input().rstrip() for _ in range(n)]
 visited = [[0]*m for _ in range(n)]
-count = 0
-counts = []
 
 def bfs(i,j):
     q = deque([(i,j)])
@@ -22,7 +19,7 @@ def bfs(i,j):
             nx = col + dx[k]
             if ny < 0 or nx < 0 or ny > n-1 or nx > m-1:
                 continue
-            elif board[ny][nx] == '1' and visited[ny][nx] == 0:
+            if board[ny][nx] == '1' and visited[ny][nx] == 0:
                 visited[ny][nx] = visited[row][col] + 1
                 q.append((ny,nx))
     return visited[n-1][m-1]
