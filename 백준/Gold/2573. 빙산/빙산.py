497deb0d4e5bcdc6031e61d737283db382527f44
yootal <92325898+yootal@users.noreply.github.com>
2023-09-12 15:28:17 +0900

    [Gold IV] Title: 빙산, Time: 3912 ms, Memory: 34216 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.py
+++ 백준/Gold/2573. 빙산/빙산.py
@@ -1,69 +1,62 @@
 import sys
-sys.setrecursionlimit(10**4)
+from collections import deque
 input = sys.stdin.readline
 
-dy,dx = [1,-1,0,0],[0,0,1,-1]
+d = [(-1,0),(1,0),(0,-1),(0,1)]
 
-def dfs(r,c):
-    visited[r][c] = 1
-    for j in range(4):
-        ny2 = r + dy[j]
-        nx2 = c + dx[j]
-        if ny2 < 0 or ny2 > row-1 or nx2 < 0 or nx2 > col-1:
-            continue
-        if visited[ny2][nx2] == 0 and iceberg[ny2][nx2] != 0:
-            dfs(ny2,nx2)
-        
-row, col = map(int,input().split())
+n,m = map(int,input().split())
+iceberg = [list(map(int,input().split())) for _ in range(n)]
 
-iceberg = []
+def bfs(x,y):
+    vis[x][y] = True
+    q = deque()
+    q.append((x,y))
+    while q:
+        px,py = q.popleft()
+        for dx,dy in d:
+            nx = px + dx
+            ny = py + dy
+            if 0 <= nx < n and 0 <= ny < m:
+                if iceberg[nx][ny] != 0 and not vis[nx][ny]:
+                    vis[nx][ny] = True
+                    q.append((nx,ny))
 
-for _ in range(row):
-    iceberg.append(list(map(int,input().split())))
+def melt(iceberg):
+    new_iceberg = [[0] * m for _ in range(n)]
+    for i in range(n):
+        for j in range(m):
+            if iceberg[i][j] != 0:
+                cnt = 0 
+                for dx,dy in d:
+                    ni = i + dx
+                    nj = j + dy
+                    if iceberg[ni][nj] == 0:
+                        cnt += 1
+                if iceberg[i][j] - cnt < 0:
+                    new_iceberg[i][j] = 0
+                else:
+                    new_iceberg[i][j] = iceberg[i][j] - cnt
+    return new_iceberg
 
 year = 0
-
 while True:
-    if year > 0:
-        melt_count = []
-        for y in range(row):
-            for x in range(col):
-                if iceberg[y][x] != 0:
-                    count = 0
-                    for i in range(4):
-                        ny = y + dy[i]
-                        nx = x + dx[i]
-                        if ny < 0 or ny > row-1 or nx < 0 or nx > col-1:
-                            continue
-                        if iceberg[ny][nx] == 0:
-                            count += 1
-                    melt_count.append((y,x,count))
-
-        for m in melt_count:
-            iceberg[m[0]][m[1]] -= m[2]
-            if iceberg[m[0]][m[1]] < 0:
-                iceberg[m[0]][m[1]] = 0
-
-    iceberg_count = 0
-    visited = [[0] * col for _ in range(row)]              
-    for y1 in range(row):
-        for x1 in range(col):
-            if iceberg[y1][x1] != 0 and visited[y1][x1] == 0:
-                dfs(y1,x1)
-                iceberg_count += 1
-    if iceberg_count >= 2:
-        print(year)   
+    vis = [[False] * m for _ in range(n)]
+    lump = 0
+    check = False
+    for i in range(n):
+        for j in range(m):
+            if iceberg[i][j] != 0 and not vis[i][j]:
+                lump += 1
+                check = True
+                bfs(i,j)
+                
+    if not check:
+        print(0)
         break
-    else:
-        if sum(map(sum,iceberg)) == 0:
-            print(0)
-            break
-        year += 1
     
-
+    if lump >= 2:
+        print(year)
+        break
     
-    
-    
-    
-
-        
+    year += 1
+    iceberg = melt(iceberg)
