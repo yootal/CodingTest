5cd9440c202c6bc64e305ca6891c5cef4528f985
yootal <92325898+yootal@users.noreply.github.com>
2023-09-12 10:32:02 +0900

    [Silver I] Title: 영역 구하기, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.py
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.py
@@ -1,48 +1,44 @@
 import sys
+sys.setrecursionlimit(10**6)
 input = sys.stdin.readline
-from collections import deque
 
-dy,dx = [1,-1,0,0],[0,0,1,-1]
+dy = [1,-1,0,0]
+dx = [0,0,1,-1]
 
-row,col,n = map(int,input().split())
-
-rectangle = []
-counts = []
-area_count = 0
-
-visited = [[0] * col for _ in range(row)]
-
-for _ in range(n):
-    rectangle.append(list(map(int,input().split())))
-    
-for r in rectangle:
-    for x in range(r[0],r[2]):
-        for y in range(r[1],r[3]):
-            visited[y][x] = 1
+m,n,k = map(int,input().split())
+vis = [[False] * n for _ in range(m)]
+for _ in range(k):
+    x1,y1,x2,y2 = map(int,input().split())
+    for i in range(y1,y2):
+        for j in range(x1,x2):
+            vis[i][j] = True
             
-def bfs(row_,col_):
-    count = 1
-    visited[row_][col_] = 1
-    q = deque([(row_,col_)])
-    while q:
-        py, px = q.popleft()
+def dfs(i,j):
+    global size
+    vis[i][j] = True
+    size += 1
+    s = []
+    s.append((i,j))
+    while s:
+        x,y = s.pop()
         for i in range(4):
-            ny, nx = py + dy[i], px + dx[i]
-            if ny < 0 or nx < 0 or ny > row - 1 or nx > col - 1:
-                continue
-            if visited[ny][nx] == 0:
-                visited[ny][nx] = 1
-                count += 1
-                q.append((ny,nx))
-    return count    
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < m and 0 <= ny < n:
+                if not vis[nx][ny]:
+                    vis[nx][ny] = True
+                    s.append((nx,ny))  
+                    size += 1
+ans = 0
+size_list = []
+for i in range(m):
+    for j in range(n):
+        if not vis[i][j]:
+            size = 0
+            dfs(i,j)
+            size_list.append(size)
+            ans += 1
             
-for r1 in range(row):
-    for c1 in range(col):
-        if visited[r1][c1] == 0:
-            counts.append(bfs(r1,c1))
-            area_count += 1
-            
-print(area_count)
-
-counts.sort()
-print(" ".join(map(str,counts)))
+print(ans)
+size_list.sort()
+print(*size_list)
