365f6b32adb7f484e222ff5fa6568cef21dd263d
yootal <92325898+yootal@users.noreply.github.com>
2023-09-14 01:24:21 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 4808 ms, Memory: 372068 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.py
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.py
@@ -1,37 +1,40 @@
 import sys
-input=sys.stdin.readline
 from collections import deque
+input=sys.stdin.readline
+inf = sys.maxsize
 
-dy,dx = [-1,1,0,0],[0,0,-1,1]
+d = [(-1,0),(1,0),(0,-1),(0,1)]
 
-n,m,k = map(int,input().split())
-MAX = float('inf')
-board = [list(map(int,input().strip())) for _ in range(n)]
-visited = [[[MAX]*(k+1) for _ in range(m)] for _ in range(n)]
-visited[0][0][k] = 0
-q = deque()
-q.append((0,0,1,k))
-result = MAX
-
-while q:
-    x,y,time,wall = q.popleft()
-    if (x, y) == (m-1, n-1) :
-        result = min(result, time)
-        continue
-    
-    daytime = time % 2
-    for i in range(4):
-        ny = y + dy[i]
-        nx = x + dx[i]
-        if -1 < nx < m and -1 < ny < n:
-            if board[ny][nx] == 0 and visited[ny][nx][wall] > time:
-                visited[ny][nx][wall] = time
+def bfs():
+    visited[k][0][0] = 1
+    q = deque()
+    q.append((0,0,1,k)) # x,y,time,wall
+    while q:
+        x,y,time,wall = q.popleft()
+        
+        if x == n-1 and y == m-1:
+            return visited[wall][x][y]
+        
+        for dx,dy in d:
+            nx = x + dx
+            ny = y + dy
+            
+            if ny < 0 or ny > m - 1 or nx < 0 or nx > n - 1:
+                continue
+            
+            if board[nx][ny] == 0 and visited[wall][nx][ny] > time + 1:
+                visited[wall][nx][ny] = time + 1
                 q.append((nx,ny,time+1,wall))
-            if board[ny][nx] == 1 and wall and visited[ny][nx][wall-1] > time:
-                if daytime:
-                    visited[ny][nx][wall-1] = time
+                
+            elif board[nx][ny] == 1 and wall and visited[wall-1][nx][ny] > time + 1:
+                if time % 2 == 1: # 낮
+                    visited[wall-1][nx][ny] = time + 1
                     q.append((nx,ny,time+1,wall-1))
-                else:
+                else: # 밤
                     q.append((x,y,time+1,wall))
-
-print(result if result < MAX else -1)
+    return -1
+    
+n,m,k = map(int,input().split())
+board = [list(map(int,input().rstrip())) for _ in range(n)]
+visited = [[[inf] * m for _ in range(n)] for _ in range(k+1)]
+print(bfs())
