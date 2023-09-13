c5e20cec9aad4ee87eed93dc163dd7070e8d0f1a
yootal <92325898+yootal@users.noreply.github.com>
2023-09-14 01:38:43 +0900

    [Gold II] Title: 불켜기, Time: 108 ms, Memory: 34640 KB -BaekjoonHub

--- 백준/Gold/11967. 불켜기/불켜기.py
+++ 백준/Gold/11967. 불켜기/불켜기.py
@@ -4,47 +4,40 @@ from collections import deque, defaultdict
 
 dx,dy = [-1,1,0,0],[0,0,-1,1]    
 n,m = map(int,input().split())
-
 turn_info = defaultdict(list)
+
 for _ in range(m):
     x,y,a,b = map(int,input().split())
     turn_info[(x-1, y-1)].append((a-1, b-1))
     
 def bfs():
-    result = 1      # 불 켤 수 있는 방의 갯수
-    visited = [[0] * n for _ in range(n)]   # 방문 표시
-    visited[0][0] = 1
-    lights = [[0] * n for _ in range(n)]    # 불 켠 방 표시
-    lights[0][0] = 1
-    Q = deque([(0, 0)])
-    while Q:
-        r, c = Q.popleft()
-        for tr, tc in turn_info[(r, c)]:   # 불 켤 수 있는 곳(딕셔너리 참조)
+    ans_cnt = 1 # 켠 불
+    visited = [[False] * n for _ in range(n)]
+    lights = [[False] * n for _ in range(n)]
+    visited[0][0] = True
+    lights[0][0] = True
+    q = deque([(0, 0)])
+    while q:
+        r, c = q.popleft()
+        for tr, tc in turn_info[(r, c)]:
             if not lights[tr][tc]:
-                lights[tr][tc] = 1      # 새로 불 켜기
-                result += 1
-                for d in range(4):      # 4방향 연결된 곳
+                lights[tr][tc] = True
+                ans_cnt += 1
+                for d in range(4):
                     nr = tr + dy[d]
                     nc = tc + dx[d]
-                    if not (0 <= nr < n and 0 <= nc < n):
-                        continue
-                    if visited[nr][nc]:     # 방문한 적 있으면(새로 연결되어 또 불을 켤 곳이 있을 수 있으므로)
-                        Q.append((nr, nc))  # 큐에 담기
+                    if 0 <= nr < n and 0 <= nc < n:
+                        if visited[nr][nc]:
+                            q.append((nr, nc))
         
-        # 현 위치를 기준으로
-        for d in range(4):      # 4방향 연결된 곳
+        for d in range(4):
             nr = r + dy[d]
             nc = c + dx[d]
-            if not (0 <= nr < n and 0 <= nc < n):
-                continue
-            # 첫 방문인데 이미 불 켜진 곳이면
-            if not visited[nr][nc] and lights[nr][nc]:
-                Q.append((nr, nc))      # 재검사를 위해 큐에 담기
-                visited[nr][nc] = 1     # 방문 표시
-    
-    return result
+            if 0 <= nr < n and 0 <= nc < n:
+                if not visited[nr][nc] and lights[nr][nc]:
+                    q.append((nr, nc))  
+                    visited[nr][nc] = True
+
+    return ans_cnt
 
 print(bfs())
-                
-     
-
