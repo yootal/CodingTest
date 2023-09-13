68e2e1f0e0c22c5b9b5a5e9593448a5469331776
yootal <92325898+yootal@users.noreply.github.com>
2023-09-13 23:03:54 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 3364 ms, Memory: 331976 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.py
@@ -2,7 +2,7 @@ import sys
 input=sys.stdin.readline
 from collections import deque
 
-dy,dx = [-1,1,0,0],[0,0,-1,1]
+d = [(-1,0),(1,0),(0,-1),(0,1)]
 
 def bfs():
     visited[k][0][0] = 1
@@ -12,15 +12,15 @@ def bfs():
         row,col,wall = q.popleft()
         if row == n-1 and col == m-1:
             return visited[wall][row][col]
-        for i in range(4):
-            ny = row + dy[i]
-            nx = col + dx[i]
+        for dx,dy in d:
+            ny = row + dy
+            nx = col + dx
             if nx < 0 or nx > m - 1 or ny < 0 or ny > n - 1:
                 continue
-            if board[ny][nx] == '1' and wall > 0 and visited[wall-1][ny][nx] == 0:
+            if board[ny][nx] == '1' and wall > 0 and not visited[wall-1][ny][nx]:
                 visited[wall-1][ny][nx] = visited[wall][row][col] + 1
                 q.append((ny,nx,wall-1))
-            elif board[ny][nx] == '0' and visited[wall][ny][nx] == 0:
+            elif board[ny][nx] == '0' and not visited[wall][ny][nx]:
                 visited[wall][ny][nx] = visited[wall][row][col] + 1
                 q.append((ny,nx,wall))
     return -1
@@ -29,11 +29,3 @@ n,m,k = map(int,input().split())
 board = [input().rstrip() for _ in range(n)]
 visited = [[[0] * m for _ in range(n)] for _ in range(k+1)]
 print(bfs())
-
-
-
-
-
-
-
-
