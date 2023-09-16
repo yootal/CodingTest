bbb417c2f14342c611d5abf358236b03c0789358
yootal <92325898+yootal@users.noreply.github.com>
2023-09-16 15:06:32 +0900

    [Platinum V] Title: 백조의 호수, Time: 952 ms, Memory: 227276 KB -BaekjoonHub

--- 백준/Platinum/3197. 백조의 호수/백조의 호수.py
+++ 백준/Platinum/3197. 백조의 호수/백조의 호수.py
@@ -11,67 +11,65 @@ n,m = map(int,input().split())
 board = [input().rstrip() for _ in range(n)]
 lake = [[0] * m for _ in range(n)]
 swan_vis = [[0] * m for _ in range(n)]
-visited = [[0] * m for _ in range(n)]
-
+water_vis = [[0] * m for _ in range(n)]
 water_q = deque()
 ice_q2 = deque()
 swan_q = deque()
 swan_q2 = deque()
-lx,ly = 0,0
-ans = 0
-check = False
 
+ans = 0
+swan_point = []
 for i in range(n):
     for j in range(m):
         if board[i][j] == 'X':
             lake[i][j] = 1
         if board[i][j] == 'L':
-            lx,ly = i,j
+            swan_point.append((i,j))
         if board[i][j] != 'X':
             water_q.append((i,j))
             
-swan_q.append((lx,ly))
-swan_vis[lx][ly] = 1
+sx,sy = swan_point[0]
+ex,ey = swan_point[1]
+swan_q.append((sx,sy))
+swan_vis[sx][sy] = 1
 
-while not check:
-    while water_q:
-        qx,qy = water_q.popleft()
-        visited[qx][qy] = 1
+while True:
+    while swan_q:
+        x,y = swan_q.popleft()
         for dx,dy in d:
-            nx = qx + dx
-            ny = qy + dy
-            if limit_check(nx,ny) or visited[nx][ny]:
+            nx = x + dx
+            ny = y + dy
+            if limit_check(nx,ny) or swan_vis[nx][ny]:
                 continue
             if lake[nx][ny] == 1:
-                ice_q2.append((nx,ny))
-            visited[nx][ny] = 1
-
-    while ice_q2:
-        qx,qy = ice_q2.popleft()
-        lake[qx][qy] = 0
-        water_q.append((qx,qy))
-    ans += 1
-
-    while swan_q:
-        sx,sy = swan_q.popleft()
-        for dx,dy in d:
-            nsx = sx + dx
-            nsy = sy + dy
-            if limit_check(nsx,nsy) or swan_vis[nsx][nsy]:
+                swan_vis[nx][ny] = 1
+                swan_q2.append((nx,ny))
                 continue
-            if lake[nsx][nsy] == 1:
-                swan_vis[nsx][nsy] = 1
-                swan_q2.append((nsx,nsy))
-                continue
-            elif board[nsx][nsy] == 'L':
-                check = True
-                break
-            swan_vis[nsx][nsy] = 1
-            swan_q.append((nsx,nsy))
-
+            elif nx == ex and ny == ey:
+                print(ans)
+                exit()
+            swan_vis[nx][ny] = 1
+            swan_q.append((nx,ny))
+            
     while swan_q2:
         sx2,sy2 = swan_q2.popleft()
         swan_q.append((sx2,sy2))
-    
-print(ans)
-           
+        
+    while water_q:
+        wx,wy = water_q.popleft()
+        water_vis[wx][wy] = 1
+        for dx,dy in d:
+            nx = wx + dx
+            ny = wy + dy
+            if limit_check(nx,ny) or water_vis[nx][ny]:
+                continue
+            if lake[nx][ny] == 1:
+                ice_q2.append((nx,ny))
+            water_vis[nx][ny] = 1
+
+    while ice_q2:
+        wx,wy = ice_q2.popleft()
+        lake[wx][wy] = 0
+        water_q.append((wx,wy))
+        
+    ans += 1
