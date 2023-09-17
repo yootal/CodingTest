940a5fff25d46f3f9c8c765bbcf937d2b44c2768
yootal <92325898+yootal@users.noreply.github.com>
2023-09-17 15:03:09 +0900

    [Silver III] Title: N과 M (2), Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.py
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -1,21 +1,22 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-def nm(ans):
-      if len(ans)==n2:
-            return print(*ans)
-      for i in range(1,n1+1):
-            if i not in ans:
-                  if len(ans) == 0:
-                        ans.append(i)
-                        nm(ans)
-                        ans.pop()
-                  elif ans[-1] < i:
-                        ans.append(i)
-                        nm(ans)
-                        ans.pop()
+n,m = map(int,input().split())
+use_check = [False] * (n+1)
+arr = [0] * m
 
-
-n1,n2 = map(int,input().split(" "))
-
-nm([])
+def bt(k):
+    if k == m:
+        print(*arr)
+        return
+    st = 1
+    if k != 0:
+        st = arr[k-1] + 1
+    for i in range(st,n+1):
+        if not use_check[i]:
+            use_check[i] = True
+            arr[k] = i
+            bt(k+1)
+            use_check[i] = False
+        
+bt(0)
