47aaeb0a00f6f6e384443416755f58656e32f65b
yootal <92325898+yootal@users.noreply.github.com>
2023-09-17 17:24:09 +0900

    [Silver II] Title: 부분수열의 합, Time: 236 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,21 +2,32 @@ import sys
 input=sys.stdin.readline
 
 def func(cur,sum):
-    global count
-    if cur >= n:
-        return
+    # global count
+    # if cur >= n:
+    #     return
     
-    sum += num_list[cur]
+    # sum += num_list[cur]
 
-    if sum == s:
-        count += 1
+    # if sum == s:
+    #     count += 1
+
+    # func(cur+1, sum)
+    # func(cur+1, sum - num_list[cur])
+    
+    global count
+    if cur == n:
+        if sum == s:
+            count += 1
+        return
 
     func(cur+1, sum)
-    func(cur+1, sum - num_list[cur])
+    func(cur+1, sum + num_list[cur])
 
 n,s = map(int,input().split())
 num_list = list(map(int,input().split()))
 
 count = 0
 func(0,0)
+if s == 0:
+    count -= 1
 print(count)
