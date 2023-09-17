67d119b94f11953bdd5d83fd9b5fb6c44a1d57aa
yootal <92325898+yootal@users.noreply.github.com>
2023-09-17 15:19:31 +0900

    [Silver III] Title: N과 M (8), Time: 64 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,19 @@
+import sys
+input=sys.stdin.readline
+
+def func(ans,cnt):
+    if cnt == m:
+        print(*ans)
+        return
+    for i in range(n):
+            if cnt != 0 and ans[-1] <= num_list[i] or cnt == 0:
+                ans.append(num_list[i])
+                func(ans,cnt+1)
+                ans.pop()
+
+n,m = map(int,input().split())
+num_list = list(map(int,input().split()))
+num_list.sort()
+
+count = 0
+func([],0)
