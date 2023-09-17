1da2b901f1f38b0b3972871994ac8362c5336524
yootal <92325898+yootal@users.noreply.github.com>
2023-09-17 15:35:58 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,23 +1,21 @@
 import sys
-input=sys.stdin.readline
-
-def dfs():
-    if len(temp) == m:
-        print(*temp)
-        return
-    remember = 0
-    for i in range(n):
-        if remember != num_list[i]:
-            if len(temp) > 0 and temp[-1] <= num_list[i] or not temp:
-                visited[i] = True
-                temp.append(num_list[i])
-                remember = num_list[i]
-                dfs()
-                visited[i] = False
-                temp.pop()
+input = sys.stdin.readline
 
 n,m = map(int,input().split())
-num_list = sorted(list(map(int,input().split())))
-visited = [False] * n
-temp = []
-dfs()
+num = list(map(int,input().split()))
+num.sort()
+use_check = [False] * (n)
+arr = [0] * m
+
+def bt(k,st):
+    if k == m:
+        print(*arr)
+        return
+    temp = 0
+    for i in range(st,n):
+        if not use_check[i] and temp != num[i]:
+            arr[k] = num[i]
+            temp = arr[k] 
+            bt(k+1,i)
+                  
+bt(0,0)
