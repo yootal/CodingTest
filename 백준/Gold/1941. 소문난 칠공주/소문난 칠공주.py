ebf54e71880ac4379211dde350a09405293d439c
yootal <92325898+yootal@users.noreply.github.com>
2023-09-17 22:37:36 +0900

    [Gold III] Title: 소문난 칠공주, Time: 2432 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -1,45 +1,44 @@
 import sys
 input=sys.stdin.readline
 
-dy,dx = [-1,1,0,0],[0,0,-1,1]
-
-cnt = 0
+d = [(-1,0),(1,0),(0,-1),(0,1)]
 board = []
 for _ in range(5):
     board += list(map(str,input().strip()))
 visited = [0 for _ in range(25)]
 check = [[0] * 5 for _ in range(5)]
 
-def permu(idx,ans,position):
+def permu(idx,route,position):
     global cnt,check
-    if ans.count('Y') > 3:
+    if route.count('Y') > 3:
         return
     if idx == 7:
-        if ans.count('S') >= 4:
+        if route.count('S') >= 4:
             position_check(position[0],position)
-            if sum(sum(check,[])) == 7:
+            if sum([sum(c) for c in check]) == 7:
                 cnt += 1
             check = [[0] * 5 for _ in range(5)]
         return
     for i in range(25):
         if visited[i] == 0:
             visited[i] = 1
-            permu(idx + 1, ans + board[i], position + [i])
+            permu(idx + 1, route + board[i], position + [i])
             for j in range(i+1,25):
                 visited[j] = 0
                 
-def position_check(s,position):
-    r = s // 5
-    c = s % 5
-    check[r][c] = 1
-    for j in range(4):
-        ny = r + dy[j]
-        nx = c + dx[j]
+def position_check(p,position):
+    x = p // 5
+    y = p % 5
+    check[x][y] = 1
+    for dx,dy in d:
+        nx = x + dx
+        ny = y + dy
         if 0 <= ny < 5 and 0 <= nx < 5:
-            next = ny * 5 + nx
-            if check[ny][nx] == 0 and next in position:
-                check[ny][nx] = 1
+            next = nx * 5 + ny
+            if check[nx][ny] == 0 and next in position:
+                check[nx][ny] = 1
                 position_check(next,position)
             
+cnt = 0
 permu(0,'',[])
 print(cnt)
