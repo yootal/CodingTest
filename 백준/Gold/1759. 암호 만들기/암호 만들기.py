327c99c281ae71ec0e87448ae8e578975362b547
yootal <92325898+yootal@users.noreply.github.com>
2023-09-17 16:06:16 +0900

    [Gold V] Title: 암호 만들기, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.py
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.py
@@ -1,28 +1,31 @@
 import sys
-input=sys.stdin.readline
-
-def dfs():
-    global count
-    if len(temp) == l:
-        if count >= 1 and len(temp) - count >= 2:
-            print("".join(temp))
-        return
-    for i in range(c):
-            if len(temp) > 0 and temp[-1] < c_list[i] or not temp:
-                temp.append(c_list[i])
-                if c_list[i] in vow:
-                    count += 1
-                    dfs()
-                    temp.pop()
-                    count -= 1
-                else:
-                    dfs()
-                    temp.pop()
-            
+input = sys.stdin.readline
 
 l,c = map(int,input().split())
-c_list = sorted(list(input().rstrip().split()))
-vow = ['a','e','i','o','u']
-temp = []
-count = 0
-dfs()
+alp = list(input().rstrip().split())
+alp.sort()
+used = [False] * c
+arr = [[] for _ in range(l)]
+cnt = 0
+consonant = {'a','e','i','o','u'}
+
+def bt(k,st):
+    global cnt
+    if k == l:
+        if cnt >= 1 and l - cnt >= 2:
+            print(''.join(arr))
+            return
+        else:
+            return
+    for i in range(st,c):
+        if not used[i]:
+            used[i] = True
+            arr[k] = alp[i]
+            if alp[i] in consonant:
+                cnt += 1
+            bt(k+1,i)
+            used[i] = False
+            if alp[i] in consonant:
+                cnt -= 1
+
+bt(0,0)
