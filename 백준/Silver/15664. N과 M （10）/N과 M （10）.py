e0606ddef5ed6d67edfbc4f380dc028e5c02dfcb
yootal <92325898+yootal@users.noreply.github.com>
2023-09-17 15:29:54 +0900

    [Silver II] Title: N과 M (10), Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.py
@@ -0,0 +1,25 @@
+import sys
+input = sys.stdin.readline
+
+n,m = map(int,input().split())
+num = list(map(int,input().split()))
+num.sort()
+use_check = [False] * (n)
+arr = [0] * m
+
+def bt(k,st):
+    if k == m:
+        print(*arr)
+        return
+    temp = 0
+    if k != 0:
+        st += 1
+    for i in range(st,n):
+        if not use_check[i] and temp != num[i]:
+            use_check[i] = True
+            arr[k] = num[i]
+            temp = arr[k] 
+            bt(k+1,i)
+            use_check[i] = False
+                  
+bt(0,0)
