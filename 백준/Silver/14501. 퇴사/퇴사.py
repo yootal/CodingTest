ea8df332245797f84e4fc205f9e9688dbb693b55
yootal <92325898+yootal@users.noreply.github.com>
2023-09-18 15:39:21 +0900

    [Silver III] Title: 퇴사, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -1,16 +1,18 @@
 import sys 
 input = sys.stdin.readline
 
-N = int(input())
-S=[[0,0]]
-S.extend(list(map(int,input().split())) for _ in range(N))
-dp = [item[1] for item in S]
-# 1 ~ N 
-for i in range(1,N+1):
-    if i + S[i][0] - 1 > N:
-        dp[i] = 0
-    else:
-        for j in range(1,i):
-            if j + S[j][0] <= i:
-                dp[i] = max(dp[i], dp[j] + S[i][1])
+n = int(input())
+schedule = [()]
+for _ in range(n):
+    schedule.append((tuple(map(int,input().split()))))
+
+dp = [0] * (n+1)
+
+for i in range(1,n+1):
+    if i + schedule[i][0] <= n + 1:
+        dp[i] = schedule[i][1]
+    for j in range(1,i):
+        if i >= j + schedule[j][0] and i + schedule[i][0] <= n + 1:
+            dp[i] = max(dp[i], dp[j] + schedule[i][1])
+        
 print(max(dp))
