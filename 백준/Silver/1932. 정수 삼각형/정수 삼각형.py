ee2d00b53f9650218e33aa9ec0a335c6bf52ba3f
yootal <92325898+yootal@users.noreply.github.com>
2023-09-18 14:54:13 +0900

    [Silver I] Title: 정수 삼각형, Time: 136 ms, Memory: 41684 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -1,27 +1,22 @@
 import sys 
 input = sys.stdin.readline
 
-inp = int(input())
+n = int(input())
+num = [[] for _ in range(n+1)]
 
-dp = []
-
-for _ in range(inp):
-    dp.append(list(map(int,input().split())))
-
-for i in range(1,inp):
-    for j in range(len(dp[i])):
-        if j == 0:
-            dp[i][j] = dp[i][j] + dp[i-1][j]
-        elif j==len(dp[i])-1:
-            dp[i][j] = dp[i][j] + dp[i-1][j-1]
-        else:
-            dp[i][j] = max(dp[i-1][j-1],dp[i-1][j]) + dp[i][j]
-
-print(max(dp[inp-1]))
-        
-
-
+for i in range(1,n+1):
+    num[i] = [0] + list(map(int,input().split()))
     
+dp = [[0] * (n+1) for _ in range(n+1)]
+dp[1][1] = num[1][1]
 
+for i in range(2,n+1):
+    for j in range(1,i+1):
+        if j == 1:
+            dp[i][j] = dp[i-1][j] + num[i][j]
+        elif j == i:
+            dp[i][j] = dp[i-1][j-1] + num[i][j]
+        else:
+            dp[i][j] = max(dp[i-1][j-1], dp[i-1][j]) + num[i][j]
             
-        
+print(max(dp[i]))
