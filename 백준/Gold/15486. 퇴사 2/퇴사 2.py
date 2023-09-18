a396b5dfe0bc634fd9e962fcb26a4e1240b43a4f
yootal <92325898+yootal@users.noreply.github.com>
2023-09-18 16:07:42 +0900

    [Gold V] Title: 퇴사 2, Time: 2380 ms, Memory: 252352 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.py
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.py
@@ -1,17 +1,19 @@
 import sys 
 input = sys.stdin.readline
 
-N = int(input())
-S = []
-for i in range(N):
-    T,P = map(int,input().split())
-    S.append([T,P])
-dp = [0] * (N+1)
-max_value = 0
-for i in range(N):
-    max_value = max(max_value,dp[i])
-    if i + S[i][0] > N:
-        continue
-    dp[i+S[i][0]] = max(max_value + S[i][1], dp[i+S[i][0]])
+n = int(input())
+schedule = [()]
+for _ in range(n):
+    schedule.append((tuple(map(int,input().split()))))
 
+dp = [0] * (n+2) # i번째 일에 상담을 시작했을 때 얻을 수 있는 최대 수익
+
+for i in range(n,0,-1):
+    # i번째 일에 상담을 할 수 있을 경우
+    if i + schedule[i][0] <= n + 1:
+        # i번째 일에 상담을 했을 때와 상담을 하지 않았을 때 얻을 수 있는 수익 중 최대 수익을 취함
+        dp[i] = max(dp[i + schedule[i][0]] + schedule[i][1], dp[i+1])
+    else:
+        dp[i] = dp[i+1]
+              
 print(max(dp))
