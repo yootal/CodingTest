40b11a204d20b9c93f9bc1fbcacdcc4fa05d7d5e
yootal <92325898+yootal@users.noreply.github.com>
2023-09-19 16:23:25 +0900

    [Silver I] Title: 카드 구매하기, Time: 88 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
+++ 백준/Silver/11052. 카드 구매하기/카드 구매하기.py
@@ -1,13 +1,11 @@
-import sys 
+import sys
 input = sys.stdin.readline
 
-N = int(input())
+n = int(input())
 card = list(map(int,input().split()))
+dp = [0] + card
 
-dp = [0]
-dp.extend(card)
-
-for i in range(1,N+1):
+for i in range(1,n+1):
     dp[i] = max(dp[j] + dp[i-j] for j in range(1,i+1))
 
-print(dp[N])
+print(dp[n])
