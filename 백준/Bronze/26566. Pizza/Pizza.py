6868fd6534aa2f943f1421e317c665277b8bf300
yootal <92325898+yootal@users.noreply.github.com>
2023-09-19 23:24:02 +0900

    [Bronze IV] Title: Pizza, Time: 48 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/26566. Pizza/Pizza.py
@@ -0,0 +1,12 @@
+import sys 
+from math import pi
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    a,p1 = map(int,input().split())
+    b,p2 = map(int,input().split())
+
+    if p1/a < p2/(b**2*pi):
+        print("Slice of pizza")
+    else:
+        print("Whole pizza")
