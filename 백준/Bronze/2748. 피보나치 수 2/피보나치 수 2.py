064b72dea75cdd574de738bd171f4d375b7c6a77
yootal <92325898+yootal@users.noreply.github.com>
2023-09-19 11:29:53 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,11 +1,13 @@
-import sys 
+import sys
 input = sys.stdin.readline
 
-N = int(input())
-dp = [0] * 91
+n = int(input())
+
+dp = [0] * (91)
+dp[0] = 1
 dp[1] = 1
 
-for i in range(2,N+1):
+for i in range(2,n):
     dp[i] = dp[i-1] + dp[i-2]
     
-print(dp[N])
+print(dp[n-1])
