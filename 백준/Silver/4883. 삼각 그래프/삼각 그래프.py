b4683e28e4deeb0f2692281be50436e980e1d926
yootal <92325898+yootal@users.noreply.github.com>
2023-09-19 20:09:34 +0900

    [Silver I] Title: 삼각 그래프, Time: 364 ms, Memory: 72756 KB -BaekjoonHub

--- 백준/Silver/4883. 삼각 그래프/삼각 그래프.py
+++ 백준/Silver/4883. 삼각 그래프/삼각 그래프.py
@@ -1,21 +1,25 @@
 import sys 
 input = sys.stdin.readline
 
-T = 0
+case = 0
 while True:
-    N = int(input())
-    if N == 0:
+    case += 1
+    n = int(input())
+    if n == 0:
         exit()
-    T += 1
-    graph = [list(map(int,input().split())) for _ in range(N)]
-    dp = [[0] * 3 for _ in range(N)]
-    dp[1][0] = graph[1][0] + graph[0][1]
-    dp[1][1] = min(dp[1][0], graph[0][1], graph[0][2] + graph[0][1]) + graph[1][1]
-    dp[1][2] = min(dp[1][1], graph[0][1], graph[0][1] + graph[0][2]) + graph[1][2]
     
-    for i in range(2,N):
-        dp[i][0] = min(dp[i-1][0], dp[i-1][1]) + graph[i][0]
-        dp[i][1] = min(dp[i-1][0], dp[i-1][1], dp[i-1][2], dp[i][0]) + graph[i][1]
-        dp[i][2] = min(dp[i-1][1], dp[i-1][2], dp[i][1]) + graph[i][2]
-    
-    print(f"{T}. { dp[N-1][1]}")
+    dp = [[0,0,0] for _ in range(n+1)]
+    board = [[]]
+    for _ in range(n):
+        board.append(list(map(int,input().split())))
+
+    dp[2][0] = board[1][1] + board[2][0]
+    dp[2][1] = min(dp[2][0], board[1][1], board[1][1] + board[1][2]) + board[2][1]
+    dp[2][2] = min(dp[2][1], board[1][1], board[1][1] + board[1][2]) + board[2][2]
+
+    for i in range(3,n+1):
+        dp[i][0] = min(dp[i-1][0], dp[i-1][1]) + board[i][0]    
+        dp[i][1] = min(dp[i-1][0], dp[i-1][1], dp[i-1][2], dp[i][0]) + board[i][1]    
+        dp[i][2] = min(dp[i-1][1], dp[i-1][2], dp[i][1]) + board[i][2]    
+
+    print(f"{case}. {dp[n][1]}")
