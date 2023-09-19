486a2fbb445b06d5ac3b62339c934ec9ab6dbf7d
yootal <92325898+yootal@users.noreply.github.com>
2023-09-19 18:02:03 +0900

    [Gold V] Title: 동전 1, Time: 196 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2293. 동전 1/동전 1.py
+++ 백준/Gold/2293. 동전 1/동전 1.py
@@ -1,14 +1,15 @@
-import sys 
+import sys
 input = sys.stdin.readline
 
 n,k = map(int,input().split())
-coin = [int(input()) for _ in range(n)]
+coins = [int(input()) for _ in range(n)]
+    
 dp = [0] * (k+1)
 dp[0] = 1
 
-for i in coin:
-    for j in range(i,k+1):
-        if j-i >= 0:
-            dp[j] += dp[j-i]
-            
+for coin in coins:
+    for j in range(coin,k+1):
+        dp[j] += dp[j - coin]
+
 print(dp[k])
+    
