6abbc2a0668641173dd00ac89c393650f691824f
yootal <92325898+yootal@users.noreply.github.com>
2023-09-19 16:06:24 +0900

    [Silver I] Title: 극장 좌석, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2302. 극장 좌석/극장 좌석.py
+++ 백준/Silver/2302. 극장 좌석/극장 좌석.py
@@ -1,25 +1,26 @@
-import sys 
+import sys
 input = sys.stdin.readline
 
-N = int(input())
-M = int(input())
-vip = [int(input()) for _ in range(M)]
-
+n = int(input())
+m = int(input())
+vip = [int(input()) for _ in range(m)]
+    
 dp = [0] * (41)
 dp[0] = 1
 dp[1] = 1
 dp[2] = 2
 
-for i in range(3,41):
+for i in range(3,n+1):
     dp[i] = dp[i-1] + dp[i-2] 
-               
+
 ans = 1
-if M > 0:
+if m > 0:
     pre = 0
-    for j in range(M):
-        ans *= dp[vip[j] - 1 - pre]
-        pre = vip[j]
-    ans *= dp[N-pre]
+    for i in range(m):
+        ans *= dp[vip[i] - pre - 1]
+        pre = vip[i]
+    ans *= dp[n-pre]
 else:
-    ans = dp[N]
+    ans = dp[n]
+
 print(ans)
