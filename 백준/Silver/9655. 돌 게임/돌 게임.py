a1f25c3665052f4748f091971727eeb1e2562c6e
yootal <92325898+yootal@users.noreply.github.com>
2023-09-20 14:13:26 +0900

    [Silver V] Title: 돌 게임, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.py
+++ 백준/Silver/9655. 돌 게임/돌 게임.py
@@ -1,8 +1,17 @@
 import sys
-input=sys.stdin.readline
+input = sys.stdin.readline
 
 n = int(input())
-if n % 2 == 0:
-    print("CY")
-else:
-    print("SK")
+
+dp = [-1] * 1001
+dp[1] = 1
+dp[2] = 0
+dp[3] = 1
+
+for i in range(4,n+1):
+    if dp[i-1] or dp[i-3]:
+        dp[i] = 0
+    else:
+        dp[i] = 1
+        
+print("CY" if dp[n] == 0 else "SK")
