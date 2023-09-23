b98270d85f32e22efc769712285779954b6c1d0b
yootal <92325898+yootal@users.noreply.github.com>
2023-09-24 06:26:29 +0900

    [Gold I] Title: 택배, Time: 3348 ms, Memory: 34372 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/8980. 택배/택배.py
+++ 백준/Gold/8980. 택배/택배.py
@@ -4,29 +4,25 @@ input = sys.stdin.readline
 
 n,c = map(int,input().split())
 m = int(input())
-truck = defaultdict(list)
+truck = []
 weight_limit = defaultdict(int)
 
 for _ in range(m):
     s,e,w = map(int,input().split())
-    truck[e-s].append((s,e,w))
-    
+    truck.append((e,s,w))
+truck.sort()
+
 ans = 0
-for i in range(1,n): 
-    for s,e,w in truck[i]:
-        possible = sys.maxsize
-        limit_check = True
-        for cur in range(s,e):
-            if weight_limit[s] == c:
-                limit_check = False
-                break
-            if weight_limit[cur] + w > c:
-                possible = min(possible, c - weight_limit[cur]) 
-            else:
-                possible = min(possible, w) 
-        if limit_check:
-            for cur in range(s,e):
-                weight_limit[cur] += possible
-            ans += possible
+for e,s,w in truck:
+    load_able = True
+    for i in range(s,e):
+        w = min(w,c - weight_limit[i])
+        if w == 0:
+            load_able = False
+            break
+    if load_able:
+        ans += w
+        for i in range(s,e):
+            weight_limit[i] += w
 
 print(ans)
