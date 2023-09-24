ea17451990e9587e580ffa49618cd549c7bbd9f9
yootal <92325898+yootal@users.noreply.github.com>
2023-09-24 18:04:24 +0900

    [Gold V] Title: 멀티버스 Ⅱ, Time: 736 ms, Memory: 152220 KB -BaekjoonHub

--- 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
+++ 백준/Gold/18869. 멀티버스 Ⅱ/멀티버스 Ⅱ.py
@@ -1,22 +1,44 @@
 import sys
-from bisect import bisect_left
 input = sys.stdin.readline
 
 m,n = map(int,input().split())
 space = [list(map(int,input().split())) for _ in range(m)]
-compress = []
+compress_space = []
 
-for i in range(m):
-    space2 = []
-    sort_space = sorted(space[i])
-    for j in range(n):
-        space2.append(bisect_left(sort_space,space[i][j]))
-    compress.append(space2)
-    
+def lower_idx(target,len,sp):
+    st = 0
+    en = len
+    while st < en:
+        mid = (st + en) // 2
+        if sp[mid] >= target:
+            en = mid
+        else:
+            st = mid + 1
+    return st
+
+def compress(sp):
+    compress_sp = []
+    sp_set = sorted(list(set(sp)))
+    sp_set_len = len(sp_set)
+    for planet in sp:
+            compress_sp.append(lower_idx(planet,sp_set_len,sp_set))
+    return compress_sp
+
+for sp in space:
+    compress_space.append(compress(sp))
+
+def space_check(s1,s2):
+    for k in range(n):
+        if s1[k] == s2[k]:
+            continue
+        else:
+            return False
+    return True
+
 ans = 0
-for i in range(m-1):
+for i in range(m):
     for j in range(i+1,m):
-        if compress[i] == compress[j]:
+        if space_check(compress_space[i],compress_space[j]):
             ans += 1
             
 print(ans)
