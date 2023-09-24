e9a7a1917ee3983df116be8ec513f076446991ce
yootal <92325898+yootal@users.noreply.github.com>
2023-09-24 18:41:44 +0900

    [Gold V] Title: 용액, Time: 708 ms, Memory: 42176 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+solutions = list(map(int,input().split()))
+
+ans = sys.maxsize
+ans_st = 0
+ans_en = 0
+
+for i in range(n-1):
+    current = solutions[i]
+    st = i + 1
+    en = n - 1
+    
+    while st <= en:
+        mid = (st + en) // 2
+        check = current + solutions[mid]
+        if abs(check) < ans:
+            ans = abs(check)
+            ans_left = i
+            ans_right = mid
+            if check == 0:
+                break
+            
+        if check < 0:
+            st = mid + 1
+        else:
+            en = mid - 1
+
+print(solutions[ans_left], solutions[ans_right])
