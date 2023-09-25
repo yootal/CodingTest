45cef948db7218fe637624e14206b4c0932a6a55
yootal <92325898+yootal@users.noreply.github.com>
2023-09-25 15:05:01 +0900

    [Gold V] Title: 수 고르기, Time: 612 ms, Memory: 35500 KB -BaekjoonHub

--- 백준/Gold/2230. 수 고르기/수 고르기.py
+++ 백준/Gold/2230. 수 고르기/수 고르기.py
@@ -1,16 +1,25 @@
 import sys
 input = sys.stdin.readline
 
+def binary_search(target,start):
+    global ans 
+    st = start
+    en = n - 1
+    while st <= en:
+        mid = (st + en) // 2
+        gap = num[mid] - target
+        if gap >= m:
+            ans = min(ans,gap)
+            en = mid - 1
+        else:
+            st = mid + 1
+    return
+
 n,m = map(int,input().split())
 num = [int(input()) for _ in range(n)]
 num.sort()
 
 ans = sys.maxsize
-en = 0
-for st in range(n):
-    while en < n and num[en] - num[st] < m:
-        en += 1
-    if en == n:
-        break
-    ans = min(ans, num[en] - num[st])
+for i in range(n-1):
+    binary_search(num[i],i+1)    
 print(ans)
