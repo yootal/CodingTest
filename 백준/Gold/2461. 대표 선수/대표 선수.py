860c93a1df831928634e65032dd75682275c718f
yootal <92325898+yootal@users.noreply.github.com>
2023-09-27 00:57:09 +0900

    [Gold I] Title: 대표 선수, Time: 1644 ms, Memory: 70284 KB -BaekjoonHub

--- 백준/Gold/2461. 대표 선수/대표 선수.py
+++ 백준/Gold/2461. 대표 선수/대표 선수.py
@@ -1,34 +1,37 @@
 import sys
-from collections import defaultdict
+from collections import deque
+from heapq import heappush, heappop
 input = sys.stdin.readline
     
 n,m = map(int,input().split())
 group = []
-for i in range(1,n+1):
+for i in range(n):
     level = list(map(int,input().split()))
-    for l in level:
-        group.append((l,i))
-group.sort()
-
-count = defaultdict(int)
-check = [True] + [False] * n
-
-en = 0
-ans = sys.maxsize
-n *= m
-for st in range(n):
-    while en < n and False in check:
-        count[group[en][1]] += 1
-        check[group[en][1]] = True
-        en += 1
+    level.sort()
+    group.append(deque(level))
     
-    if en == n:
+q = []
+mn = sys.maxsize
+mx = 0
+
+for i in range(n):
+    v = group[i].popleft()
+    mx = max(mx, v)
+    mn = min(mn, v)
+    heappush(q, (v, i))
+    
+ans = mx - mn
+
+while q:
+    pre_mn,i = heappop(q)
+    if not group[i]:
         break
-    if group[en-1][1] != group[st][1]:
-        ans = min(ans,group[en-1][0] - group[st][0])
+    new = group[i].popleft()
+    heappush(q,(new,i))
     
-    count[group[st][1]] -= 1
-    if count[group[st][1]] == 0:
-        check[group[st][1]] = False
+    if mx < new:
+        mx = new
+    mn = q[0][0]
+    ans = min(ans, mx - mn)
 
 print(ans)
