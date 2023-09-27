7321a1a8a952f4a9461c591bd018dd2167e8c814
yootal <92325898+yootal@users.noreply.github.com>
2023-09-27 17:43:22 +0900

    [Gold IV] Title: 합이 0, Time: 8452 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Gold/3151. 합이 0/합이 0.py
+++ 백준/Gold/3151. 합이 0/합이 0.py
@@ -1,38 +1,34 @@
 import sys
 input = sys.stdin.readline
-
-n = int(input())
-ability = list(map(int,input().split()))
-ability.sort()
-
-def lower_idx(target,start):
-    st = start
-    en = n
     
-    while st < en:
-        mid = (st + en) // 2
-        if ability[mid] < target:
-            st = mid + 1
-        else:
-            en = mid
-    return st
+n = int(input())
+student = list(map(int,input().split()))
+student.sort()
 
-def upper_idx(target,start):
-    st = start
-    en = n
+def two_pointer(st,en,find):  
+    global ans  
+    max_idx = n
     while st < en:
-        mid = (st + en) // 2
-        if ability[mid] <= target:
-            st = mid + 1
+        value = student[st] + student[en]
+        if value < find:
+            st += 1
+        elif value == find:
+            if student[st] == student[en]:
+                ans += (en - st)
+            else:
+                if max_idx > en:
+                    max_idx = en
+                    while max_idx > st and student[max_idx - 1] == student[en]:
+                        max_idx -= 1
+                ans += en - max_idx + 1
+            st += 1
         else:
-            en = mid
-    return st
-
+            en -= 1
+                
 ans = 0
-for i in range(n-1):
-    for j in range(i+1,n):
-        right = upper_idx(-ability[i]-ability[j],j+1)
-        left = lower_idx(-ability[i]-ability[j],j+1)
-        ans += (right - left)
+for i in range(n-2):
+    st = i + 1
+    en = n - 1       
+    two_pointer(st,en,-student[i]) 
+        
 print(ans)
-
