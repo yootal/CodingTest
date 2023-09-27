894a7d4c422ff6c7c33e00d746ed2c3be3bc081d
yootal <92325898+yootal@users.noreply.github.com>
2023-09-27 18:06:29 +0900

    [Gold IV] Title: 좋다, Time: 7760 ms, Memory: 116832 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.py
+++ 백준/Gold/1253. 좋다/좋다.py
@@ -1,37 +1,41 @@
 import sys
 input = sys.stdin.readline
-    
+
 n = int(input())
 num = list(map(int,input().split()))
 num.sort()
+idx_check = [False] * n
 
-check = [False] * n
-
-def two_pointer(st,en,i):  
-    global ans 
-    find = num[i] 
-    while st < en:
-        if st == i:
-            st += 1
-            continue
-        if en == i:
-            en -= 1
-            continue
-        value = num[st] + num[en]
-        if value < find:
-            st += 1
-        elif value == find:
-            if not check[i]:
-                check[i] = True
-                ans += 1
-            return
-        else:
-            en -= 1
-        
-ans = 0
-for i in range(n):
+def lower_idx(target):
     st = 0
-    en = n-1
-    two_pointer(st,en,i) 
-        
+    en = n
+    while st < en:
+        mid = (st + en) // 2
+        if num[mid] < target:
+            st = mid + 1
+        else:
+            en = mid
+    return st
+
+def upper_idx(target):
+    st = 0
+    en = n
+    while st < en:
+        mid = (st + en) // 2
+        if num[mid] <= target:
+            st = mid + 1
+        else:
+            en = mid
+    return st
+
+ans = 0
+for i in range(n-1):
+    for j in range(i+1,n):
+        target = num[i] + num[j]
+        left = lower_idx(target)
+        right = upper_idx(target)
+        for k in range(left,right):
+                if k != i and k != j and not idx_check[k]:
+                    idx_check[k] = True
+                    ans += 1
 print(ans)
