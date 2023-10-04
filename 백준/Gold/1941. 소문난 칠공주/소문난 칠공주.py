460beb735e74be25866ac94d7b317e97e9ec653e
yootal <92325898+yootal@users.noreply.github.com>
2023-10-04 17:57:11 +0900

    [Gold III] Title: 소문난 칠공주, Time: 2472 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
+++ 백준/Gold/1941. 소문난 칠공주/소문난 칠공주.py
@@ -1,36 +1,45 @@
 import sys
-from itertools import combinations
-from collections import deque
-input = sys.stdin.readline
+input=sys.stdin.readline
 
-location = {5 * i + j: (i, j) for i in range(5) for j in range(5)}
-d = [(-1,0),(1,0),(0,-1),(0,1)]
+dy,dx = [-1,1,0,0],[0,0,-1,1]
 
-def check(case):
-    vis = set(case[1:])
-    q = deque([case[0]])
-    while q:
-        x,y = q.popleft()
-        for dx,dy in d:
-            nx = x + dx
-            ny = y + dy
-            if nx < 0 or nx >= 5 or ny < 0 or ny >= 5:
-                continue
-            if (nx,ny) in vis:
-                vis.remove((nx,ny))
-                q.append((nx,ny))
-    if vis:
-        return False
-    return True
-
+cnt = 0
 board = []
 for _ in range(5):
-    board += list(input().rstrip())
-    
-ans = 0
-for points in combinations(range(25),7):
-    count = sum(map(lambda x: 1 if board[x] == 'S' else 0, points))
-    if count >= 4 and check(list(map(lambda x: location[x], points))):
-        ans += 1
-    
-print(ans)
+    board += list(map(str,input().strip()))
+visited = [0 for _ in range(25)]
+check = [[0] * 5 for _ in range(5)]
+
+def permu(idx,ans,position):
+    global cnt,check
+    if ans.count('Y') > 3:
+        return
+    if idx == 7:
+        if ans.count('S') >= 4:
+            position_check(position[0],position)
+            if sum(sum(check,[])) == 7:
+                cnt += 1
+            check = [[0] * 5 for _ in range(5)]
+        return
+    for i in range(25):
+        if visited[i] == 0:
+            visited[i] = 1
+            permu(idx + 1, ans + board[i], position + [i])
+            for j in range(i+1,25):
+                visited[j] = 0
+                
+def position_check(s,position):
+    r = s // 5
+    c = s % 5
+    check[r][c] = 1
+    for j in range(4):
+        ny = r + dy[j]
+        nx = c + dx[j]
+        if 0 <= ny < 5 and 0 <= nx < 5:
+            next = ny * 5 + nx
+            if check[ny][nx] == 0 and next in position:
+                check[ny][nx] = 1
+                position_check(next,position)
+            
+permu(0,'',[])
+print(cnt)
