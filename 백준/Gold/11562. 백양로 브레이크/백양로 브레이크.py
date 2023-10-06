70d55cd665c62fded63a20aad111db0d4730b8d1
yootal <92325898+yootal@users.noreply.github.com>
2023-10-06 14:46:24 +0900

    [Gold III] Title: 백양로 브레이크, Time: 1420 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.py
+++ 백준/Gold/11562. 백양로 브레이크/백양로 브레이크.py
@@ -1,26 +1,28 @@
-import sys
-input = sys.stdin.readline
-inf = sys.maxsize
+from sys import maxsize, stdin
+input = stdin.readline
+
+def floyd():
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1,n+1):
+                if graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
 
 n,m = map(int,input().split())
-graph = [[inf] * (n+1) for _ in range(n+1)]
-
+graph = [[maxsize] * (n+1) for _ in range(n+1)]
 for _ in range(m):
-    u,v,b = map(int,input().split())
-    if b == 0:
-        graph[u][v] = 0
-        graph[v][u] = 1
-    else:
-        graph[u][v] = 0
-        graph[v][u] = 0
-        
+    a,b,c = map(int,input().split())
+    if c == 0:
+        graph[a][b] = 0
+        graph[b][a] = 1
+    elif c == 1:
+        graph[a][b] = 0
+        graph[b][a] = 0
+
 for i in range(1,n+1):
     graph[i][i] = 0
-        
-for l in range(1,n+1):
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            graph[i][j] = min(graph[i][j], graph[i][l] + graph[l][j]) 
+                
+floyd()
 
 k = int(input())
 for _ in range(k):
