264296ee3cffd4cf09ed85f71101a5c020923c1a
yootal <92325898+yootal@users.noreply.github.com>
2023-10-06 14:37:02 +0900

    [Gold IV] Title: 운동, Time: 6420 ms, Memory: 36900 KB -BaekjoonHub

--- 백준/Gold/1956. 운동/운동.py
+++ 백준/Gold/1956. 운동/운동.py
@@ -1,30 +1,30 @@
 import sys
-input = sys.stdin.readline
 inf = sys.maxsize
+input = sys.stdin.readline
+
+def floyd():
+    for k in range(1,v+1):
+        for i in range(1,v+1):
+            for j in range(1,v+1):
+                if graph[i][j] > graph[i][k] + graph[k][j]:
+                    graph[i][j] = graph[i][k] + graph[k][j]
 
 v,e = map(int,input().split())
 graph = [[inf] * (v+1) for _ in range(v+1)]
-
 for _ in range(e):
     a,b,c = map(int,input().split())
-    c = min(graph[a][b],c)
     graph[a][b] = c
-    
-for i in range(1,v+1):
-    graph[i][i] = 0    
-    
-for k in range(1,v+1):
-    for i in range(1,v+1):
-        for j in range(1,v+1):
-            if graph[i][j] > graph[i][k] + graph[k][j]:
-                graph[i][j] = graph[i][k] + graph[k][j]
 
-ans = inf
 for i in range(1,v+1):
-    for j in range(i+1,v+1):
-        ans = min(ans, graph[i][j] + graph[j][i])
-        
-if ans == inf:
-    print(-1)
-else:
-    print(ans)
+    graph[i][i] = 0
+                
+floyd()
+min_cycle = inf
+for i in range(1,v+1):
+    for j in range(1,v+1):
+        if i == j:
+            continue
+        if min_cycle > graph[i][j] + graph[j][i]:
+            min_cycle = graph[i][j] + graph[j][i]
+
+print(min_cycle if min_cycle != inf else -1)
