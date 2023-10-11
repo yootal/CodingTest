245253315abc239205694ef7b50a37460bfb3e81
yootal <92325898+yootal@users.noreply.github.com>
2023-10-11 15:04:25 +0900

    [Silver II] Title: 마인크래프트, Time: 140 ms, Memory: 34052 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,38 +1,29 @@
 from sys import stdin, maxsize
+from collections import Counter
 input = stdin.readline
 
 n,m,b = map(int,input().split())
-ground = []
-
-highest = 0
+ground = Counter()
 for _ in range(n):
-    inp = list(map(int,input().split()))
-    for x in inp:
-        highest = max(highest,x)
-    ground.append(inp)
+    ground.update(map(int,input().split()))
     
 ans_h = 0
 ans_t = maxsize
 
-for h in range(highest,-1,-1):
+for h in range(min(ground),max(ground)+1):
     have = b
     time = 0
-    for i in range(n):
-        for j in range(m):
-            cnt = h - ground[i][j]
-            if cnt > 0:
-                time += cnt
-                have -= cnt    
-            elif cnt < 0:
-                time -= 2*cnt
-                have -= cnt
-                
-    if time >= ans_t:
-        break     
-    if have < 0:
-        continue
-
-    ans_t = time
-    ans_h = h
+    for height, cnt in ground.items():
+        if height > h:
+                time += (height - h) * 2 * cnt
+                have += (height - h) * cnt    
+        else:
+            time += (h - height) * cnt
+            have -= (h - height) * cnt
+    if have >= 0:
+        if time <= ans_t:
+            ans_t = time
+            ans_h = h         
 
 print(ans_t,ans_h)
+     
