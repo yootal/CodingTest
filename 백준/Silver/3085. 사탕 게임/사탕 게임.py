3404f78c08c43541167586a207af1447ae13d1ea
yootal <92325898+yootal@users.noreply.github.com>
2023-10-11 15:59:56 +0900

    [Silver II] Title: 사탕 게임, Time: 68 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/3085. 사탕 게임/사탕 게임.py
+++ 백준/Silver/3085. 사탕 게임/사탕 게임.py
@@ -3,27 +3,19 @@ input = stdin.readline
 
 d = [(1,0),(-1,0),(0,1),(0,-1)]
 
-def count():
-    global max_cnt
-    for i in range(n):
-        for j in range(n):
-            cur = board[i][j]
-            ni = i
-            nj = j
-            cnt1 = 0
-            cnt2 = 0
-            while 0 <= nj < n and board[i][nj] == cur:
-                cnt1 += 1
-                nj += 1
-            while 0 <= ni < n and board[ni][j] == cur:
-                cnt2 += 1
-                ni += 1
-            max_cnt = max(max_cnt,cnt1,cnt2)
-
 n = int(input())
 board = [list(input().rstrip()) for _ in range(n)]
 
 max_cnt = 0
+for i in range(n):
+    for j in range(n):
+        x,y = i,j
+        while x < n and board[i][j] == board[x][j]:
+            x += 1
+        while y < n and board[i][j] == board[i][y]:
+            y += 1
+        max_cnt = max(max_cnt,x-i,y-j)
+        
 for i in range(n):
     for j in range(n):
         cur = board[i][j]
@@ -32,12 +24,18 @@ for i in range(n):
             ny = j + dy
             if 0 <= nx < n and 0 <= ny < n:
                 if board[nx][ny] != board[i][j]:
-                    temp = board[i][j]
-                    board[i][j] = board[nx][ny]
-                    board[nx][ny] = temp
-                    count()
-                    temp = board[i][j]
-                    board[i][j] = board[nx][ny]
-                    board[nx][ny] = temp
+                    board[i][j],board[nx][ny] = board[nx][ny],board[i][j]
+                    x1,x2 = i,i
+                    while 0 <= x1 and board[i][j] == board[x1][j]:
+                        x1 -= 1
+                    while x2 < n and board[i][j] == board[x2][j]:
+                        x2 += 1
+                    y1,y2 = j,j
+                    while 0 <= y1 and board[i][j] == board[i][y1]:
+                        y1 -= 1
+                    while y2 < n and board[i][j] == board[i][y2]:
+                        y2 += 1
+                    max_cnt = max(max_cnt,x2-x1-1,y2-y1-1)
+                    board[i][j],board[nx][ny] = board[nx][ny],board[i][j]
                     
 print(max_cnt)
