a4216b424ac84eb92dae1625b0b626a315b64386
yootal <92325898+yootal@users.noreply.github.com>
2023-10-13 15:12:06 +0900

    [Silver I] Title: 배열 돌리기 1, Time: 136 ms, Memory: 35096 KB -BaekjoonHub

--- 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
+++ 백준/Silver/16926. 배열 돌리기 1/배열 돌리기 1.py
@@ -1,22 +1,30 @@
 from sys import stdin
+from collections import deque
 input = stdin.readline
 
 n,m,r = map(int,input().split())
 board = [list(map(int,input().split())) for _ in range(n)]
+ans = [[0] * m for _ in range(n)]
+dq = deque()
 
-for _ in range(r):
-    for i in range(min(n,m)//2):
-        temp = board[i][i]
-        for i1 in range(i,m-i-1):
-            board[i][i1] = board[i][i1+1]
-        for i2 in range(i,n-i-1):
-            board[i2][m-1-i] = board[i2+1][m-1-i]
-        for i3 in range(m-1-i,i,-1):
-            board[n-1-i][i3] = board[n-1-i][i3-1]
-        for i4 in range(n-1-i,1+i,-1):
-            board[i4][i] = board[i4-1][i]
-        board[i+1][i] = temp
+loop = min(n,m) // 2
+for i in range(loop):
+    dq.clear()
+    dq.extend(board[i][i:m-i])
+    dq.extend([row[m-i-1] for row in board[i+1:n-i-1]])
+    dq.extend(board[n-i-1][i:m-i][::-1])
+    dq.extend([row[i] for row in board[i+1:n-i-1]][::-1])
+    dq.rotate(-r)
+    
+    for j in range(i,m-i):
+        ans[i][j] = dq.popleft()
+    for j in range(i+1,n-i-1):
+        ans[j][m-i-1] = dq.popleft()
+    for j in range(m-i-1,i-1,-1):
+        ans[n-i-1][j] = dq.popleft()
+    for j in range(n-i-2,i,-1):
+        ans[j][i] = dq.popleft()
 
-for b in board: 
-    print(*b)
+for a in ans: 
+    print(*a)
         
