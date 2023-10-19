c92a3fee3f42920e38d401ebd05ed7b7e70197a7
yootal <92325898+yootal@users.noreply.github.com>
2023-10-19 23:04:37 +0900

    [D2] Title: 새로운 불면증 치료법, Time: 175 ms, Memory: 61,720 KB -BaekjoonHub

---
+++ SWEA/D2/1288. 새로운 불면증 치료법/새로운 불면증 치료법.py
@@ -0,0 +1,13 @@
+t  = int(input())
+for case in range(1,t+1):
+    n = int(input())
+    check = {0,1,2,3,4,5,6,7,8,9}
+    count = 0
+    value = n
+    while check:
+        count += 1
+        num = list(str(value))
+        for x in num:
+            check.discard(int(x))
+        value += n
+    print(f"#{case} {count*n}")
