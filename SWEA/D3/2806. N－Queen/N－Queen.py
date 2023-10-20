ff85a6c11c21b40b69be0b0e33139b3bfbcbc665
yootal <92325898+yootal@users.noreply.github.com>
2023-10-20 18:06:13 +0900

    [D3] Title: N-Queen, Time: 246 ms, Memory: 63,832 KB -BaekjoonHub

---
+++ SWEA/D3/2806. N－Queen/N－Queen.py
@@ -0,0 +1,39 @@
+def n_queen(k):
+    global ans
+    if k == n:
+        ans += 1
+        return
+    for j in range(n):
+        if not j_check[j] and check(k,j):
+            j_check[j] = True
+            board[k][j] = True
+            n_queen(k+1)
+            j_check[j] = False
+            board[k][j] = False
+        
+def check(x,y):
+    i = x
+    j = y
+    while j > 0 and i > 0:
+        i -= 1
+        j -= 1
+        if board[i][j]:
+            return False
+    i = x
+    j = y
+    while j < n - 1 and i > 0:
+        i -= 1
+        j += 1
+        if board[i][j]:
+            return False
+
+    return True
+
+t = int(input())
+for case in range(1,t+1):
+    n = int(input())
+    board = [[False] * n for _ in range(n)]
+    j_check = [False] * n
+    ans = 0
+    n_queen(0)
+    print(f"#{case} {ans}")
