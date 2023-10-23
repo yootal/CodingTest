90ebadc8f13b72e5c235cfc2f96d77b19a3b6a92
yootal <92325898+yootal@users.noreply.github.com>
2023-10-23 16:34:42 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 172 ms, Memory: 60,412 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.py
@@ -0,0 +1,39 @@
+for case in range(1,11):
+    n = int(input())
+    original = list(map(int,input().split()))
+    c = int(input())
+    command = list(input().rstrip().split())
+    cur = 0
+    count = 0
+    while count < c:
+        if command[cur] == 'I':
+            cur += 1
+            idx = int(command[cur])
+            cur += 1
+            cnt = int(command[cur])
+            cur += 1
+            for x in range(cur+cnt-1,cur-1,-1):
+                original.insert(idx,int(command[x]))
+            cur += cnt
+            count += 1
+        elif command[cur] == 'D':
+            cur += 1
+            idx = int(command[cur])
+            cur += 1
+            en = int(command[cur])
+            for x in range(en):
+                original.pop(idx)
+            cur += 1
+            count += 1
+        elif command[cur] == 'A':
+            cur += 1
+            cnt = int(command[cur])
+            cur += 1
+            for x in range(cur,cur+cnt):
+                original.append(int(command[x]))
+            cur += cnt
+            count += 1
+    print(f"#{case}",end=" ")
+    for cur in range(10):
+        print(original[cur],end=" ")
+    print()
