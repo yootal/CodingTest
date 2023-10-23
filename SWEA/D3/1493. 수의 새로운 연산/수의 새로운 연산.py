e26997200fc0cc8d579cad4ec6cba091272f5fd5
yootal <92325898+yootal@users.noreply.github.com>
2023-10-23 14:10:54 +0900

    [D3] Title: 수의 새로운 연산, Time: 299 ms, Memory: 63,120 KB -BaekjoonHub

---
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.py
@@ -0,0 +1,20 @@
+points = [0] * 50001
+board = [[0] * 400 for _ in range(400)]
+x,y = 1,1
+nxt = 2 
+for i in range(1,50001):
+    points[i] = (x,y)
+    board[x][y] = i
+    y -= 1
+    if y == 0:
+        x = 1
+        y = nxt
+        nxt += 1
+        continue
+    x += 1
+
+t = int(input())
+for case in range(1,t+1):
+    p,q = map(int,input().split())
+    after = (points[p][0] + points[q][0],points[p][1] + points[q][1])
+    print(f"#{case} {board[after[0]][after[1]]}")
