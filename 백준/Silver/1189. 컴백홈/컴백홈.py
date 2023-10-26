4b61aa5790ab7770b10b49cb0fb1f473cccb051f
yootal <92325898+yootal@users.noreply.github.com>
2023-10-26 17:52:47 +0900

    [Silver I] Title: 컴백홈, Time: 132 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1189. 컴백홈/컴백홈.py
+++ 백준/Silver/1189. 컴백홈/컴백홈.py
@@ -1,27 +1,23 @@
 from sys import stdin
-from copy import deepcopy
 input = stdin.readline
 
 n,m,k = map(int,input().split())
 board = [list(input().rstrip()) for _ in range(n)]
-visit = [[-1] * m for _ in range(n)]
 
-def dfs(x,y,vis):
+def dfs(x,y,d):
     global ans
-    if x == 0 and y == m-1:
-        if vis[x][y] == k:
-            ans += 1
-        return
-    for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-        nx = x + dx
-        ny = y + dy
-        if 0 <= nx < n and 0 <= ny < m:
-            if board[nx][ny] == '.' and vis[nx][ny] == -1:
-                vis2 = deepcopy(vis)
-                vis2[nx][ny] = vis[x][y] + 1
-                dfs(nx,ny,vis2)
+    if x == 0 and y == m-1 and d == k:
+        ans += 1
+    else:
+        board[x][y] = 'T'
+        for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
+            nx = x + dx
+            ny = y + dy
+            if 0 <= nx < n and 0 <= ny < m and board[nx][ny] == '.':
+                board[nx][ny] = 'T'
+                dfs(nx,ny,d+1)
+                board[nx][ny] = '.'
 
 ans = 0
-visit[n-1][0] = 1
-dfs(n-1,0,visit)
-print(ans)       
+dfs(n-1,0,1)
+print(ans)      
