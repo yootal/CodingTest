bf15f48ffc9c435e6bdeda61ab421855be90185f
yootal <92325898+yootal@users.noreply.github.com>
2023-10-28 00:47:21 +0900

    [D3] Title: 제곱수 만들기, Time: 932 ms, Memory: 73,136 KB -BaekjoonHub

---
+++ SWEA/D3/10965. 제곱수 만들기/README.md
@@ -0,0 +1,15 @@
+# [D3] 제곱수 만들기 - 10965 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXWXH_h695kDFAST) 
+
+### 성능 요약
+
+메모리: 73,136 KB, 시간: 932 ms, 코드길이: 599 Bytes
+
+### 제출 일자
+
+2023-10-28 00:47
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/10965. 제곱수 만들기/제곱수 만들기.py
@@ -0,0 +1,26 @@
+def eratosthenes():
+    prime = []
+    num = [False] * (int(10000000 ** 0.5) + 2)
+    for i in range(2, int(10000000 ** 0.5) + 2):
+        if not num[i]:
+            prime.append(i)
+            for j in range(i * i, int(10000000 ** 0.5) + 2, i):
+                num[j] = True
+    return prime
+
+
+t = int(input())
+ans = []
+primes = eratosthenes()
+for case in range(1, t + 1):
+    a = int(input())
+    i = 0
+    while a >= primes[i] ** 2:
+        if a % (primes[i] ** 2) == 0:
+            a //= primes[i] ** 2
+        else:
+            i += 1
+    ans.append(f'#{case} {a}')
+
+for x in ans:
+    print(x)
