e9d77563cc79a2e720face4be2f8470f52f90ed9
yootal <92325898+yootal@users.noreply.github.com>
2023-10-27 18:28:42 +0900

    [D3] Title: 의석이의 세로로 말해요, Time: 242 ms, Memory: 64,484 KB -BaekjoonHub

---
+++ SWEA/D3/5356. 의석이의 세로로 말해요/의석이의 세로로 말해요.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+t = int(input())
+for case in range(1, t+1):
+    idx = defaultdict(list)
+    for _ in range(5):
+        inp = list(input().rstrip())
+        for i in range(len(inp)):
+            idx[i].append(inp[i])
+    ans = ""
+    for k in sorted(idx.keys()):
+        for x in idx[k]:
+            ans += x
+    print(f'#{case} {ans}')
