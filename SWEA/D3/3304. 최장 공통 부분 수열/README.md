51f88142fd664bb98c0095dc9af53ed12338bb7b
yootal <92325898+yootal@users.noreply.github.com>
2023-10-29 20:39:55 +0900

    [D3] Title: 최장 공통 부분 수열, Time: 348 ms, Memory: 91,032 KB -BaekjoonHub

---
+++ SWEA/D3/3304. 최장 공통 부분 수열/README.md
@@ -0,0 +1,15 @@
+# [D3] 최장 공통 부분 수열 - 3304 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWBOHEx66kIDFAWr) 
+
+### 성능 요약
+
+메모리: 91,032 KB, 시간: 348 ms, 코드길이: 427 Bytes
+
+### 제출 일자
+
+2023-10-29 20:39
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/3304. 최장 공통 부분 수열/최장 공통 부분 수열.py
@@ -0,0 +1,11 @@
+t = int(input())
+for case in range(1, t + 1):
+    s1, s2 = input().rstrip().split()
+    dp = [[0] * (len(s2) + 1) for _ in range(len(s1) + 1)]
+    for i in range(1, len(s1) + 1):
+        for j in range(1, len(s2) + 1):
+            if s1[i - 1] == s2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+    print(f'#{case} {dp[len(s1)][len(s2)]}')
