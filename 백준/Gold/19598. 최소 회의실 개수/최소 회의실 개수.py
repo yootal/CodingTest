f0b2b48ee96be883f7b1be3bf847e54828abfdeb
yootal <92325898+yootal@users.noreply.github.com>
2023-10-30 15:09:34 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 264 ms, Memory: 47436 KB -BaekjoonHub

--- 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -3,17 +3,13 @@ from heapq import heappush, heappop
 input = stdin.readline        
 
 n  = int(input())
-time = [tuple(map(int,input().split())) for _ in range(n)]
-time.sort(key=lambda x:(-x[1],x[0]),reverse=True)
+time = sorted([tuple(map(int,input().split())) for _ in range(n)])
 
 hq = []
 ans = 0
-while time:
-    s,e = time.pop()
-    if hq and -hq[0][0] >= e:
+for st, en in time:
+    if hq and hq[0] <= st:
         heappop(hq)
-        heappush(hq,(-s,e))
-    else:
-        ans += 1
-        heappush(hq,(-s,e))
-print(ans) 
+    heappush(hq,en)
+
+print(len(hq)) 
