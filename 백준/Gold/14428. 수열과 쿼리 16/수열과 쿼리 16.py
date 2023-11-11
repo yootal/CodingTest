3a79f77b72a18350a5996f5c950d1f8f275605b7
yootal <92325898+yootal@users.noreply.github.com>
2023-11-11 20:31:08 +0900

    [Gold I] Title: 수열과 쿼리 16, Time: 2048 ms, Memory: 53188 KB -BaekjoonHub

---
+++ 백준/Gold/14428. 수열과 쿼리 16/수열과 쿼리 16.py
@@ -0,0 +1,49 @@
+from sys import stdin
+from math import ceil, log2
+input = stdin.readline
+
+def init(node,st,ed):
+    if st == ed:
+        tree[node] = num_idx[st]
+        return tree[node]
+    mid = (st + ed) // 2
+    tree[node] = min(init(2*node,st,mid),init(2*node+1,mid+1,ed),key=lambda x: x[0])
+    return tree[node]
+
+def segment_min(node,st,ed,l,r):
+    if l > ed or r < st:
+        return (1000000001,1000000001)
+    if l <= st and ed <= r:
+        return tree[node]
+    mid = (st + ed) // 2
+    return min(segment_min(node*2,st,mid,l,r),segment_min(node*2+1,mid+1,ed,l,r),key=lambda x:x[0])
+
+def update(node,st,ed,idx,diff):
+    if idx < st or idx > ed:
+        return
+    if st == ed:
+        tree[node] = (diff,idx+1)
+    else:
+        mid = (st + ed) // 2
+        update(node*2,st,mid,idx,diff)
+        update(node*2+1,mid+1,ed,idx,diff)
+        tree[node] = min(tree[node*2],tree[node*2+1])
+
+n = int(input())
+h = ceil(log2(n))
+tree_size = 1 << (h+1)
+tree = [0] * tree_size
+num = list(map(int,input().split()))
+num_idx = []
+for i in range(1,n+1):
+    num_idx.append((num[i-1],i))
+m = int(input())
+    
+init(1,0,n-1)
+    
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    if a == 1:
+        update(1,0,n-1,b-1,c)
+    else:
+        print(segment_min(1,0,n-1,b-1,c-1)[1])
