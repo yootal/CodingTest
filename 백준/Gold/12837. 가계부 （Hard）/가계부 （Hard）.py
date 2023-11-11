0c8107a9d5877548b1b6675dca5a6e2ded8554eb
yootal <92325898+yootal@users.noreply.github.com>
2023-11-11 20:38:33 +0900

    [Gold I] Title: 가계부 (Hard), Time: 1640 ms, Memory: 68708 KB -BaekjoonHub

---
+++ 백준/Gold/12837. 가계부 （Hard）/가계부 （Hard）.py
@@ -0,0 +1,46 @@
+from sys import stdin
+from math import ceil, log2
+input = stdin.readline
+
+def init(node,st,ed):
+    if st == ed:
+        tree[node] = num[st]
+        return tree[node]
+    mid = (st + ed) // 2
+    tree[node] = init(2*node,st,mid) + init(2*node+1,mid+1,ed)
+    return tree[node]
+
+def segment_sum(node,st,ed,l,r):
+    if l > ed or r < st:
+        return 0
+    if l <= st and ed <= r:
+        return tree[node]
+    mid = (st + ed) // 2
+    return segment_sum(node*2,st,mid,l,r) + segment_sum(node*2+1,mid+1,ed,l,r)
+
+def update(node,st,ed,idx,diff):
+    if idx < st or idx > ed:
+        return
+    if st == ed:
+        tree[node] += diff
+    else:
+        mid = (st + ed) // 2
+        update(node*2,st,mid,idx,diff)
+        update(node*2+1,mid+1,ed,idx,diff)
+        tree[node] = tree[node*2] + tree[node*2+1]
+
+n,m = map(int,input().split())
+h = ceil(log2(n))
+tree_size = 1 << (h+1)
+tree = [0] * tree_size
+num = [0] * n
+    
+init(1,0,n-1)
+    
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    if a == 1:
+        update(1,0,n-1,b-1,c)
+    else:
+        print(segment_sum(1,0,n-1,b-1,c-1))
+        
