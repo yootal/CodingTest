f8f4383e1bdf2c329bc586ad6587a2a306d84ea8
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 03:11:27 +0900

    [D2] Title: 초심자의 회문 검사, Time: 148 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.py
@@ -1,7 +1,7 @@
-t  = int(input())
-for case in range(1,t+1):
-    string = input().rstrip()
-    if string == string[::-1]:
-        print(f"#{case} 1")
+t = int(input())
+for case in range(1, t + 1):
+    s = input()
+    if s == s[::-1]:
+        print(f'#{case} 1')
     else:
-        print(f"#{case} 0")
+        print(f'#{case} 0')
