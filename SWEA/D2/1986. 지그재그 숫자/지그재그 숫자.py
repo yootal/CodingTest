edc7ea26b8a97cc66afbc3c8f48231fdda829aff
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 04:14:40 +0900

    [D2] Title: 지그재그 숫자, Time: 140 ms, Memory: 58,760 KB -BaekjoonHub

--- SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
+++ SWEA/D2/1986. 지그재그 숫자/지그재그 숫자.py
@@ -1,12 +1,10 @@
-t  = int(input())
-for case in range(1,t+1):
+t = int(input())
+for case in range(1, t + 1):
     n = int(input())
-    total = 0
-    
-    for i in range(1,n+1):
-        if i % 2 == 0:
-            total -= i
+    ans = 0
+    for i in range(1, n + 1):
+        if i % 2:
+            ans += i
         else:
-            total += i
-        
-    print(f"#{case} {total}")
+            ans -= i
+    print(f'#{case} {ans}')
