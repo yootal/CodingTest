8761df5bd19ec449e96fc073dd5ffd07b7012fc1
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 02:51:47 +0900

    [D2] Title: 수도 요금 경쟁 D2 Attack, Time: 140 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
+++ SWEA/D2/1284. 수도 요금 경쟁 D2 Attack/수도 요금 경쟁 D2 Attack.py
@@ -1,9 +1,9 @@
-t  = int(input())
-for case in range(1,t+1):
-    p,q,r,s,w = map(int,input().split())
-    if w <= r:
-        B_cost = q
-    else:
-        B_cost = q + s * (w - r)
-    ans = min(p*w,B_cost)
+t = int(input())
+for case in range(1, t + 1):
+    p, q, r, s, w = map(int, input().split())
+    price1 = p * w
+    price2 = q
+    if r < w:
+        price2 += (w - r) * s
+    ans = min(price1, price2)
     print(f"#{case} {ans}")
