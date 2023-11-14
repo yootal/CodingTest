c9c59b4b37313544cf0bcac025629c5926230162
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 03:26:13 +0900

    [D2] Title: 조교의 성적 매기기, Time: 170 ms, Memory: 58,768 KB -BaekjoonHub

--- SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
+++ SWEA/D2/1983. 조교의 성적 매기기/조교의 성적 매기기.py
@@ -1,14 +1,15 @@
-t  = int(input())
-score = ['A+','A0','A-','B+','B0','B-','C+','C0','C-','D0']
-for case in range(1,t+1):
-    n,k = map(int,input().split())
-    student = []
-    for i in range(1,n+1):
-        a,b,c = map(int,input().split())
-        student.append((a*35 + b*45 + c*20,i))
-    student.sort(reverse=True)
-    gap = n // 10
-    for i in range(1,n+1):
-        if student[i][1] == k:
-            print(f"#{case} {score[i//gap]}")
-            break
+grade = ('A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D0')
+
+t = int(input())
+for case in range(1, t + 1):
+    n, k = map(int, input().split())
+    score = []
+    for i in range(1, n + 1):
+        a, b, c = map(int, input().split())
+        total = a * 35 + b * 45 + c * 20
+        if i == k:
+            check = total
+        score.append(total)
+    score.sort(reverse=True)
+    idx = score.index(check)
+    print(f'#{case} {grade[idx // (n // 10)]}')
