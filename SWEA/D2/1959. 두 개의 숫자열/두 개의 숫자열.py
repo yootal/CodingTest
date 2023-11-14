ee7e0178b340596bb087f2286f403cbf81fc0ad5
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 03:34:06 +0900

    [D2] Title: 두 개의 숫자열, Time: 148 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
+++ SWEA/D2/1959. 두 개의 숫자열/두 개의 숫자열.py
@@ -1,21 +1,15 @@
-t  = int(input())
-for case in range(1,t+1):
-    n,m = map(int,input().split())
-    l1 = list(map(int,input().split()))
-    l2 = list(map(int,input().split()))
-    
+t = int(input())
+for case in range(1, t + 1):
+    n, m = map(int, input().split())
+    num1 = list(map(int, input().split()))
+    num2 = list(map(int, input().split()))
     ans = 0
-    if n < m:
-        for i in range(m-n+1):
-            total = 0
-            for j in range(n):
-                total += l1[j] * l2[i+j]
-            ans = max(ans,total)
-    else:
-        for i in range(n-m+1):
-            total = 0
-            for j in range(m):
-                total += l2[j] * l1[i+j]
-            ans = max(ans,total)
-            
-    print(f"#{case} {ans}")
+    if n > m:
+        n, m = m, n
+        num1, num2 = num2, num1
+    for i in range(m - n + 1):
+        total = 0
+        for j in range(n):
+            total += num1[j] * num2[i + j]
+        ans = max(ans, total)
+    print(f'#{case} {ans}')
