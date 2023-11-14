39e995a697203d4371feabe1a8dd46a8484bef8b
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 00:16:14 +0900

    [D2] Title: 달팽이 숫자, Time: 145 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.py
@@ -1,24 +1,21 @@
 d = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
-
-def dfs(x, y, st, dir):
-    while 0 <= x < n and 0 <= y < n and matrix[x][y] == 0:
-        matrix[x][y] = st
-        st += 1
-        x += d[dir][0]
-        y += d[dir][1]
-        if st == n ** 2 + 1:
-            return
-    x = x - d[dir][0] + d[(dir + 1) % 4][0]
-    y = y - d[dir][1] + d[(dir + 1) % 4][1]
-    dfs(x, y, st, (dir + 1) % 4)
-
-
 t = int(input())
 for case in range(1, t + 1):
     n = int(input())
     matrix = [[0 for _ in range(n)] for _ in range(n)]
-    dfs(0, 0, 1, 0)
+    r, c = 0, 0
+    dist = 0
+    for x in range(1, n * n + 1):
+        matrix[r][c] = x
+        r += d[dist][0]
+        c += d[dist][1]
+        if r < 0 or r >= n or c < 0 or c >= n or matrix[r][c] != 0:
+            r -= d[dist][0]
+            c -= d[dist][1]
+            dist = (dist + 1) % 4
+            r += d[dist][0]
+            c += d[dist][1]
     print(f'#{case}')
     for row in matrix:
         print(*row)
