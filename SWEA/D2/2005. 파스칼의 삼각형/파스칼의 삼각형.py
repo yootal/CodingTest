5ac5a1ca7f414275e16f7a15df6a4a8cd3fabe60
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 02:08:29 +0900

    [D2] Title: 파스칼의 삼각형, Time: 142 ms, Memory: 58,528 KB -BaekjoonHub

--- SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
+++ SWEA/D2/2005. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -1,20 +1,15 @@
-def solve():
-    t = int(input())
-    for case in range(1, t + 1):
-        n = int(input())
-        ans = [[1]]
-        for i in range(2, n + 1):
-            row = [0] * i
-            for j in range(i):
-                if j == 0 or j == i - 1:
-                    row[j] = ans[i - 2][0]
-                else:
-                    row[j] = ans[i - 2][j - 1] + ans[i - 2][j]
-            ans.append(row)
-        print(f'#{case}')
-        for r in ans:
-            print(*r)
-
-
-if __name__ == '__main__':
-    solve()
+t = int(input())
+for case in range(1, t + 1):
+    n = int(input())
+    ans = [[1]]
+    for i in range(2, n + 1):
+        row = [0] * i
+        for j in range(i):
+            if j == 0 or j == i - 1:
+                row[j] = ans[i - 2][0]
+            else:
+                row[j] = ans[i - 2][j - 1] + ans[i - 2][j]
+        ans.append(row)
+    print(f'#{case}')
+    for r in ans:
+        print(*r)
