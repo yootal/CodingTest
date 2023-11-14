f893bd3091e6b49e3ecc5cb97bd2a1d600281678
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 00:52:14 +0900

    [D2] Title: 파리 퇴치, Time: 156 ms, Memory: 58,520 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,20 +1,13 @@
 t = int(input())
-for case in range(1,t+1):
-    n,m = map(int,input().split())
-    board = [list(map(int,input().split())) for _ in range(n)]
-    prefix_sum = [[0] * (n+1) for _ in range(n+1)]
-    
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            prefix_sum[i][j] = board[i-1][j-1]
-    
-    for i in range(1,n+1):
-        for j in range(1,n+1):
-            prefix_sum[i][j] = prefix_sum[i-1][j] + prefix_sum[i][j-1] - prefix_sum[i-1][j-1] + board[i-1][j-1]
-    
+for case in range(1, t + 1):
+    n, m = map(int, input().split())
+    matrix = [list(map(int, input().split())) for _ in range(n)]
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + matrix[i - 1][j - 1] - dp[i - 1][j - 1]
     ans = 0
-    for i in range(m,n+1):
-        for j in range(m,n+1):
-            ans = max(ans,prefix_sum[i][j]-prefix_sum[i-m][j]-prefix_sum[i][j-m]+prefix_sum[i-m][j-m])
-    
-    print(f"#{case} {ans}")
+    for i in range(m, n + 1):
+        for j in range(m, n + 1):
+            ans = max(ans, dp[i][j] - dp[i][j - m] - dp[i - m][j] + dp[i - m][j - m])
+    print(f'#{case} {ans}')
