537bce2f94c31ef0566d7c7ac908d5cf5efe337e
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 03:53:30 +0900

    [D2] Title: 숫자 배열 회전, Time: 154 ms, Memory: 58,528 KB -BaekjoonHub

--- SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
+++ SWEA/D2/1961. 숫자 배열 회전/숫자 배열 회전.py
@@ -1,27 +1,23 @@
-t  = int(input())
-for case in range(1,t+1):
+t = int(input())
+for case in range(1, t + 1):
     n = int(input())
-    matrix = [list(input().rstrip().split()) for _ in range(n)]
-    ans = [[] for _ in range(n)]
-    
+    matrix = [list(input().split()) for _ in range(n)]
+    ans = [[0] * 3 for _ in range(n)]
     for i in range(n):
-        num = ""
-        for j in range(n-1,-1,-1):
+        num = ''
+        for j in range(n - 1, -1, -1):
             num += matrix[j][i]
-        ans[0].append(num)
-    
-    for i in range(n-1,-1,-1):
-        num = ""
-        for j in range(n-1,-1,-1):
+        ans[i][0] = int(num)
+    for i in range(n):
+        num = ''
+        for j in range(n - 1, -1, -1):
             num += matrix[i][j]
-        ans[1].append(num)
-    
-    for i in range(n-1,-1,-1):
-        num = ""
+        ans[-i - 1][1] = num
+    for i in range(n - 1, -1, -1):
+        num = ''
         for j in range(n):
             num += matrix[j][i]
-        ans[2].append(num)
-        
-    print(f"#{case}")
-    for i in range(n):
-        print(ans[0][i],ans[1][i],ans[2][i])
+        ans[-i - 1][2] = num
+    print(f'#{case}')
+    for row in ans:
+        print(*row)
