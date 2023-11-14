da3c9060284735571c489aa9bfadd6ad553e2c6e
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 04:20:23 +0900

    [D2] Title: 날짜 계산기, Time: 141 ms, Memory: 58,272 KB -BaekjoonHub

--- SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
+++ SWEA/D2/1948. 날짜 계산기/날짜 계산기.py
@@ -1,15 +1,12 @@
-last = [31,28,31,30,31,30,31,31,30,31,30,31]
-t  = int(input())
-for case in range(1,t+1):
-    a1,a2,b1,b2 = map(int,input().split())
-    
-    ans = 0
-    if a1 == b1:
-        ans = b2 - a2 + 1
+day = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
+
+t = int(input())
+for case in range(1, t + 1):
+    m1, d1, m2, d2 = map(int, input().split())
+    if m1 == m2:
+        ans = d2 - d1 + 1
     else:
-        ans += last[a1-1] - a2 + 1
-        for i in range(a1,b1-1):
-            ans += last[i]
-        ans += b2
-        
-    print(f"#{case} {ans}")
+        ans = day[m1] - d1 + 1 + d2
+        for x in range(m1 + 1, m2):
+            ans += day[x]
+    print(f'#{case} {ans}')
