8c30adc3d0069feae7fe25f7112ead3e39b866ea
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 01:09:56 +0900

    [D2] Title: 어디에 단어가 들어갈 수 있을까, Time: 163 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
+++ SWEA/D2/1979. 어디에 단어가 들어갈 수 있을까/어디에 단어가 들어갈 수 있을까.py
@@ -8,21 +8,15 @@ for case in range(1, t + 1):
         for j in range(n):
             if board[i][j] == 1:
                 cnt += 1
-            else:
+            if board[i][j] == 0 or j == n - 1:
                 if cnt == k:
                     ans += 1
                 cnt = 0
-        if cnt == k:
-            ans += 1
-    for i in range(n):
-        cnt = 0
         for j in range(n):
             if board[j][i] == 1:
                 cnt += 1
-            else:
+            if board[j][i] == 0 or j == n - 1:
                 if cnt == k:
                     ans += 1
                 cnt = 0
-        if cnt == k:
-            ans += 1
     print(f'#{case} {ans}')
