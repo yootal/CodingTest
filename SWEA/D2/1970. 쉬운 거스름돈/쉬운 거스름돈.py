9dc7effd36fbf95544722665562cc84cc5ab8521
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 04:28:02 +0900

    [D2] Title: 쉬운 거스름돈, Time: 142 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.py
@@ -1,13 +1,11 @@
-change = [50000,10000,5000,1000,500,100,50,10]
-t  = int(input())
-for case in range(1,t+1):
+cost = [50000, 10000, 5000, 1000, 500, 100, 50, 10]
+t = int(input())
+for case in range(1, t + 1):
+    cost_cnt = {i: 0 for i in cost}
     n = int(input())
-    cnt = [0,0,0,0,0,0,0,0]
-    for i in range(8):
-        if n >= change[i]:
-            cnt[i] = n // change[i] 
-            n %= change[i]
-            if n == 0:
-                break 
+    for i in cost:
+        a, b = divmod(n, i)
+        cost_cnt[i] = a
+        n = b
     print(f"#{case}")
-    print(*cnt)
+    print(*list(cost_cnt.values()))
