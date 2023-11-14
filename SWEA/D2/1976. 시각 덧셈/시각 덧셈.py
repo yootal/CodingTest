5d1638f8cbd5350a21ca6a694de64aaad47eed2b
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 02:54:24 +0900

    [D2] Title: 시각 덧셈, Time: 142 ms, Memory: 58,764 KB -BaekjoonHub

--- SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
+++ SWEA/D2/1976. 시각 덧셈/시각 덧셈.py
@@ -1,11 +1,11 @@
-t  = int(input())
-for case in range(1,t+1):
-    a1,a2,b1,b2 = map(int,input().split())
-    h = a1 + b1
-    m = a2 + b2
-    if m >= 60:
-        m -= 60
+t = int(input())
+for case in range(1, t + 1):
+    h1, m1, h2, m2 = map(int, input().split())
+    h = h1 + h2
+    m = m1 + m2
+    if m > 60:
         h += 1
+        m -= 60
     if h > 12:
         h -= 12
     print(f"#{case} {h} {m}")
