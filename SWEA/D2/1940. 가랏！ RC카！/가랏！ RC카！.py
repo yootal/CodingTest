2b14b861f8e8655a6c30f3c89599c6d6a5c8f4d4
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 03:10:07 +0900

    [D2] Title: 가랏! RC카!, Time: 150 ms, Memory: 58,244 KB -BaekjoonHub

--- SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
+++ SWEA/D2/1940. 가랏！ RC카！/가랏！ RC카！.py
@@ -1,15 +1,15 @@
-t  = int(input())
-for case in range(1,t+1):
+t = int(input())
+for case in range(1, t + 1):
     n = int(input())
     v = 0
     ans = 0
-    for i in range(1,n+1):
-        inp = list(map(int,input().split()))
-        if inp[0] == 1:
-            v += inp[1]
-        elif inp[0] == 2:
-            v -= inp[1]
-        if v < 0:
-            v = 0
+    for _ in range(n):
+        c = list(map(int, input().split()))
+        if c[0] == 1:
+            v += c[1]
+        elif c[0] == 2:
+            v -= c[1]
+            if v < 0:
+                v = 0
         ans += v
-    print(f"#{case} {ans}")
+    print(f'#{case} {ans}')
