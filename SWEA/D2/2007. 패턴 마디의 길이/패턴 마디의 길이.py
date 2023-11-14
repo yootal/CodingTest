48f861557a2411d8d029704e15558aa067053c82
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 02:20:38 +0900

    [D2] Title: 패턴 마디의 길이, Time: 150 ms, Memory: 58,784 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.py
@@ -1,14 +1,7 @@
-t  = int(input())
-for case in range(1,t+1):
-    string = input()
-    ans = 0
-    for i in range(1,31):
-        if 2*i <= 31:
-            if string[:i] == string[i:2*i]:
-                ans = i
-                break
-        else:
-            if string[i:31] == string[:31-i]:
-                ans = i
-                break
-    print(f"#{case} {ans}")
+t = int(input())
+for test_case in range(1, t + 1):
+    s = input()
+    for i in range(1, 10):
+        if s[:i] == s[i:2 * i]:
+            print(f'#{test_case} {i}')
+            break
