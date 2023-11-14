66643ddc4d0980e12e2917d10978db0bc1a0ee7c
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 00:57:03 +0900

    [D2] Title: 파리 퇴치, Time: 158 ms, Memory: 58,248 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -2,13 +2,12 @@ t = int(input())
 for case in range(1, t + 1):
     n, m = map(int, input().split())
     matrix = [list(map(int, input().split())) for _ in range(n)]
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + matrix[i - 1][j - 1] - dp[i - 1][j - 1]
     ans = 0
-    for i in range(n - m + 1):
-        for j in range(n - m + 1):
-            _sum = 0
-            for k in range(m):
-                for l in range(m):
-                    _sum += matrix[i + k][j + l]
-            if _sum > ans:
-                ans = _sum
+    for i in range(m, n + 1):
+        for j in range(m, n + 1):
+            ans = max(ans, dp[i][j] - dp[i][j - m] - dp[i - m][j] + dp[i - m][j - m])
     print(f'#{case} {ans}')
