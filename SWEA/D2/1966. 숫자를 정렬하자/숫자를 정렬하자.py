c3a5bbe0697262b3a013729ade191fb807af9dae
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 04:34:49 +0900

    [D2] Title: 숫자를 정렬하자, Time: 162 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.py
@@ -1,6 +1,5 @@
-t  = int(input())
-for case in range(1,t+1):
+t = int(input())
+for case in range(1, t + 1):
     n = int(input())
-    num = list(map(int,input().split()))
-    print(f"#{case}",end=" ")
-    print(*sorted(num))
+    num = sorted(list(map(int, input().split())))
+    print(f"#{case}", *num)
