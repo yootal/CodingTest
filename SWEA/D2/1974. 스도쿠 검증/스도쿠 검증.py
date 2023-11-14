47c6fbd775c181dad768f1e239decbb93f333e32
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 01:21:40 +0900

    [D2] Title: 스도쿠 검증, Time: 171 ms, Memory: 58,268 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -1,47 +1,32 @@
-t  = int(input())
-for case in range(1,t+1):
-    board = [list(map(int,input().split())) for _ in range(9)]
-    
-    flag = True
-    
+def sudoku():
     for i in range(9):
-        if not flag:
-            break
-        cnt = [0,0,0,0,0,0,0,0,0]
+        vis1 = [False] * 9
+        vis2 = [False] * 9
         for j in range(9):
-            cnt[board[i][j]-1] += 1
-        for c in cnt:
-            if c != 1:
-                flag = False
-                break
-    
-    for i in range(9):
-        if not flag:
-            break
-        cnt = [0,0,0,0,0,0,0,0,0]
+            if vis1[board[i][j] - 1]:
+                return False
+            vis1[board[i][j] - 1] = True
+
         for j in range(9):
-            cnt[board[j][i]-1] += 1
-        for c in cnt:
-            if c != 1:
-                flag = False
-                break
-    
-    for i in range(0,9,3):
-        if not flag:
-            break
-        for j in range(0,9,3):
-            if not flag:
-                break
-            cnt = [0,0,0,0,0,0,0,0,0]
-            for i2 in range(i,i+3):
-                for j2 in range(j,j+3):
-                    cnt[board[i2][j2]-1] += 1
-            for c in cnt:
-                if c != 1:
-                    flag = False
-                    break
-    
-    if flag:
-        print(f"#{case} 1")
+            if vis2[board[j][i] - 1]:
+                return False
+            vis2[board[j][i] - 1] = True
+
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
+            vis3 = [False] * 9
+            for k in range(3):
+                for l in range(3):
+                    if vis3[board[i + k][j + l] - 1]:
+                        return False
+                    vis3[board[i + k][j + l] - 1] = True
+    return True
+
+
+t = int(input())
+for case in range(1, t + 1):
+    board = [list(map(int, input().split())) for _ in range(9)]
+    if sudoku():
+        print(f'#{case} 1')
     else:
-        print(f"#{case} 0")
+        print(f'#{case} 0')
