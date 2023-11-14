897f6c37302befb6acf4a4d940048c099ab18c7a
yootal <92325898+yootal@users.noreply.github.com>
2023-11-14 23:47:14 +0900

    [D2] Title: 백만 장자 프로젝트, Time: 1,166 ms, Memory: 250,484 KB -BaekjoonHub

--- SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
+++ SWEA/D2/1859. 백만 장자 프로젝트/백만 장자 프로젝트.py
@@ -1,15 +1,12 @@
 t = int(input())
-for case in range(1,t+1):
+for case in range(1, t + 1):
     n = int(input())
-    cost = list(map(int,input().split()))
-    
+    price = list(map(int, input().split()))
+    ans = 0
     max_cost = 0
-    profit = 0
-    
-    for i in range(n-1,-1,-1):
-        if cost[i] > max_cost:
-            max_cost = cost[i]
+    for i in range(n - 1, -1, -1):
+        if max_cost < price[i]:
+            max_cost = price[i]
         else:
-            profit += (max_cost - cost[i])
-    
-    print(f"#{case} {profit}")
+            ans += max_cost - price[i]
+    print(f'#{case} {ans}')
