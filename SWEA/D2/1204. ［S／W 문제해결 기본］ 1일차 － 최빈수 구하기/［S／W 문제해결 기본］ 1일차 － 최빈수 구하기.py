abe6d52e57e0e0cd38c006aa7087918e407f52ba
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 00:25:48 +0900

    [D2] Title: [S/W 문제해결 기본] 1일차 - 최빈수 구하기, Time: 195 ms, Memory: 60,236 KB -BaekjoonHub

--- SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
+++ SWEA/D2/1204. ［S／W 문제해결 기본］ 1일차 － 최빈수 구하기/［S／W 문제해결 기본］ 1일차 － 최빈수 구하기.py
@@ -5,5 +5,12 @@ for case in range(1, t + 1):
     n = int(input())
     num = list(map(int, input().split()))
     cnt = Counter(num)
-    num.sort(key=lambda x: cnt[x])
-    print(f'#{n} {num[-1]}')
+    max_cnt = 0
+    ans = 0
+    for k, v in cnt.items():
+        if max_cnt < v:
+            max_cnt = v
+            ans = k
+        elif max_cnt == v:
+            ans = max(ans, k)
+    print(f'#{n} {ans}')
