a32a0965c61e13d437940340042ab510863aaa8a
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 18:50:02 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 148 ms, Memory: 58,532 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.py
@@ -1,12 +1,12 @@
 t = int(input())
-for case in range(1,t+1):
-    inp = list(input().rstrip())
-    st = inp[0]
-    cnt = 0
-    for i in range(1,len(inp)):
-        if inp[i] != inp[i-1]:
-            cnt += 1
-            st = inp[i]
-    if inp[0] == '1':
-        cnt += 1
-    print(f"#{case} {cnt}")
+for case in range(1, t + 1):
+    ans = 0
+    n = list(map(int, input()))
+    st = n[0]
+    if st == 1:
+        ans += 1
+    for i in range(1, len(n)):
+        if n[i] != st:
+            st = n[i]
+            ans += 1
+    print(f"#{case} {ans}")
