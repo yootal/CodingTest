5cf8e475b34e47452eb00cca98bf169139040415
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 20:20:15 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - 회문2, Time: 547 ms, Memory: 67,244 KB -BaekjoonHub

--- SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
+++ SWEA/D3/1216. ［S／W 문제해결 기본］ 3일차 － 회문2/［S／W 문제해결 기본］ 3일차 － 회문2.py
@@ -1,24 +1,26 @@
-for case in range(1,11):
-    n = int(input())
-    board = [list(input().rstrip()) for _ in range(100)]
-    ans = 0
-    
+def palindrome(m, arr):
     for i in range(100):
-        for j in range(100):
-            for k in range(j,100):
-                word = ""
-                for l in range(j,k+1):
-                    word += board[i][l]
-                if word == word[::-1]:
-                    ans = max(ans,len(word))
-                 
-    for i in range(100):
-        for j in range(100):
-            for k in range(j,100):
-                word = ""
-                for l in range(j,k+1):
-                    word += board[l][i]
-                if word == word[::-1]:
-                    ans = max(ans,len(word))
-             
-    print(f"#{case} {ans}")
+        for j in range(0, 100 - m + 1):
+            if arr[i][j] == arr[i][j + m - 1]:
+                for k in range(1, m // 2):
+                    if arr[i][j + k] != arr[i][j + m - 1 - k]:
+                        break
+                else:
+                    return True
+        for j in range(0, 100 - m + 1):
+            if arr[j][i] == arr[j + m - 1][i]:
+                for k in range(1, m // 2):
+                    if arr[j + k][i] != arr[j + m - 1 - k][i]:
+                        break
+                else:
+                    return True
+    return False
+
+
+for case in range(1, 11):
+    N = int(input())
+    board = [list(input()) for _ in range(100)]
+    for ans in range(100, -1, -1):
+        if palindrome(ans, board):
+            print(f'#{case} {ans}')
+            break
