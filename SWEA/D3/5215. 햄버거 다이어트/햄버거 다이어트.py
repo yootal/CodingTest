eb03789a3066a929e5dfcd22ba20d6c0be9ec623
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 18:58:04 +0900

    [D3] Title: 햄버거 다이어트, Time: 243 ms, Memory: 64,624 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,16 +1,15 @@
 t = int(input())
-for case in range(1,t+1):
-    n,l = map(int,input().split())
-    material = [tuple(map(int,input().split())) for _ in range(n)]
-    dp = [[0] * (l+1) for _ in range(n+1)]
-
-    for i in range(1,n+1):
-        for j in range(1,l+1):
-            prefer = material[i-1][0]
-            calorie = material[i-1][1]
-            if j < calorie:
-                dp[i][j] = dp[i-1][j]
+for case in range(1, t + 1):
+    N, L = map(int, input().split())
+    data = [tuple(map(int, input().split())) for _ in range(N)]
+    dp = [[0] * (L + 1) for _ in range(N + 1)]
+    for i in range(1, N + 1):
+        score = data[i - 1][0]
+        calorie = data[i - 1][1]
+        for j in range(1, L + 1):
+            if calorie > j:
+                dp[i][j] = dp[i - 1][j]
             else:
-                dp[i][j] = max(prefer + dp[i-1][j-calorie],dp[i-1][j])  
-                  
-    print(f"#{case} {dp[n][l]}")
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - calorie] + score)
+    ans = dp[N][L]
+    print(f"#{case} {ans}")
