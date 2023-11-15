d670003cb88e470fbb760177d21e93851a36b5f3
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 18:46:11 +0900

    [D3] Title: 농작물 수확하기, Time: 231 ms, Memory: 64,352 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.py
@@ -1,27 +1,16 @@
-from collections import deque
-
 t = int(input())
-for case in range(1,t+1):
+for case in range(1, t + 1):
     n = int(input())
-    farm = [[int(x) for x in list(input().rstrip())] for _ in range(n)]
-    dist = [[-1] * n for _ in range(n)]
-    
-    limit = n//2
-    ans = farm[n//2][n//2]
-    
-    dq = deque()
-    dq.append((n//2,n//2))
-    dist[n//2][n//2] = 0
-    while dq:
-        x,y = dq.popleft()
-        for dx,dy in [(-1,0),(1,0),(0,-1),(0,1)]:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < n and 0 <= ny < n:
-                if dist[nx][ny] == -1:
-                    dist[nx][ny] = dist[x][y] + 1
-                    ans += farm[nx][ny]
-                    if dist[nx][ny] < limit:
-                        dq.append((nx,ny))
-                        
+    farm = [list(map(int, input())) for _ in range(n)]
+    ans = 0
+    s, e = n // 2, n // 2
+    for i in range(n):
+        for j in range(s, e + 1):
+            ans += farm[i][j]
+        if i < n // 2:
+            s -= 1
+            e += 1
+        else:
+            s += 1
+            e -= 1
     print(f"#{case} {ans}")
