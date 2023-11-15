623598958c178006048e825861258f8d45a8d1ec
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 19:06:27 +0900

    [D3] Title: 햄버거 다이어트, Time: 658 ms, Memory: 65,428 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,19 +1,20 @@
-from itertools import combinations
+def dfs(i, s, k):
+    global ans
+    if k > L:
+        return
+    if ans < s:
+        ans = s
+    if i == N:
+        return
+    score, kcal = data[i]
+    dfs(i + 1, s + score, k + kcal)
+    dfs(i + 1, s, k)
+
 
 t = int(input())
 for case in range(1, t + 1):
     N, L = map(int, input().split())
     data = [tuple(map(int, input().split())) for _ in range(N)]
     ans = 0
-    for i in range(1, N + 1):
-        for value in combinations(data, i):
-            calorie = 0
-            score = 0
-            for v in range(len(value)):
-                calorie += value[v][1]
-                score += value[v][0]
-            if calorie > L:
-                continue
-            if ans < score:
-                ans = score
+    dfs(0, 0, 0)
     print(f"#{case} {ans}")
