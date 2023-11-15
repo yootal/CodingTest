cc22c77e5e82c91d1a82a7dae2abdbdb79779c5a
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 20:08:39 +0900

    [D3] Title: 부분 수열의 합, Time: 682 ms, Memory: 70,952 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,29 +1,22 @@
-def back_tracking(arr):
+def check(st, total):
     global ans
-    
-    if arr: 
-        value = sum(map(lambda x: num[x],arr))
-        if value == k:
-            ans += 1
-            return
-        elif value > k:
-            return
-    
-    if not arr:
-        st = 0
-    else:
-        st = arr[-1] + 1
-    
-    for i in range(st,n):
-        if i not in arr:
-            arr.append(i)
-            back_tracking(arr)
-            arr.pop()
+    if total == k:
+        ans += 1
+        return
+    if total > k:
+        return
+    for i in range(st, n):
+        if not vis[i]:
+            vis[i] = True
+            check(i + 1, total + num[i])
+            vis[i] = False
+
 
 t = int(input())
-for case in range(1,t+1):
-    n,k = map(int,input().split())
-    num = list(map(int,input().split()))
+for case in range(1, t + 1):
+    n, k = map(int, input().split())
+    num = list(map(int, input().split()))
+    vis = [False] * n
     ans = 0
-    back_tracking([])
+    check(0, 0)
     print(f"#{case} {ans}")
