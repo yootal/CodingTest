ef8537525f9cd3bedb32470168bace10fbbc168b
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 19:04:01 +0900

    [D3] Title: 햄버거 다이어트, Time: 1,602 ms, Memory: 63,060 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,15 +1,19 @@
+from itertools import combinations
+
 t = int(input())
 for case in range(1, t + 1):
     N, L = map(int, input().split())
     data = [tuple(map(int, input().split())) for _ in range(N)]
-    dp = [[0] * (L + 1) for _ in range(N + 1)]
+    ans = 0
     for i in range(1, N + 1):
-        score = data[i - 1][0]
-        calorie = data[i - 1][1]
-        for j in range(1, L + 1):
-            if calorie > j:
-                dp[i][j] = dp[i - 1][j]
-            else:
-                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - calorie] + score)
-    ans = dp[N][L]
+        for value in combinations(data, i):
+            calorie = 0
+            score = 0
+            for v in range(len(value)):
+                calorie += value[v][1]
+                score += value[v][0]
+            if calorie > L:
+                continue
+            if ans < score:
+                ans = score
     print(f"#{case} {ans}")
