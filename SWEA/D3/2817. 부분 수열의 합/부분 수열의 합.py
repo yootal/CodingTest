ae9c0223452ca8616cc59fd46b28cc5d28bdf760
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 20:11:17 +0900

    [D3] Title: 부분 수열의 합, Time: 3,308 ms, Memory: 63,328 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.py
@@ -1,22 +1,13 @@
-def check(st, total):
-    global ans
-    if total == k:
-        ans += 1
-        return
-    if total > k:
-        return
-    for i in range(st, n):
-        if not vis[i]:
-            vis[i] = True
-            check(i + 1, total + num[i])
-            vis[i] = False
-
+from itertools import combinations
 
 t = int(input())
 for case in range(1, t + 1):
     n, k = map(int, input().split())
     num = list(map(int, input().split()))
-    vis = [False] * n
     ans = 0
-    check(0, 0)
-    print(f"#{case} {ans}")
+    for i in range(1, len(num) + 1):
+        for value in combinations(num, i):
+            if sum(value) == k:
+                ans += 1
+
+    print(f'#{case} {ans}')
