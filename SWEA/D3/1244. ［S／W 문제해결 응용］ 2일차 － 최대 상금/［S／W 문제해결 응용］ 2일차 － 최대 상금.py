2323e5668e85ab698d14974b306b5baf866fa2be
yootal <92325898+yootal@users.noreply.github.com>
2023-11-15 18:20:38 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 169 ms, Memory: 62,004 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,46 +1,17 @@
-def solve(arr,loop,idx,en,check):
-    global ans
-    if loop == 0:
-        x = ""
-        for item in arr:
-            x += str(item)
-        ans = max(ans,int(x))
-        return
-    
-    if check == True:
-        arr[idx],arr[idx-1] = arr[idx-1],arr[idx]
-        solve(arr,loop-1,idx,en,check)
-        return
-    
-    if idx+1 == en:
-        cnt = [0,0,0,0,0,0,0,0,0,0]
-        for x in arr:
-            cnt[x] += 1
-            if cnt[x] >= 2:
-                x = ""
-                for item in arr:
-                    x += str(item)
-                ans = max(ans,int(x))
-                return
-        arr[idx],arr[idx-1] = arr[idx-1],arr[idx]
-        solve(arr,loop-1,idx,en,True)
-        return
-    
-    _max = max(arr[idx+1:en])
-    if arr[idx] < _max:
-        for i in range(en-1,idx,-1):
-            if arr[i] == _max:
-                arr[idx],arr[i] = arr[i],arr[idx]
-                solve(arr,loop-1,idx+1,en,check)
-                arr[idx],arr[i] = arr[i],arr[idx]
-    else:
-        solve(arr,loop,idx+1,en,check)
-                
 t = int(input())
-for case in range(1,t+1):
-    inp = list(input().rstrip().split())
-    num = list(map(int,inp[0]))
-    loop = int(inp[1])
-    ans = 0
-    solve(num,loop,0,len(num),False)
-    print(f"#{case} {ans}")
+for case in range(1, t + 1):
+    num, k = input().split()
+    n = len(num)
+    k = int(k)
+    now = {num}
+    nxt = set()
+    for _ in range(k):
+        while now:
+            num2 = list(now.pop())
+            for i in range(n - 1):
+                for j in range(i + 1, n):
+                    num2[i], num2[j] = num2[j], num2[i]
+                    nxt.add(''.join(num2))
+                    num2[i], num2[j] = num2[j], num2[i]
+        now, nxt = nxt, now
+    print(f'#{case} {max(map(int, now))}')
