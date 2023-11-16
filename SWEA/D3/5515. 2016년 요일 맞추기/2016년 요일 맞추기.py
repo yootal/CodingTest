febd6a2f2970eadd8df1017b107d2b113d30700c
yootal <92325898+yootal@users.noreply.github.com>
2023-11-17 00:02:15 +0900

    [D3] Title: 2016년 요일 맞추기, Time: 382 ms, Memory: 73,460 KB -BaekjoonHub

--- SWEA/D3/5515. 2016년 요일 맞추기/2016년 요일 맞추기.py
+++ SWEA/D3/5515. 2016년 요일 맞추기/2016년 요일 맞추기.py
@@ -1,8 +1,10 @@
+from calendar import monthrange
+
 t = int(input())
-days = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days = [monthrange(2016, x)[1] for x in range(1, 13)]
 for case in range(1, t + 1):
     m, d = map(int, input().split())
-    day = d + 3
+    day = d + (4 - 1)
     for i in range(m - 1):
         day += days[i]
     print(f'#{case} {day % 7}')
