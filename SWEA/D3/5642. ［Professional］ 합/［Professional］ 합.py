ad21e3d4cbe205ddd603e1fcb88303e36f4251f0
yootal <92325898+yootal@users.noreply.github.com>
2023-11-16 23:43:33 +0900

    [D3] Title: [Professional] 합, Time: 535 ms, Memory: 120,616 KB -BaekjoonHub

--- SWEA/D3/5642. ［Professional］ 합/［Professional］ 합.py
+++ SWEA/D3/5642. ［Professional］ 합/［Professional］ 합.py
@@ -1,8 +1,8 @@
 t = int(input())
 for case in range(1, t + 1):
     n = int(input())
-    dp = list(map(int, input().split()))
+    num = list(map(int, input().split()))
     for i in range(1, n):
-        if dp[i] < dp[i - 1] + dp[i]:
-            dp[i] = dp[i - 1] + dp[i]
-    print(f'#{case} {max(dp)}')
+        if num[i - 1] + num[i] > num[i]:
+            num[i] = num[i - 1] + num[i]
+    print(f'#{case} {max(num)}')
