7abd99feca3a365178d4ded1813943e6a040178c
yootal <92325898+yootal@users.noreply.github.com>
2023-11-17 01:59:49 +0900

    [D3] Title: 문자열 교집합, Time: 1,851 ms, Memory: 211,680 KB -BaekjoonHub

--- SWEA/D3/2948. 문자열 교집합/문자열 교집합.py
+++ SWEA/D3/2948. 문자열 교집합/문자열 교집합.py
@@ -1,18 +1,9 @@
 t = int(input())
 for case in range(1, t + 1):
     n, m = map(int, input().split())
-    arr1 = sorted(list(input().split()))
-    arr2 = sorted(list(input().split()))
-    ans = 0
-    i1 = 0
-    i2 = 0
-    while i1 != len(arr1) and i2 != len(arr2):
-        if arr1[i1] == arr2[i2]:
-            ans += 1
-            i1 += 1
-            i2 += 1
-        elif arr1[i1] > arr2[i2] and i2 < len(arr2):
-            i2 += 1
-        elif arr1[i1] < arr2[i2] and i1 < len(arr1):
-            i1 += 1
+    total = n + m
+    nl = list(input().split())
+    ml = list(input().split())
+    all = nl + ml
+    ans = total - len(set(all))
     print(f'#{case} {ans}')
