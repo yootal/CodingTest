34e8af4c30d07b10d4bd245264903fe4b43aa4d4
yootal <92325898+yootal@users.noreply.github.com>
2023-11-16 22:46:29 +0900

    [D3] Title: 구구단 1, Time: 168 ms, Memory: 58,744 KB -BaekjoonHub

--- SWEA/D3/12004. 구구단 1/구구단 1.py
+++ SWEA/D3/12004. 구구단 1/구구단 1.py
@@ -1,13 +1,11 @@
 t = int(input())
 for case in range(1, t + 1):
     n = int(input())
-    flag = False
     for i in range(1, 10):
-        a = int(n / i)
+        a = n // i
         if 1 <= a <= 9 and a * i == n:
-            flag = True
             break
-    if flag:
-        print(f'#{case} Yes')
     else:
         print(f'#{case} No')
+        continue
+    print(f'#{case} Yes')
