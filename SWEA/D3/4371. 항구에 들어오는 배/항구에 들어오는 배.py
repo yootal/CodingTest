d2c3c34e26c3e6bfbe6f80f88ac3fbdc2b34672e
yootal <92325898+yootal@users.noreply.github.com>
2023-11-17 02:11:20 +0900

    [D3] Title: 항구에 들어오는 배, Time: 301 ms, Memory: 64,092 KB -BaekjoonHub

--- SWEA/D3/4371. 항구에 들어오는 배/항구에 들어오는 배.py
+++ SWEA/D3/4371. 항구에 들어오는 배/항구에 들어오는 배.py
@@ -1,22 +1,16 @@
-from collections import defaultdict
-
 t = int(input())
 for case in range(1, t + 1):
     n = int(input())
     happy = []
-    check = defaultdict(bool)
     for _ in range(n):
-        day = int(input())
-        happy.append(day)
-        check[day] = False
-    check[1] = True
+        happy.append(int(input()))
+    ships = set()
     ans = 0
     for i in range(1, n):
-        if not check[happy[i]]:
-            check[happy[i]] = True
-            ans += 1
-            gap = happy[i] - 1
-            for j in range(i + 1, n):
-                if (happy[j] - happy[i]) % gap == 0:
-                    check[happy[j]] = True
+        if happy[i] in ships:
+            continue
+        gap = happy[i] - 1
+        for j in range(1 + gap, happy[-1] + 1, gap):
+            ships.add(j)
+        ans += 1
     print(f'#{case} {ans}')
