c935c9a33619454bd71b93d9c08dc025ccab280e
yootal <92325898+yootal@users.noreply.github.com>
2023-11-16 22:25:48 +0900

    [D3] Title: 이진수 표현, Time: 1,347 ms, Memory: 65,792 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.py
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -1,17 +1,8 @@
 t = int(input())
 for case in range(1, t + 1):
     n, m = map(int, input().split())
-    ans = []
-    if m == 0:
-        ans.append('0')
-    while m != 0:
-        if m % 2:
-            ans.append('1')
-        else:
-            ans.append('0')
-        m //= 2
-    ans.reverse()
-    if ''.join(ans[-n:]) == '1' * n:
+    m = list(map(int, bin(m)[2:]))
+    if m[-n:].count(1) == n:
         print(f'#{case} ON')
     else:
         print(f'#{case} OFF')
