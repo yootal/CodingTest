4bc906c69cb5c19c6263d1aa5a269f917d804862
yootal <92325898+yootal@users.noreply.github.com>
2023-11-18 00:26:10 +0900

    [D3] Title: 세가지 합 구하기, Time: 1,266 ms, Memory: 65,132 KB -BaekjoonHub

--- SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
+++ SWEA/D3/3408. 세가지 합 구하기/세가지 합 구하기.py
@@ -1,7 +1,7 @@
 t = int(input())
 for case in range(1, t + 1):
     n = int(input())
-    if n % 2 == 0:
-        print(f'#{case} {(1 + n) * (n // 2)} {2 * n * (n // 2)} {2 * (n + 1) * (n // 2)}')
-    else:
-        print(f'#{case} {(1 + n) * (n // 2) + (n // 2 + 1)} {2 * n * (n // 2) + n} {2 * (n + 1) * (n // 2) + (n // 2 + 1) * 2}')
+    s1 = (n * (2 + (n - 1))) // 2
+    s2 = (n * (2 + (n - 1) * 2)) // 2
+    s3 = (n * (4 + (n - 1) * 2)) // 2
+    print(f'#{case} {s1} {s2} {s3}')
