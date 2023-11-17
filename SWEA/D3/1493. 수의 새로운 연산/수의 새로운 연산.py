8bf925e3b5bf38ddb1617cb5b5dc0df0ff213d4d
yootal <92325898+yootal@users.noreply.github.com>
2023-11-18 00:06:36 +0900

    [D3] Title: 수의 새로운 연산, Time: 366 ms, Memory: 65,932 KB -BaekjoonHub

--- SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.py
+++ SWEA/D3/1493. 수의 새로운 연산/수의 새로운 연산.py
@@ -1,31 +1,19 @@
-from collections import defaultdict
-
-
-def init():
-    cur = 1
-    x, y = 1, 1
-    nxt = 2
-    while y <= 300:
-        pos[cur] = (x, y)
-        num[(x, y)] = cur
-        cur += 1
-        y -= 1
-        if y == 0:
-            x = 1
-            y = nxt
-            nxt += 1
-            continue
-        x += 1
-
-
-pos = defaultdict(tuple)
-num = defaultdict(int)
-init()
+points = [(0, 0)] * 50001
+board = [[0] * 400 for _ in range(400)]
+x, y = 1, 1
+nxt = 2
+for i in range(1, 50001):
+    points[i] = (x, y)
+    board[x][y] = i
+    y -= 1
+    if y == 0:
+        x = 1
+        y = nxt
+        nxt += 1
+        continue
+    x += 1
 t = int(input())
 for case in range(1, t + 1):
     p, q = map(int, input().split())
-    pos_p = pos[p]
-    pos_q = pos[q]
-    temp = (pos_p[0] + pos_q[0], pos_p[1] + pos_q[1])
-    ans = num[temp]
-    print(f'#{case} {ans}')
+    after = (points[p][0] + points[q][0], points[p][1] + points[q][1])
+    print(f"#{case} {board[after[0]][after[1]]}")
