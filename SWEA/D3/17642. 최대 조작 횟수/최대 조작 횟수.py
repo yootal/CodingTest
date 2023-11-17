182b3244c5e760a3daf54e0c7861ae5fadf5c438
yootal <92325898+yootal@users.noreply.github.com>
2023-11-18 03:03:55 +0900

    [D3] Title: 최대 조작 횟수, Time: 151 ms, Memory: 58,760 KB -BaekjoonHub

--- SWEA/D3/17642. 최대 조작 횟수/최대 조작 횟수.py
+++ SWEA/D3/17642. 최대 조작 횟수/최대 조작 횟수.py
@@ -1,12 +1,7 @@
 t = int(input())
 for case in range(1, t + 1):
     a, b = map(int, input().split())
-    diff = abs(b - a)
-    if diff == 0:
-        print(f'#{case} 0')
-    elif a > b:
-        print(f'#{case} -1')
-    elif diff == 1:
+    if abs(a - b) == 1 or a > b:
         print(f'#{case} -1')
     else:
-        print(f'#{case} {diff // 2}')
+        print(f'#{case} {abs(a - b) // 2}')
