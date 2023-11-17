88607c13b48d409749506d8f438e61d54410194f
yootal <92325898+yootal@users.noreply.github.com>
2023-11-18 04:37:24 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 173 ms, Memory: 62,004 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.py
@@ -1,17 +1,18 @@
 t = int(input())
 for case in range(1, t + 1):
     num, k = input().split()
-    n = len(num)
+    num_len = len(num)
     k = int(k)
-    now = {num}
+    cur = {num}
     nxt = set()
     for _ in range(k):
-        while now:
-            num2 = list(now.pop())
-            for i in range(n - 1):
-                for j in range(i + 1, n):
-                    num2[i], num2[j] = num2[j], num2[i]
-                    nxt.add(''.join(num2))
-                    num2[i], num2[j] = num2[j], num2[i]
-        now, nxt = nxt, now
-    print(f'#{case} {max(map(int, now))}')
+        while cur:
+            num_list = list(cur.pop())
+            for i in range(num_len - 1):
+                for j in range(i + 1, num_len):
+                    num_list[i], num_list[j] = num_list[j], num_list[i]
+                    nxt.add(''.join(num_list))
+                    num_list[i], num_list[j] = num_list[j], num_list[i]
+        cur, nxt = nxt, cur
+    ans = max(cur)
+    print(f'#{case} {ans}')
