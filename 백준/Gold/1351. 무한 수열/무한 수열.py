bec8d831b68ff9da8eac25f3d23bf7266e403112
yootal <92325898+yootal@users.noreply.github.com>
2023-11-24 03:31:32 +0900

    [Gold V] Title: 무한 수열, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1351. 무한 수열/무한 수열.py
+++ 백준/Gold/1351. 무한 수열/무한 수열.py
@@ -2,14 +2,14 @@ import sys
 input = sys.stdin.readline
 
 n,p,q = map(int,input().split())
-dict = {}
-dict[0] = 1
+dp = {}
+dp[0] = 1
 
 def dfs(n):
-    if n in dict:
-        return dict[n]
+    if n in dp:
+        return dp[n]
     else:
-        dict[n] = dfs(n//p) + dfs(n//q)
-        return dict[n]
-    
+        dp[n] = dfs(n//p) + dfs(n//q)
+        return dp[n]
+
 print(dfs(n))
