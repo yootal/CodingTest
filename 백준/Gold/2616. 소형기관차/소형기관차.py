566c6deb9a5fd751ae6c1ff06791e5e786f72554
yootal <92325898+yootal@users.noreply.github.com>
2023-12-05 03:22:39 +0900

    [Gold III] Title: 소형기관차, Time: 132 ms, Memory: 35552 KB -BaekjoonHub

--- 백준/Gold/2616. 소형기관차/소형기관차.py
+++ 백준/Gold/2616. 소형기관차/소형기관차.py
@@ -5,9 +5,16 @@ n = int(input())
 num = list(map(int,input().split()))
 m = int(input())
 
+prefix = [0] + num[:]
+for i in range(2,n+1):
+    prefix[i] += prefix[i-1]
+
 dp = [[0] * (n+1) for _ in range(4)]
 for i in range(1,4):
     for j in range(m*i,n+1):
-        dp[i][j] = max(dp[i][j-1],dp[i-1][j-m] + sum(num[j-m:j]))
+        if i == 1:
+            dp[i][j] = max(dp[i][j-1],prefix[j]-prefix[j-m])
+        else:
+            dp[i][j] = max(dp[i][j-1],dp[i-1][j-m]+prefix[j]-prefix[j-m])
 
 print(dp[3][n])
