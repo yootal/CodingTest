85206758b08434fd7cb857701b6b9c73ede5b853
yootal <92325898+yootal@users.noreply.github.com>
2023-12-04 16:18:35 +0900

    [Gold I] Title: 할 일 정하기 1, Time: 804 ms, Memory: 120700 KB -BaekjoonHub

--- 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.py
+++ 백준/Gold/1311. 할 일 정하기 1/할 일 정하기 1.py
@@ -3,20 +3,19 @@ input = stdin.readline
 
 n = int(input())
 cost = [tuple(map(int,input().split())) for _ in range(n)]
-dp = [[maxsize] * (1 << n) for _ in range(n)]
+dp = [maxsize] * (1 << n)
 
 def solve(user,work_bitmask):
-    global ans
     if user == n:
         return 0
-    if dp[user][work_bitmask] != maxsize:
-        return dp[user][work_bitmask]
+    if dp[work_bitmask] != maxsize:
+        return dp[work_bitmask]
     c = maxsize
     for i in range(n):
         if work_bitmask & (1 << i):
             continue
         c = min(c,solve(user + 1, work_bitmask | (1 << i)) + cost[user][i])
-    dp[user][work_bitmask] = c
-    return dp[user][work_bitmask]
+    dp[work_bitmask] = c
+    return dp[work_bitmask]
 
 print(solve(0,0))
