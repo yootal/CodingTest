cfcc78955a579914c76eea72c7c3444b115398de
yootal <92325898+yootal@users.noreply.github.com>
2023-12-14 04:26:50 +0900

    [Platinum V] Title: 큰 수 만들기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
+++ 백준/Platinum/16496. 큰 수 만들기/큰 수 만들기.py
@@ -1,32 +1,15 @@
 from sys import stdin
 input = stdin.readline
 
-def check(x):
-    idx = len(x) - 1
-    value = 0
-    x_int = int(x)
-    flag = False
-    while x_int != 0:
-        w = (x_int % 10)
-        if w > int(x[0]):
-            flag = True
-        value += w * (10**-idx)
-        x_int //= 10
-        idx -= 1
-    if flag or len(x) == 1:
-        if int(x[0]) < int(x[-1]):
-            value += (int(x[-1])) * (10**-len(x))
-        else:
-            value += (int(x[-1])+1) * (10**-len(x))
-    else:
-        if int(x[0]) > int(x[-1]):
-            value += (int(x[-1])+1) * (10**-len(x))
-        else:
-            for i in range(len(x),10):
-                value += int(x[-1]) * (10**-i)
-    return round(value,9)
-
 n = int(input())
 num = list(input().rstrip().split())
-num.sort(key=lambda x: (check(x),len(x)),reverse=True)
-print(int(''.join(num)))
+compare = []
+for x in num:
+    temp = x[:]
+    l = len(temp)
+    while len(temp) < 10:
+        temp += temp[len(temp)-l]
+    compare.append([temp,x])
+compare.sort(key=lambda x: x[0], reverse=True)
+ans = ''.join([item[1] for item in compare])
+print(ans if int(ans) != 0 else 0)
