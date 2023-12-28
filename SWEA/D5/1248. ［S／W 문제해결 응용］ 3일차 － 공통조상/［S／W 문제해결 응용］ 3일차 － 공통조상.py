a82ef51d74a871135c7472ce69835d3650e4a25b
yootal <92325898+yootal@users.noreply.github.com>
2023-12-28 21:03:06 +0900

    [D5] Title: [S/W 문제해결 응용] 3일차 - 공통조상, Time: 260 ms, Memory: 66,548 KB -BaekjoonHub

--- SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.py
+++ SWEA/D5/1248. ［S／W 문제해결 응용］ 3일차 － 공통조상/［S／W 문제해결 응용］ 3일차 － 공통조상.py
@@ -1,30 +1,21 @@
 from collections import defaultdict
 
-def find(x):
+def cnt(x):
     if dp[x]:
         return dp[x]
     else:
-        total = 0
-        for nxt in child[x]:
-            total += find(nxt)
-        dp[x] = total
-        return dp[x]
-
-def cnt(x):
-    if cnt_dp[x]:
-        return cnt_dp[x]
-    else:
         if len(child[x]) == 0:
-            cnt_dp[x] = 1
+            dp[x] = 1
         else:
             total = 0
             for nxt in child[x]:
                 total += cnt(nxt)
-            cnt_dp[x] = total + 1
-        return cnt_dp[x]
+            dp[x] = total + 1
+        return dp[x]
     
 def search_parent(x):
-    if find(x) != 2:
+    if not check[x]:
+        check[x] = 1
         return search_parent(parent[x])
     else:
         return x
@@ -35,14 +26,15 @@ for case in range(1, t + 1):
     info = list(map(int,input().split()))
     child = defaultdict(list)
     parent = defaultdict(int)
+    check = [0] * (v+1)
     dp = [0] * (v+1)
-    dp[p1], dp[p2] = 1,1
-    cnt_dp = [0] * (v+1)
     for i in range(0,len(info),2):
         p = info[i]
         c = info[i+1]
         child[p].append(c)
         parent[c] = p
-    ans_p = search_parent(p1)
+    search_parent(p1)
+    ans_p = search_parent(p2)
     ans_cnt = cnt(ans_p)
     print(f'#{case} {ans_p} {ans_cnt}')
+            
