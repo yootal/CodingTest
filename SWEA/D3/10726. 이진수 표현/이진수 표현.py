7539467d0e1fa0e200b6a5760ebdfc39a8ff7cb7
yootal <92325898+yootal@users.noreply.github.com>
2024-01-27 15:40:02 +0900

    [D3] Title: 이진수 표현, Time: 1,286 ms, Memory: 67,256 KB -BaekjoonHub

--- SWEA/D3/10726. 이진수 표현/이진수 표현.py
+++ SWEA/D3/10726. 이진수 표현/이진수 표현.py
@@ -1,10 +1,25 @@
-t = int(input())
-for case in range(1, t + 1):
-    n, m = map(int, input().split())
-    for _ in range(n):
-        if m % 2 != 1:
-            print(f'#{case} OFF')
-            break
-        m //= 2
-    else:
-        print(f'#{case} ON')
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		for (int c = 1; c <= t; c++) {
+			st = new StringTokenizer(br.readLine());
+			int n = Integer.parseInt(st.nextToken());
+			int m = Integer.parseInt(st.nextToken());
+			if ((m & ((1 << n) - 1)) == ((1 << n) - 1)) {
+				sb.append("#").append(c).append(" ").append("ON").append("\n");
+			} else {
+				sb.append("#").append(c).append(" ").append("OFF").append("\n");
+			}
+		}
+		System.out.print(sb);
+	}
+
+}
