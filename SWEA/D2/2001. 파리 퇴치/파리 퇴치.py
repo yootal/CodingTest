0f5694437adafd4fc67e213f8bf55df043707858
yootal <92325898+yootal@users.noreply.github.com>
2024-02-01 09:47:33 +0900

    [D2] Title: 파리 퇴치, Time: 144 ms, Memory: 58,768 KB -BaekjoonHub

--- SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
+++ SWEA/D2/2001. 파리 퇴치/파리 퇴치.py
@@ -1,13 +1,45 @@
-t = int(input())
-for case in range(1, t + 1):
-    n, m = map(int, input().split())
-    matrix = [list(map(int, input().split())) for _ in range(n)]
-    dp = [[0] * (n + 1) for _ in range(n + 1)]
-    for i in range(1, n + 1):
-        for j in range(1, n + 1):
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] + matrix[i - 1][j - 1] - dp[i - 1][j - 1]
-    ans = 0
-    for i in range(m, n + 1):
-        for j in range(m, n + 1):
-            ans = max(ans, dp[i][j] - dp[i][j - m] - dp[i - m][j] + dp[i - m][j - m])
-    print(f'#{case} {ans}')
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static int[][] board;
+	static int n;
+	static int m;
+	static int ans;
+	static int sum;
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		for (int c = 1; c <= t; c++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			m = Integer.parseInt(st.nextToken());
+			board = new int[n][n];
+			for (int i = 0; i < n; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < n; j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			ans = 0;
+			for (int i = 0; i <= n - m; i++) {
+				for (int j = 0; j <= n - m; j++) {
+					sum = 0;
+					for (int i2 = i; i2 < i + m; i2++) {
+						for (int j2 = j; j2 < j + m; j2++) {
+							sum += board[i2][j2];
+						}
+					}
+					ans = Math.max(ans, sum);
+				}
+			}
+			sb.append("#").append(c).append(" ").append(ans).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+}
