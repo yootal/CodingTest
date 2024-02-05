203dbf88e1c52fc1449e881a4997aac8b84e99e4
yootal <92325898+yootal@users.noreply.github.com>
2024-02-05 16:41:04 +0900

    [D4] Title: 치즈 도둑, Time: 380 ms, Memory: 94,312 KB -BaekjoonHub

--- SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
+++ SWEA/D4/7733. 치즈 도둑/치즈 도둑.java
@@ -2,8 +2,11 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static final int[] dx = { 1, 0, -1, 0 };
-	static final int[] dy = { 0, -1, 0, 1 };
+	static final int[] dx = { 0, 1, 0, -1 };
+	static final int[] dy = { 1, 0, -1, 0 };
+	static int[][] cheese;
+	static boolean[][] v;
+	static int n;
 
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
@@ -12,8 +15,8 @@ public class Solution {
 		StringTokenizer st;
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			int n = Integer.parseInt(br.readLine());
-			int[][] cheese = new int[n][n];
+			n = Integer.parseInt(br.readLine());
+			cheese = new int[n][n];
 			for (int i = 0; i < n; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < n; j++) {
@@ -22,8 +25,6 @@ public class Solution {
 			}
 			int ans = 0;
 			int day = 0;
-			boolean[][] v;
-			Queue<int[]> q;
 			while (day <= 100) {
 				int cnt = 0;
 				v = new boolean[n][n];
@@ -31,22 +32,7 @@ public class Solution {
 					for (int j = 0; j < n; j++) {
 						if (cheese[i][j] > day && !v[i][j]) {
 							cnt++;
-							v[i][j] = true;
-							q = new ArrayDeque<>();
-							q.offer(new int[] { i, j });
-							while (!q.isEmpty()) {
-								int[] cur = q.poll();
-								for (int d = 0; d < 4; d++) {
-									int nx = cur[0] + dx[d];
-									int ny = cur[1] + dy[d];
-									if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
-										if (cheese[nx][ny] > day && !v[nx][ny]) {
-											v[nx][ny] = true;
-											q.offer(new int[] { nx, ny });
-										}
-									}
-								}
-							}
+							dfs(i, j, day);
 						}
 					}
 				}
@@ -60,4 +46,17 @@ public class Solution {
 		System.out.print(sb);
 	}
 
+	static void dfs(int x, int y, int day) {
+		v[x][y] = true;
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			if (nx >= 0 && nx < n && ny >= 0 && ny < n) {
+				if (cheese[nx][ny] > day && !v[nx][ny]) {
+					dfs(nx, ny, day);
+				}
+			}
+		}
+
+	}
 }
