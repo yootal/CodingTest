03ecdeca0ca6d531ec20830d01c9162aa881eb53
yootal <92325898+yootal@users.noreply.github.com>
2024-02-05 12:48:01 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 109 ms, Memory: 43,272 KB -BaekjoonHub

--- SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.py
@@ -1,19 +1,38 @@
-for case in range(1,11):
-    n = int(input())
-    original = list(map(int,input().split()))
-    c = int(input())
-    command = list(input().rstrip().split())
-    i = 0
-    count = 0
-    while count < c:
-        if command[i] == 'I':
-            idx = int(command[i+1])
-            cnt = int(command[i+2])
-            num = list(map(int,command[i+3:i+3+cnt]))
-            original = original[:idx] + num + original[idx:]
-            i = i + 3 + cnt
-            count += 1
-    print(f"#{case}",end=" ")
-    for i in range(10):
-        print(original[i],end=" ")
-    print()
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		ArrayList<Integer> al;
+		ArrayList<Integer> temp;
+		for (int tc = 1; tc <= 10; tc++) {
+			br.readLine();
+			al = new ArrayList<>();
+			st = new StringTokenizer(br.readLine(), " ");
+			while (st.hasMoreTokens())
+				al.add(Integer.parseInt(st.nextToken()));
+			br.readLine();
+			st = new StringTokenizer(br.readLine(), " ");
+			while (st.hasMoreTokens()) {
+				st.nextToken();
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				temp = new ArrayList<>();
+				for (int i = 0; i < y; i++) {
+					temp.add(Integer.parseInt(st.nextToken()));
+				}
+				al.addAll(x, temp);
+			}
+			sb.append("#").append(tc + " ");
+			for (int i = 0; i < 10; i++) {
+				sb.append(al.get(i) + " ");
+			}
+			sb.append("\n");
+		}
+		System.out.print(sb);
+	}
+}
