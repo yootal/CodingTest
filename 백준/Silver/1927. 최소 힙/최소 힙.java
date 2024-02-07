3590f2d5f84b8e4641c5bbf0a0ec0027fa9f6b56
yootal <92325898+yootal@users.noreply.github.com>
2024-02-07 12:23:52 +0900

    [Silver II] Title: 최소 힙, Time: 348 ms, Memory: 26696 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int n = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		for (int i = 0; i < n; i++) {
+			int num = Integer.parseInt(br.readLine());
+			if (num == 0) {
+				if (!pq.isEmpty()) {
+					sb.append(pq.poll()).append("\n");
+				} else
+					sb.append(0).append("\n");
+			} else {
+				pq.offer(num);
+			}
+		}
+		System.out.println(sb);
+	}
+}
