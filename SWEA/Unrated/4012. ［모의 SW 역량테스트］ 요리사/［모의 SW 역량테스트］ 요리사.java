47c9084442a9865fe7e4dc6c7ba433223960d27f
yootal <92325898+yootal@users.noreply.github.com>
2024-02-10 14:53:44 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 314 ms, Memory: 22,248 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,56 +1,70 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Solution {
-    static int N;
-    static int ans;
-    static int[][] synergy;
-    static boolean[] vis;
- 
-    public static void main(String args[]) throws Exception {
-        //System.setIn(new FileInputStream("res/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuilder sb = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= T; tc++) {
-            N = Integer.parseInt(br.readLine());
-            synergy = new int[N][N];
-            vis = new boolean[N];
-            ans = Integer.MAX_VALUE;
-            for (int i = 0; i < N; i++) {
-                st = new StringTokenizer(br.readLine(), " ");
-                for (int j = 0; j < N; j++) {
-                    synergy[i][j] = Integer.parseInt(st.nextToken());
-                }
-            }
-            calc(0, 0);
-            sb.append("#").append(tc).append(" ").append(ans).append("\n");
-        }
-        System.out.print(sb);
-    }
- 
-    static void calc(int depth, int idx) {
-        if (depth == N / 2) {
-            int sum1 = 0, sum2 = 0;
-            for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (vis[i] && vis[j])
-                        sum1 += synergy[i][j];
-                    else if (!vis[i] && !vis[j])
-                        sum2 += synergy[i][j];
-                }
-            }
-            ans = Math.min(ans, Math.abs(sum1 - sum2));
-            return;
-        }
- 
-        for (int i = idx; i < N; i++) {
-            if (!vis[i]) {
-                vis[i] = true;
-                calc(depth + 1, i + 1);
-                vis[i] = false;
-            }
-        }
-    }
+	static int N;
+	static int[][] synergy;
+	static int[] member = new int[2];
+	static int ans;
+	static boolean[] team;
+	static int team1;
+	static int team2;
+
+	public static void main(String args[]) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			team = new boolean[N];
+			synergy = new int[N][N];
+			ans = Integer.MAX_VALUE;
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < N; j++) {
+					synergy[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			makeTeam(0, 0);
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+	static void makeTeam(int cnt, int start) {
+		if (cnt == N / 2) {
+			team1 = 0;
+			team2 = 0;
+			comb(0, 0, true);
+			comb(0, 0, false);
+			ans = Math.min(ans, Math.abs(team1 - team2));
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			team[i] = true;
+			makeTeam(cnt + 1, i + 1);
+			team[i] = false;
+		}
+	}
+
+	static void comb(int cnt, int start, boolean check) {
+		if (cnt == 2) {
+			if (check) {
+				team1 += synergy[member[0]][member[1]] + synergy[member[1]][member[0]];
+			} else {
+				team2 += synergy[member[0]][member[1]] + synergy[member[1]][member[0]];
+			}
+			return;
+		}
+		for (int i = start; i < N; i++) {
+			if (team[i] != check)
+				continue;
+			member[cnt] = i;
+			comb(cnt + 1, i + 1, check);
+		}
+
+	}
+
 }
