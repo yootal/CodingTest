d25d966c67b9ad050e4bafea2a7c62e5a235dc9e
yootal <92325898+yootal@users.noreply.github.com>
2024-02-10 15:15:43 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 247 ms, Memory: 22,368 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -3,12 +3,9 @@ import java.util.*;
 
 public class Solution {
 	static int N;
-	static int[][] synergy;
-	static int[] member = new int[2];
 	static int ans;
-	static boolean[] team;
-	static int team1;
-	static int team2;
+	static int[][] synergy;
+	static boolean[] vis;
 
 	public static void main(String args[]) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
@@ -18,8 +15,8 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
-			team = new boolean[N];
 			synergy = new int[N][N];
+			vis = new boolean[N];
 			ans = Integer.MAX_VALUE;
 			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
@@ -27,44 +24,33 @@ public class Solution {
 					synergy[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
-			makeTeam(0, 0);
+			calc(0, 0);
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 		}
 		System.out.print(sb);
 	}
 
-	static void makeTeam(int cnt, int start) {
-		if (cnt == N / 2) {
-			team1 = 0;
-			team2 = 0;
-			comb(0, 0, true);
-			comb(0, 0, false);
-			ans = Math.min(ans, Math.abs(team1 - team2));
-			return;
-		}
-		for (int i = start; i < N; i++) {
-			team[i] = true;
-			makeTeam(cnt + 1, i + 1);
-			team[i] = false;
-		}
-	}
-
-	static void comb(int cnt, int start, boolean check) {
-		if (cnt == 2) {
-			if (check) {
-				team1 += synergy[member[0]][member[1]] + synergy[member[1]][member[0]];
-			} else {
-				team2 += synergy[member[0]][member[1]] + synergy[member[1]][member[0]];
+	static void calc(int depth, int idx) {
+		if (depth == N / 2) {
+			int sum1 = 0, sum2 = 0;
+			for (int i = 0; i < N - 1; i++) {
+				for (int j = i + 1; j < N; j++) {
+					if (vis[i] && vis[j])
+						sum1 += synergy[i][j] + synergy[j][i];
+					else if (!vis[i] && !vis[j])
+						sum2 += synergy[i][j] + synergy[j][i];
+				}
 			}
+			ans = Math.min(ans, Math.abs(sum1 - sum2));
 			return;
 		}
-		for (int i = start; i < N; i++) {
-			if (team[i] != check)
-				continue;
-			member[cnt] = i;
-			comb(cnt + 1, i + 1, check);
+
+		for (int i = idx; i < N; i++) {
+			if (!vis[i]) {
+				vis[i] = true;
+				calc(depth + 1, i + 1);
+				vis[i] = false;
+			}
 		}
-
 	}
-
 }
