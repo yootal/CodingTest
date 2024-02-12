c11902404b692bca8543dfdb192011e77a9b55f2
yootal <92325898+yootal@users.noreply.github.com>
2024-02-12 16:23:08 +0900

    [D3] Title: 햄버거 다이어트, Time: 146 ms, Memory: 29,704 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.java
@@ -14,23 +14,34 @@ public class Solution {
 			int N = Integer.parseInt(st.nextToken());
 			int L = Integer.parseInt(st.nextToken());
 			int[][] dp = new int[N + 1][L + 1];
-			int[] score = new int[N + 1];
-			int[] calorie = new int[N + 1];
-			for (int i = 1; i <= N; i++) {
+			Buger[] buger = new Buger[N];
+			for (int i = 0; i < N; i++) {
 				st = new StringTokenizer(br.readLine(), " ");
-				score[i] = Integer.parseInt(st.nextToken());
-				calorie[i] = Integer.parseInt(st.nextToken());
+				int score = Integer.parseInt(st.nextToken());
+				int calorie = Integer.parseInt(st.nextToken());
+				buger[i] = new Buger(score, calorie);
 			}
 			for (int i = 1; i <= N; i++) {
-				for (int j = 1; j < calorie[i]; j++) {
-					dp[i][j] = dp[i - 1][j];
-				}
-				for (int j = calorie[i]; j <= L; j++) {
-					dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - calorie[i]] + score[i]);
+				Buger cur = buger[i - 1];
+				for (int j = 1; j <= L; j++) {
+					if (j < cur.calorie)
+						dp[i][j] = dp[i - 1][j];
+					else
+						dp[i][j] = Math.max(dp[i - 1][j], dp[i - 1][j - cur.calorie] + cur.score);
 				}
 			}
 			sb.append("#").append(tc).append(" ").append(dp[N][L]).append("\n");
 		}
 		System.out.print(sb);
+	}
+
+	static class Buger {
+		int score, calorie;
+
+		public Buger(int score, int calorie) {
+			this.score = score;
+			this.calorie = calorie;
+		}
+
 	}
 }
