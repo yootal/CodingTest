835c3a8fbc546bfc2ae2743bd6c33e8de839b790
yootal <92325898+yootal@users.noreply.github.com>
2024-02-19 01:16:14 +0900

    [Gold IV] Title: 이차원 배열과 연산, Time: 172 ms, Memory: 18668 KB -BaekjoonHub

--- 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.java
+++ 백준/Gold/17140. 이차원 배열과 연산/이차원 배열과 연산.java
@@ -2,10 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	static int[][] arr;
-	static int row_length;
-	static int col_length;
-	static HashMap<Integer, Integer> countMap;
+	static int arr[][], row_length, col_length, cnt[];
+	static ArrayList<Cnt> al = new ArrayList<>();
 
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
@@ -43,27 +41,26 @@ public class Main {
 	static void operR() { // R연산
 		int update_col_length = 0; // 가장 큰 크기로 갱신을 위한 변수
 		for (int i = 0; i < row_length; i++) {
-			countMap = new HashMap<>(); // 등장 횟수를 저장할 HashMap
+			cnt = new int[101]; // 등장 횟수를 저장할 배열
 			for (int j = 0; j < col_length; j++) {
 				if (arr[i][j] == 0) // 0은 무시한다
 					continue;
-				if (countMap.containsKey(arr[i][j])) { // 이미 카운팅하고 있는 수면 갯수를 1 올려주고
-					countMap.put(arr[i][j], countMap.get(arr[i][j]) + 1);
-				} else { // 없다면 1로 초기화
-					countMap.put(arr[i][j], 1);
-				}
+				cnt[arr[i][j]]++;
 				arr[i][j] = 0; // 배열을 0으로 초기화 해줘야한다
-
 			}
-			List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(countMap.entrySet()); // 객체의 집합 ArrayList로
-			entryList.sort(Comparator.comparing(Map.Entry<Integer, Integer>::getValue).thenComparing(Map.Entry::getKey));
+			al.clear(); // 카운팅한 수만 모아서 정렬
+			for (int j = 1; j < 101; j++) {
+				if (cnt[j] > 0)
+					al.add(new Cnt(j, cnt[j]));
+			}
+			Collections.sort(al);
 			// 갯수로 먼저 비교하고 같으면 숫자 비교
-			int size = entryList.size() * 2; // 수, 수의 갯수를 기록하기 때문에 사이즈는 2배
+			int size = al.size() * 2; // 수, 수의 갯수를 기록하기 때문에 사이즈는 2배
 			if (size > 100)
 				size = 100; // 100까지만 쓴다
 			for (int j = 0; j < size; j += 2) {
-				arr[i][j] = entryList.get(j / 2).getKey();
-				arr[i][j + 1] = entryList.get(j / 2).getValue();
+				arr[i][j] = al.get(j / 2).idx;
+				arr[i][j + 1] = al.get(j / 2).cnt;
 			}
 			update_col_length = Math.max(size, update_col_length); // 가장 큰 크기를 기록
 		}
@@ -73,29 +70,43 @@ public class Main {
 	static void operC() {
 		int update_row_length = 0;
 		for (int j = 0; j < col_length; j++) {
-			countMap = new HashMap<>();
+			cnt = new int[101];
 			for (int i = 0; i < row_length; i++) {
 				if (arr[i][j] == 0)
 					continue;
-				if (countMap.containsKey(arr[i][j])) {
-					countMap.put(arr[i][j], countMap.get(arr[i][j]) + 1);
-				} else {
-					countMap.put(arr[i][j], 1);
-				}
+				cnt[arr[i][j]]++;
 				arr[i][j] = 0;
-
 			}
-			List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(countMap.entrySet());
-			entryList.sort(Comparator.comparing(Map.Entry<Integer, Integer>::getValue).thenComparing(Map.Entry::getKey));
-			int size = entryList.size() * 2;
+			al.clear();
+			for (int i = 1; i < 101; i++) {
+				if (cnt[i] > 0)
+					al.add(new Cnt(i, cnt[i]));
+			}
+			Collections.sort(al);
+			int size = al.size() * 2;
 			if (size > 100)
 				size = 100;
 			for (int i = 0; i < size; i += 2) {
-				arr[i][j] = entryList.get(i / 2).getKey();
-				arr[i + 1][j] = entryList.get(i / 2).getValue();
+				arr[i][j] = al.get(i / 2).idx;
+				arr[i + 1][j] = al.get(i / 2).cnt;
 			}
 			update_row_length = Math.max(size, update_row_length);
 		}
 		row_length = update_row_length;
+	}
+
+	static class Cnt implements Comparable<Cnt> {
+		int idx, cnt;
+
+		public Cnt(int idx, int cnt) {
+			this.idx = idx;
+			this.cnt = cnt;
+		}
+
+		@Override
+		public int compareTo(Cnt o) {
+			return cnt == o.cnt ? idx - o.idx : cnt - o.cnt;
+		}
+
 	}
 }
