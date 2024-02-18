dbd10f60f75d184e567d391f6aa6d5e0f41197ba
yootal <92325898+yootal@users.noreply.github.com>
2024-02-18 16:29:16 +0900

    [Gold IV] Title: 키 순서, Time: 848 ms, Memory: 43352 KB -BaekjoonHub

---
+++ 백준/Gold/2458. 키 순서/키 순서.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N, arr[][], vis[], upCnt, downCnt, num;
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		arr = new int[N + 1][N + 1];
+		vis = new int[N + 1];
+		for (int i = 0; i < M; i++) {
+			st = new StringTokenizer(br.readLine());
+			// a < b
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			arr[a][b] = 1;
+		}
+		int ans = 0;
+		for (int i = 1; i <= N; i++) {
+			upCnt = 0;
+			downCnt = 0;
+			num = i;
+			countUp(i);
+			num = -i;
+			countDown(i);
+			if (upCnt + downCnt == N - 1)
+				ans++;
+		}
+		System.out.print(ans);
+	}
+
+	static void countUp(int x) {
+		vis[x] = num;
+		Queue<Integer> q = new ArrayDeque<>();
+		q.offer(x);
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			for (int i = 1; i <= N; i++) {
+				if (vis[i] != num && arr[i][cur] == 1) {
+					vis[i] = num;
+					q.offer(i);
+					upCnt++;
+				}
+			}
+		}
+	}
+
+	static void countDown(int x) {
+		vis[x] = num;
+		Queue<Integer> q = new ArrayDeque<>();
+		q.offer(x);
+		while (!q.isEmpty()) {
+			int cur = q.poll();
+			for (int i = 1; i <= N; i++) {
+				if (vis[i] != num && arr[cur][i] == 1) {
+					vis[i] = num;
+					q.offer(i);
+					downCnt++;
+				}
+			}
+		}
+	}
+}
