d56a06650a0368201f328851cfa263158c3aee07
yootal <92325898+yootal@users.noreply.github.com>
2024-02-19 00:28:12 +0900

    [D4] Title: [Professional] 키 순서, Time: 1,966 ms, Memory: 92,392 KB -BaekjoonHub

--- SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
+++ SWEA/D4/5643. ［Professional］ 키 순서/［Professional］ 키 순서.java
@@ -12,15 +12,15 @@ public class Solution {
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
-			arr = new int[N + 1][N + 1];
-			vis = new int[N + 1];
+			arr = new int[N + 1][N + 1]; // 인접 행렬에 기록
+			vis = new int[N + 1]; // 카운팅 한 번만 하기 위한 방문 배열
 			int M = Integer.parseInt(br.readLine());
 			for (int i = 0; i < M; i++) {
 				st = new StringTokenizer(br.readLine());
 				// a < b
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
-				arr[a][b] = 1;
+				arr[a][b] = 1; // 연결 표시
 			}
 			int ans = 0;
 			for (int i = 1; i <= N; i++) {
@@ -30,6 +30,7 @@ public class Solution {
 				countUp(i);
 				num = -i;
 				countDown(i);
+				// N-1 -> 몇명보다 크고 작은지 다 셀 수 있으면 답 + 1
 				if (upCnt + downCnt == N - 1)
 					ans++;
 			}
@@ -38,7 +39,17 @@ public class Solution {
 		System.out.print(sb);
 	}
 
-	static void countUp(int x) {
+	static void countUp(int x) { // DFS
+		vis[x] = num;
+		for (int i = 1; i <= N; i++) {
+			if (vis[i] != num && arr[x][i] == 1) {
+				countUp(i);
+				upCnt++;
+			}
+		}
+	}
+
+	static void countDown(int x) { // BFS
 		vis[x] = num;
 		Queue<Integer> q = new ArrayDeque<>();
 		q.offer(x);
@@ -46,22 +57,6 @@ public class Solution {
 			int cur = q.poll();
 			for (int i = 1; i <= N; i++) {
 				if (vis[i] != num && arr[i][cur] == 1) {
-					vis[i] = num;
-					q.offer(i);
-					upCnt++;
-				}
-			}
-		}
-	}
-
-	static void countDown(int x) {
-		vis[x] = num;
-		Queue<Integer> q = new ArrayDeque<>();
-		q.offer(x);
-		while (!q.isEmpty()) {
-			int cur = q.poll();
-			for (int i = 1; i <= N; i++) {
-				if (vis[i] != num && arr[cur][i] == 1) {
 					vis[i] = num;
 					q.offer(i);
 					downCnt++;
