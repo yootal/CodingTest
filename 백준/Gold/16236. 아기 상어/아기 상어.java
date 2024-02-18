d10cfb88e002992a1b6f5542c96a0af0b0563f25
yootal <92325898+yootal@users.noreply.github.com>
2024-02-18 23:34:22 +0900

    [Gold III] Title: 아기 상어, Time: 184 ms, Memory: 20600 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -6,7 +6,7 @@ public class Main {
 	static final int[] dy = { -1, 0, 1, 0 };
 	static int N, time, area[][];
 	static boolean vis[][];
-	static Shark s;
+	static Shark s; // 상어
 
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
@@ -18,14 +18,14 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 0; j < N; j++) {
 				area[i][j] = Integer.parseInt(st.nextToken());
-				if (area[i][j] == 9) {
+				if (area[i][j] == 9) { // 상어 좌표 기록하고 맵에 표시 지우기
 					s = new Shark(i, j, 2);
 					area[i][j] = 0;
 				}
 			}
 		}
 		while (true) {
-			if (!bfs())
+			if (!bfs()) // 먹을 물고기 없을 때까지 반복
 				break;
 		}
 		System.out.println(time);
@@ -37,18 +37,18 @@ public class Main {
 		vis[s.x][s.y] = true;
 		int minDist = Integer.MAX_VALUE;
 		int[] minPos = new int[2];
-		q.offer(new int[] { s.x, s.y, 0 });
+		q.offer(new int[] { s.x, s.y, 0 }); // 상어 좌표와 거리
 		while (!q.isEmpty()) {
 			int[] cur = q.poll();
 			for (int d = 0; d < 4; d++) {
 				int nx = cur[0] + dx[d];
 				int ny = cur[1] + dy[d];
 				if (nx >= 0 && nx < N && ny >= 0 && ny < N && !vis[nx][ny]) {
-					if (area[nx][ny] > 0) {
-						if (area[nx][ny] == s.size) {
+					if (area[nx][ny] > 0) { // 물고기가 있는 곳
+						if (area[nx][ny] == s.size) { // 상어 크기와 같으면 지나가기만
 							vis[nx][ny] = true;
 							q.offer(new int[] { nx, ny, cur[2] + 1 });
-						} else if (area[nx][ny] < s.size) {
+						} else if (area[nx][ny] < s.size) { // 상어 크기보다 작은 것 중 가깝고 위쪽, 왼쪽 선정
 							vis[nx][ny] = true;
 							if (minDist > cur[2] + 1) {
 								minDist = cur[2] + 1;
@@ -62,14 +62,14 @@ public class Main {
 							}
 							q.offer(new int[] { nx, ny, cur[2] + 1 });
 						}
-					} else {
+					} else { // 물고기 없으면 그냥 거리만 + 1 
 						vis[nx][ny] = true;
 						q.offer(new int[] { nx, ny, cur[2] + 1 });
 					}
 				}
 			}
 		}
-		if (minDist != Integer.MAX_VALUE) {
+		if (minDist != Integer.MAX_VALUE) { // 물고기가 있으면 먹고 거리 카운팅 후 먹었다 표시
 			area[minPos[0]][minPos[1]] = 0;
 			s.x = minPos[0];
 			s.y = minPos[1];
@@ -81,7 +81,7 @@ public class Main {
 			time += minDist;
 			return true;
 		}
-		return false;
+		return false; // 물고기 없으면 못먹었어요~
 	}
 
 	static class Shark {
