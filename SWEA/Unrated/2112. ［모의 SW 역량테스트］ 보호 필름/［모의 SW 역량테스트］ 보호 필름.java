a96b182075aae7295c9e00bed4bf4a87bbf50027
yootal <92325898+yootal@users.noreply.github.com>
2024-02-20 09:32:41 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 315 ms, Memory: 25,172 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -2,8 +2,8 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int D, W, K, R, ans;
-	static int[][] film;
+	static int D, W, K, ans;
+	static int[][] film, temp;
 
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
@@ -17,19 +17,16 @@ public class Solution {
 			W = Integer.parseInt(st.nextToken());
 			K = Integer.parseInt(st.nextToken());
 			film = new int[D][W];
+			temp = new int[D][W];
 			for (int i = 0; i < D; i++) {
 				st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < W; j++) {
 					film[i][j] = Integer.parseInt(st.nextToken());
+					temp[i][j] = film[i][j];
 				}
 			}
-			ans = -1;
-			for (int i = 0; i <= K; i++) {
-				R = i;
-				dfs(0, 0);
-				if (ans != -1)
-					break;
-			}
+			ans = K;
+			dfs(0, 0);
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 		}
 		System.out.print(sb);
@@ -38,10 +35,10 @@ public class Solution {
 	static boolean check() {
 		boolean flag;
 		for (int j = 0; j < W; j++) {
-			int state = -1;
-			int cnt = 0;
+			int state = film[0][j];
+			int cnt = 1;
 			flag = false;
-			for (int i = 0; i < D; i++) {
+			for (int i = 1; i < D; i++) {
 				if (state != film[i][j]) {
 					state = film[i][j];
 					cnt = 1;
@@ -58,24 +55,28 @@ public class Solution {
 	}
 
 	static void dfs(int cnt, int idx) {
-		if (R - cnt > D - idx)
+		if (cnt >= ans)
 			return;
-		if (cnt == R) {
+
+		if (idx == D) {
 			if (check()) {
-				ans = R;
+				ans = Math.min(ans, cnt);
 			}
 			return;
 		}
-		int[] temp = film[idx];
 
-		film[idx] = new int[W];
-		Arrays.fill(film[idx], 1);
-		dfs(cnt + 1, idx + 1);
-
-		Arrays.fill(film[idx], 0);
-		dfs(cnt + 1, idx + 1);
-
-		film[idx] = temp;
 		dfs(cnt, idx + 1);
+
+		for (int i = 0; i < W; i++)
+			film[idx][i] = 1;
+		dfs(cnt + 1, idx + 1);
+
+		for (int i = 0; i < W; i++)
+			film[idx][i] = 0;
+		dfs(cnt + 1, idx + 1);
+
+		for (int i = 0; i < W; i++)
+			film[idx][i] = temp[idx][i];
+
 	}
 }
