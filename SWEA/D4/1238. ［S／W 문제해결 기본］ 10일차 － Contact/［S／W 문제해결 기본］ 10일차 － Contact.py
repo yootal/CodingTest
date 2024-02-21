82cf582fc0fd8611cef10a98ecd5d808b7c432a5
yootal <92325898+yootal@users.noreply.github.com>
2024-02-22 08:41:30 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 188 ms, Memory: 59,836 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.py
@@ -1,30 +1,50 @@
-from collections import deque, defaultdict
+import java.io.*;
+import java.util.*;
 
+public class Solution {
 
-def bfs(x):
-    dq = deque()
-    dq.append((x, 0))
-    while dq:
-        cur, d = dq.popleft()
-        for nxt in list(graph[cur]):
-            if nxt not in vis:
-                vis.add(nxt)
-                dq.append((nxt, d + 1))
-                depth[d + 1].append(nxt)
-
-
-for case in range(1, 11):
-    graph = defaultdict(set)
-    depth = defaultdict(list)
-    vis = set()
-    n, st = map(int, input().split())
-    inp = list(map(int, input().split()))
-    for i in range(0, n, 2):
-        graph[inp[i]].add(inp[i + 1])
-    vis.add(st)
-    bfs(st)
-    max_key = 0
-    for k in depth.keys():
-        max_key = max(max_key, k)
-    ans = max(depth[max_key])
-    print(f'#{case} {ans}')
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		boolean[][] arr;
+		boolean[] vis;
+		for (int tc = 1; tc < 11; tc++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int S = Integer.parseInt(st.nextToken());
+			arr = new boolean[101][101];
+			vis = new boolean[101];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N / 2; i++) {
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				arr[a][b] = true;
+			}
+			int depth = 0;
+			int ans = 0;
+			Queue<int[]> q = new ArrayDeque<>();
+			q.offer(new int[] { S, 0 });
+			vis[S] = true;
+			while (!q.isEmpty()) {
+				int[] cur = q.poll();
+				if (depth < cur[1]) {
+					depth = cur[1];
+					ans = 0;
+				}
+				if (depth == cur[1]) {
+					ans = Math.max(ans, cur[0]);
+				}
+				for (int i = 1; i < 101; i++) {
+					if (!vis[i] && arr[cur[0]][i]) {
+						vis[i] = true;
+						q.offer(new int[] { i, cur[1] + 1 });
+					}
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.print(sb);
+	}
+}
