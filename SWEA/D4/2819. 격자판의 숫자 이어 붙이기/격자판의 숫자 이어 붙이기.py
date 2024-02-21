083fc3a700ff68e54ff02d80e83d0593580147b8
yootal <92325898+yootal@users.noreply.github.com>
2024-02-21 14:19:26 +0900

    [D4] Title: 격자판의 숫자 이어 붙이기, Time: 311 ms, Memory: 68,580 KB -BaekjoonHub

--- SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
+++ SWEA/D4/2819. 격자판의 숫자 이어 붙이기/격자판의 숫자 이어 붙이기.py
@@ -1,20 +1,51 @@
-def dfs(x, y, depth, arr):
-    global ans
-    if depth == 7:
-        ans.add(arr)
-        return
-    for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
-        nx = x + dx
-        ny = y + dy
-        if 0 <= nx < 4 and 0 <= ny < 4:
-            dfs(nx, ny, depth + 1, arr + board[nx][ny])
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+	static final int[] dx = { -1, 0, 1, 0 };
+	static final int[] dy = { 0, 1, 0, -1 };
+	static String num[], board[][];
+	static HashSet<String> set = new HashSet<>();
 
-t = int(input())
-for case in range(1, t + 1):
-    ans = set()
-    board = [list(input().split()) for _ in range(4)]
-    for i in range(4):
-        for j in range(4):
-            dfs(i, j, 1, f'{board[i][j]}')
-    print(f'#{case} {len(ans)}')
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= T; tc++) {
+			set.clear();
+			board = new String[4][4];
+			num = new String[7];
+			for (int i = 0; i < 4; i++) {
+				st = new StringTokenizer(br.readLine(), " ");
+				for (int j = 0; j < 4; j++) {
+					board[i][j] = st.nextToken();
+				}
+			}
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					dfs(new int[] { i, j }, 0);
+				}
+			}
+			sb.append("#").append(tc).append(" ").append(set.size()).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+	static void dfs(int[] p, int cnt) {
+		if (cnt == 7) {
+			String x = String.join("", num);
+			set.add(x);
+			return;
+		}
+		for (int d = 0; d < 4; d++) {
+			int nx = p[0] + dx[d];
+			int ny = p[1] + dy[d];
+			if (nx >= 0 && nx < 4 && ny >= 0 && ny < 4) {
+				num[cnt] = board[nx][ny];
+				dfs(new int[] { nx, ny }, cnt + 1);
+			}
+		}
+	}
+}
