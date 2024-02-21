5901c990cef07d2893e0c0fd597948f3b008bf31
yootal <92325898+yootal@users.noreply.github.com>
2024-02-22 08:23:13 +0900

    [D4] Title: 최소 스패닝 트리, Time: 2,176 ms, Memory: 116,240 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -2,37 +2,45 @@ import java.io.*;
 import java.util.*;
 
 public class Solution {
-	static int[] parent;
 
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
+		ArrayList<Node>[] graph;
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
 			st = new StringTokenizer(br.readLine());
 			int V = Integer.parseInt(st.nextToken());
 			int E = Integer.parseInt(st.nextToken());
-			parent = new int[V + 1];
+			graph = new ArrayList[V + 1];
 			for (int i = 1; i <= V; i++) {
-				parent[i] = i;
+				graph[i] = new ArrayList<>();
 			}
-			PriorityQueue<Edge> pq = new PriorityQueue<>();
 			for (int i = 0; i < E; i++) {
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int v = Integer.parseInt(st.nextToken());
-				pq.offer(new Edge(a, b, v));
+				graph[a].add(new Node(b, v));
+				graph[b].add(new Node(a, v));
 			}
+			boolean[] vis = new boolean[V + 1];
+			PriorityQueue<Node> pq = new PriorityQueue<>();
+			pq.offer(new Node(1, 0));
 			int cnt = 0;
 			long ans = 0;
-			while (cnt < V - 1) {
-				Edge cur = pq.poll();
-				if (union(cur.s, cur.e)) {
-					ans += cur.v;
-					cnt++;
+			while (cnt < V) {
+				Node cur = pq.poll();
+				if (vis[cur.nxt])
+					continue;
+				vis[cur.nxt] = true;
+				ans += cur.v;
+				cnt++;
+				for (Node nxt : graph[cur.nxt]) {
+					if (!vis[nxt.nxt])
+						pq.offer(nxt);
 				}
 			}
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
@@ -40,32 +48,16 @@ public class Solution {
 		System.out.print(sb);
 	}
 
-	static int find(int cur) {
-		if (cur != parent[cur])
-			parent[cur] = find(parent[cur]);
-		return parent[cur];
-	}
+	static class Node implements Comparable<Node> {
+		int nxt, v;
 
-	static boolean union(int x, int y) {
-		x = find(x);
-		y = find(y);
-		if (x == y)
-			return false;
-		parent[y] = x;
-		return true;
-	}
-
-	static class Edge implements Comparable<Edge> {
-		int s, e, v;
-
-		public Edge(int s, int e, int v) {
-			this.s = s;
-			this.e = e;
+		public Node(int nxt, int v) {
+			this.nxt = nxt;
 			this.v = v;
 		}
 
 		@Override
-		public int compareTo(Edge o) {
+		public int compareTo(Node o) {
 			return v - o.v;
 		}
 
