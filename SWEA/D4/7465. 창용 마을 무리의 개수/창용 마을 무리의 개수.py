630aaaab051f7b7cab08c32881d441276473f640
yootal <92325898+yootal@users.noreply.github.com>
2024-02-21 10:27:18 +0900

    [D4] Title: 창용 마을 무리의 개수, Time: 267 ms, Memory: 64,200 KB -BaekjoonHub

--- SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
+++ SWEA/D4/7465. 창용 마을 무리의 개수/창용 마을 무리의 개수.py
@@ -1,29 +1,55 @@
-from collections import defaultdict, deque
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+	static int parent[];
 
-def bfs(cur):
-    vis[cur] = True
-    dq = deque([cur])
-    while dq:
-        x = dq.popleft()
-        for nxt in graph[x]:
-            if not vis[nxt]:
-                vis[nxt] = True
-                dq.append(nxt)
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			parent = new int[N + 1];
+			for (int i = 1; i <= N; i++) {
+				parent[i] = i;
+			}
+			sb.append("#").append(tc).append(" ");
+			for (int i = 0; i < M; i++) {
+				st = new StringTokenizer(br.readLine());
+				int a = Integer.parseInt(st.nextToken());
+				int b = Integer.parseInt(st.nextToken());
+				union(a, b);
+			}
+			boolean[] check = new boolean[N + 1];
+			int ans = 0;
+			for (int i = 1; i <= N; i++) {
+				if (!check[find(i)]) {
+					check[find(i)] = true;
+					ans++;
+				}
+			}
+			sb.append(ans).append("\n");
+		}
+		System.out.print(sb);
+	}
 
+	static int find(int x) {
+		if (x != parent[x]) {
+			parent[x] = find(parent[x]);
+		}
+		return parent[x];
+	}
 
-t = int(input())
-for case in range(1, t + 1):
-    n, m = map(int, input().split())
-    graph = defaultdict(list)
-    vis = [False] * (n + 1)
-    for _ in range(m):
-        s, e = map(int, input().split())
-        graph[s].append(e)
-        graph[e].append(s)
-    ans = 0
-    for i in range(1, n + 1):
-        if not vis[i]:
-            ans += 1
-            bfs(i)
-    print(f'#{case} {ans}')
+	static void union(int x, int y) {
+		x = find(x);
+		y = find(y);
+		if (x != y) {
+			parent[y] = x;
+		}
+	}
+}
