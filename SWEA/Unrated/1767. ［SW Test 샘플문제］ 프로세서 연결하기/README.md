c0f64c69bc9483d8e0dac605bd20fa1b2cce241c
yootal <92325898+yootal@users.noreply.github.com>
2024-02-22 11:42:40 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 1,886 ms, Memory: 39,840 KB -BaekjoonHub

---
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [SW Test 샘플문제] 프로세서 연결하기 - 1767 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV4suNtaXFEDFAUf) 
+
+### 성능 요약
+
+메모리: 39,840 KB, 시간: 1,886 ms, 코드길이: 2,667 Bytes
+
+### 제출 일자
+
+2024-02-22 11:42
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -0,0 +1,119 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	static final int[] dx = { -1, 0, 1, 0 };
+	static final int[] dy = { 0, 1, 0, -1 };
+	static int N, maxCnt, ans;
+	static int board[][];
+	static ArrayList<Core> core;
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			board = new int[N][N];
+			core = new ArrayList<>();
+			for (int i = 0; i < N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					board[i][j] = Integer.parseInt(st.nextToken());
+					if (board[i][j] == 1) {
+						if (i > 0 && i < N - 1 && j > 0 && j < N - 1)
+							core.add(new Core(i, j, false));
+					}
+				}
+			}
+			maxCnt = 0;
+			ans = Integer.MAX_VALUE;
+			solve(0);
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+	static class Core {
+		int x, y;
+		boolean power;
+
+		public Core(int x, int y, boolean power) {
+			this.x = x;
+			this.y = y;
+			this.power = power;
+		}
+
+	}
+
+	static void solve(int cnt) {
+		if (cnt == core.size()) {
+			int onCnt = 0;
+			for (Core c : core) {
+				if (c.power)
+					onCnt++;
+			}
+			if (maxCnt < onCnt) {
+				maxCnt = onCnt;
+				ans = Integer.MAX_VALUE;
+			}
+			if (maxCnt == onCnt) {
+				ans = Math.min(ans, countLink());
+			}
+			return;
+		}
+		Core cur = core.get(cnt);
+		for (int d = 0; d < 4; d++) {
+			if (link(cur.x, cur.y, d, cnt + 1)) {
+				cur.power = true;
+				solve(cnt + 1);
+				clear(cur.x, cur.y, d, cnt + 1);
+				cur.power = false;
+			} else {
+				clear(cur.x, cur.y, d, cnt + 1);
+				solve(cnt + 1);
+			}
+		}
+	}
+
+	static boolean link(int x, int y, int d, int idx) {
+		if (board[x][y] != 1) {
+			board[x][y] -= idx;
+		}
+		if (x == 0 || x == N - 1 || y == 0 || y == N - 1) {
+			return true;
+		}
+		int nx = x + dx[d];
+		int ny = y + dy[d];
+		if (nx >= 0 && nx < N && ny >= 0 && ny < N && board[nx][ny] == 0) {
+			return link(nx, ny, d, idx);
+		} else {
+			return false;
+		}
+	}
+
+	static void clear(int x, int y, int d, int idx) {
+		if (board[x][y] != 1) {
+			board[x][y] += idx;
+		}
+		int nx = x + dx[d];
+		int ny = y + dy[d];
+		if (nx >= 0 && nx < N && ny >= 0 && ny < N && board[nx][ny] == -idx) {
+			clear(nx, ny, d, idx);
+		}
+	}
+
+	static int countLink() {
+		int linkCnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				if (board[i][j] < 0) {
+					linkCnt++;
+				}
+			}
+		}
+		return linkCnt;
+	}
+}
