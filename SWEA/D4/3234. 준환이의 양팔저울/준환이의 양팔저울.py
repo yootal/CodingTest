1af4a9f33828616f28d42cacf1bddf879e9696e4
yootal <92325898+yootal@users.noreply.github.com>
2024-02-22 16:49:34 +0900

    [D4] Title: 준환이의 양팔저울, Time: 3,018 ms, Memory: 70,996 KB -BaekjoonHub

--- SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.py
+++ SWEA/D4/3234. 준환이의 양팔저울/준환이의 양팔저울.py
@@ -1,29 +1,62 @@
-from math import factorial
+import java.io.*;
+import java.util.*;
 
+public class Solution {
+	static int N, sum, ans;
+	static int[] num;
+	static boolean[] v;
+	static int dp[];
 
-def back_tracking(left, right, cnt):
-    global ans
-    if cnt == n:
-        ans += 1
-        return
-    if left > sum_w // 2:
-        ans += (2 ** (n - cnt)) * factorial(n - cnt)
-        return
-    for i in range(n):
-        if not vis[i]:
-            vis[i] = True
-            back_tracking(left + w[i], right, cnt + 1)
-            if left >= right + w[i]:
-                back_tracking(left, right + w[i], cnt + 1)
-            vis[i] = False
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		dp = new int[10];
+		dp[1] = 1;
+		factorial(9);
+		for (int tc = 1; tc <= T; tc++) {
+			N = Integer.parseInt(br.readLine());
+			num = new int[N];
+			v = new boolean[N];
+			sum = 0;
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				num[i] = Integer.parseInt(st.nextToken());
+				sum += num[i];
+			}
+			ans = 0;
+			solve(0, 0, 0);
+			sb.append("#").append(tc).append(" ").append(ans).append("\n");
+		}
+		System.out.print(sb);
+	}
 
+	static void solve(int left, int right, int cnt) {
+		if (cnt == N) {
+			ans++;
+			return;
+		}
+		if (left > sum / 2) {
+			ans += Math.pow(2, N - cnt) * dp[N - cnt];
+			return;
+		}
+		for (int i = 0; i < N; i++) {
+			if (v[i])
+				continue;
+			v[i] = true;
+			solve(left + num[i], right, cnt + 1);
+			if (left >= right + num[i]) {
+				solve(left, right + num[i], cnt + 1);
+			}
+			v[i] = false;
+		}
+	}
 
-t = int(input())
-for case in range(1, t + 1):
-    n = int(input())
-    w = list(map(int, input().split()))
-    sum_w = sum(w)
-    vis = [False] * n
-    ans = 0
-    back_tracking(0, 0, 0)
-    print(f'#{case} {ans}')
+	static int factorial(int x) {
+		if (x == 1)
+			return dp[1];
+		return dp[x] = x * factorial(x - 1);
+	}
+}
