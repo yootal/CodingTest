5bbb89de4ad780d3003c9b10e8c30d1c32039818
yootal <92325898+yootal@users.noreply.github.com>
2024-02-23 10:15:27 +0900

    [Gold II] Title: 색종이 붙이기, Time: 168 ms, Memory: 15960 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -4,13 +4,15 @@ import java.util.*;
 public class Main {
 	static int ans = Integer.MAX_VALUE;
 	static int extra[], paper[][], total;
+	static boolean check[][];
 
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		paper = new int[10][10]; // 종이 상태
+		paper = new int[10][10]; // 종이
 		extra = new int[] { 0, 5, 5, 5, 5, 5 }; // 사용할 수 있는 종이 수
+		check = new boolean[10][10]; // 붙인 상태 기록
 		total = 0; // 전체 1 수
 		for (int i = 0; i < 10; i++) {
 			st = new StringTokenizer(br.readLine());
@@ -37,7 +39,7 @@ public class Main {
 		for (int x = row; x < 10; x++) { // 전에 붙인 종이 다음부터 시작
 			for (int y = col; y < 10; y++) {
 				col = 0; // 다음 반복부터는 0부터
-				if (paper[x][y] == 1) {
+				if (paper[x][y] == 1 && !check[x][y]) {
 					label: for (int k = 5; k > 0; k--) { // 1일 때 붙일 색종이 크기 반복문돌려서 찾는다.
 
 						if (extra[k] == 0 || x + k > 10 || y + k > 10)
@@ -45,16 +47,14 @@ public class Main {
 
 						for (int i = x; i < x + k; i++) {
 							for (int j = y; j < y + k; j++) {
-								if (paper[i][j] != 1)
+								if (paper[i][j] != 1 || check[i][j])
 									continue label; // 1이 아닌 칸이 존재하면 바로 다음으로 작은 색종이 확인
 							}
 						}
 
-						int[][] temp = new int[k][k]; // 백트래킹시 돌아갈 상태 기록
-						for (int i = x; i < x + k; i++) {
+						for (int i = x; i < x + k; i++) { // 백트래킹시 돌아갈 상태 기록
 							for (int j = y; j < y + k; j++) {
-								temp[i - x][j - y] = paper[i][j];
-								paper[i][j] = -1;
+								check[i][j] = true;
 							}
 						}
 
@@ -64,10 +64,9 @@ public class Main {
 
 						for (int i = x; i < x + k; i++) { // 돌아와서 원 상태로 복구
 							for (int j = y; j < y + k; j++) {
-								paper[i][j] = temp[i - x][j - y];
+								check[i][j] = false;
 							}
 						}
-
 						extra[k]++; // 복구했으니 사용했던 색종이 수 다시 +1
 					}
 					return; // 3. 색종이 못붙이면 바로 리턴, 이거 안넣으면 종이 다돔
