768eb8c25566c89dda7b0ba867d6c567b60e8104
yootal <92325898+yootal@users.noreply.github.com>
2024-02-27 16:04:35 +0900

    [Gold IV] Title: 최단경로, Time: 656 ms, Memory: 104352 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,64 +2,59 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+	static class Node implements Comparable<Node> {
+		int vertex, weight;
+		Node link;
 
-	public static void main(String args[]) throws Exception {
-		//System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stt = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(stt.nextToken());
-		int E = Integer.parseInt(stt.nextToken());
-		int st = Integer.parseInt(br.readLine());
-		int[] sdt = new int[V + 1];
-		ArrayList<Edge>[] graph = new ArrayList[V + 1];
-		for (int i = 0; i <= V; i++) {
-			graph[i] = new ArrayList<Edge>();
-		}
-		for (int i = 0; i < E; i++) {
-			stt = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(stt.nextToken());
-			int v = Integer.parseInt(stt.nextToken());
-			int w = Integer.parseInt(stt.nextToken());
-			graph[u].add(new Edge(v, w));
-		}
-		for (int i = 1; i <= V; i++) {
-			sdt[i] = Integer.MAX_VALUE;
-		}
-		sdt[st] = 0;
-		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
-		pq.offer(new Edge(st, sdt[st]));
-		while (!pq.isEmpty()) {
-			Edge cur = pq.poll();
-			if (cur.value != sdt[cur.vertex])
-				continue;
-			for (Edge nxt : graph[cur.vertex]) {
-				if (sdt[nxt.vertex] > cur.value + nxt.value) {
-					sdt[nxt.vertex] = cur.value + nxt.value;
-					pq.offer(new Edge(nxt.vertex, sdt[nxt.vertex]));
-				}
-			}
-		}
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i <= V; i++) {
-			sb.append(sdt[i] == Integer.MAX_VALUE ? "INF" : sdt[i]).append("\n");
-		}
-		System.out.print(sb);
-	}
-
-	static class Edge implements Comparable<Edge> {
-		int vertex, value;
-
-		Edge(int vertex, int value) {
+		public Node(int vertex, int weight, Node link) {
+			super();
 			this.vertex = vertex;
-			this.value = value;
+			this.weight = weight;
+			this.link = link;
 		}
 
 		@Override
-		public int compareTo(Edge e) {
-			if (this.value > e.value)
-				return 1;
-			else
-				return -1;
+		public int compareTo(Node o) {
+			return weight - o.weight;
 		}
+
+	}
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(br.readLine());
+		Node[] nodeList = new Node[V + 1];
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine());
+			int s = Integer.parseInt(st.nextToken());
+			int e = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			nodeList[s] = new Node(e, v, nodeList[s]);
+		}
+		int[] dist = new int[V + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+		dist[K] = 0;
+		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
+		pq.offer(new int[] { K, dist[K] });
+		while (!pq.isEmpty()) {
+			int[] cur = pq.poll();
+			if (dist[cur[0]] != cur[1])
+				continue;
+			for (Node nxt = nodeList[cur[0]]; nxt != null; nxt = nxt.link) {
+				if (dist[nxt.vertex] > dist[cur[0]] + nxt.weight) {
+					dist[nxt.vertex] = dist[cur[0]] + nxt.weight;
+					pq.offer(new int[] { nxt.vertex, dist[nxt.vertex] });
+				}
+			}
+		}
+		for (int i = 1; i <= V; i++) {
+			sb.append(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]).append("\n");
+		}
+		System.out.print(sb);
 	}
 }
