a45a8476652a661dfa4f6e95701313d3bb1bdd12
yootal <92325898+yootal@users.noreply.github.com>
2024-02-27 16:24:36 +0900

    [Silver III] Title: 2×n 타일링, Time: 124 ms, Memory: 14456 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[1001];
+		dp[1] = 1;
+		dp[2] = 2;
+		dp[3] = 3;
+		for (int i = 4; i <= N; i++) {
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+		}
+		System.out.println(dp[N] % 10007);
+	}
+}
