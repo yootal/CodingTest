1c72101cb275970d143a684803f5e5d592a4aebb
yootal <92325898+yootal@users.noreply.github.com>
2024-02-28 11:40:31 +0900

    [Silver V] Title: 다리 놓기, Time: 152 ms, Memory: 14628 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		int[][] dp = new int[31][31];
+		for (int i = 0; i <= 30; i++) {
+			for (int j = 0; j <= i; j++) {
+				if (j == 0 || j == i)
+					dp[i][j] = 1;
+				else
+					dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+			}
+		}
+		for (int i = 0; i < T; i++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int M = Integer.parseInt(st.nextToken());
+			System.out.println(dp[M][N]);
+		}
+	}
+}
