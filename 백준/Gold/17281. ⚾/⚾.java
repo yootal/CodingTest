6d8e999a3a530c105786924c4e072d64802e06aa
yootal <92325898+yootal@users.noreply.github.com>
2024-02-28 09:19:59 +0900

    [Gold IV] Title: ⚾, Time: 728 ms, Memory: 18168 KB -BaekjoonHub

---
+++ 백준/Gold/17281. ⚾/⚾.java
@@ -0,0 +1,86 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int[][] play;
+	static int[] seq;
+	static int[] pos;
+	static boolean[] v;
+	static int N, playerIdx, ans;
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		N = Integer.parseInt(br.readLine());
+		play = new int[N][9];
+		seq = new int[9];
+		pos = new int[9];
+		v = new boolean[9];
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 9; j++) {
+				play[i][j] = Integer.parseInt(st.nextToken());
+			}
+		}
+		seq[3] = 0;
+		v[0] = true;
+		ans = 0;
+		perm(0);
+		System.out.println(ans);
+	}
+
+	static void perm(int cnt) {
+		if (cnt == 3) {
+			perm(cnt + 1);
+			return;
+		}
+		if (cnt == 9) {
+			ans = Math.max(ans, game());
+			return;
+		}
+		for (int i = 1; i < 9; i++) {
+			if (v[i])
+				continue;
+			v[i] = true;
+			seq[cnt] = i;
+			perm(cnt + 1);
+			v[i] = false;
+		}
+	}
+
+	static int game() {
+		playerIdx = 0;
+		int score = 0;
+		int outCnt = 0;
+		int turn = 0;
+		while (turn != N) {
+			for (int i = 0; i < 9; i++) {
+				pos[i] = 0;
+			}
+			while (true) {
+				int action = play[turn][seq[playerIdx]];
+				playerIdx = (playerIdx + 1) % 9;
+				if (action == 0) {
+					outCnt++;
+					if (outCnt == 3) {
+						outCnt = 0;
+						break;
+					}
+				} else {
+					for (int i = 0; i < 9; i++) {
+						if (i == seq[playerIdx] || pos[i] > 0) {
+							pos[i] += action;
+							if (pos[i] >= 4) {
+								pos[i] = 0;
+								score++;
+							}
+						}
+					}
+				}
+			}
+			turn++;
+		}
+		return score;
+	}
+}
