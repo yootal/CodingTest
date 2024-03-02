d8f5023d7ba778fc86fab74518df616caddec27f
yootal <92325898+yootal@users.noreply.github.com>
2024-03-02 14:11:37 +0900

    [Unrated] Title: [모의 SW 역량테스트] 디저트 카페, Time: 490 ms, Memory: 104,192 KB -BaekjoonHub

--- SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
+++ SWEA/Unrated/2105. ［모의 SW 역량테스트］ 디저트 카페/［모의 SW 역량테스트］ 디저트 카페.java
@@ -5,7 +5,7 @@ public class Solution {
 	static final int[] dx = { 1, 1, -1, -1 };
 	static final int[] dy = { 1, -1, -1, 1 };
 	static int N, ans, board[][];
-	static HashSet<Integer> set;
+	static boolean vis[];
 
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
@@ -13,6 +13,7 @@ public class Solution {
 		StringTokenizer st;
 		StringBuilder sb = new StringBuilder();
 		int T = Integer.parseInt(br.readLine());
+		vis = new boolean[101];
 		for (int tc = 1; tc <= T; tc++) {
 			N = Integer.parseInt(br.readLine());
 			board = new int[N][N];
@@ -23,12 +24,11 @@ public class Solution {
 				}
 			}
 			ans = -1;
-			set = new HashSet<>();
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
-					set.add(board[i][j]);
-					dfs(i, j, 0, i, j);
-					set.remove(board[i][j]);
+					vis[board[i][j]] = true;
+					dfs(i, j, 0, i, j, 1);
+					vis[board[i][j]] = false;
 				}
 			}
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
@@ -40,17 +40,17 @@ public class Solution {
 		return (x >= 0 && x < N && y >= 0 && y < N);
 	}
 
-	static void dfs(int x, int y, int d, int sx, int sy) {
+	static void dfs(int x, int y, int d, int sx, int sy, int cnt) {
 		int nx = x + dx[d];
 		int ny = y + dy[d];
 		if (nx == sx && ny == sy) {
-			ans = Math.max(ans, set.size());
+			ans = Math.max(ans, cnt);
 			return;
 		}
-		if (limit(nx, ny) && !set.contains(board[nx][ny])) {
-			set.add(board[nx][ny]);
-			dfs(nx, ny, d, sx, sy);
-			set.remove(board[nx][ny]);
+		if (limit(nx, ny) && !vis[board[nx][ny]]) {
+			vis[board[nx][ny]] = true;
+			dfs(nx, ny, d, sx, sy, cnt + 1);
+			vis[board[nx][ny]] = false;
 		}
 		if (d == 3)
 			return;
@@ -58,13 +58,13 @@ public class Solution {
 		nx = x + dx[d];
 		ny = y + dy[d];
 		if (nx == sx && ny == sy) {
-			ans = Math.max(ans, set.size());
+			ans = Math.max(ans, cnt);
 			return;
 		}
-		if (limit(nx, ny) && !set.contains(board[nx][ny])) {
-			set.add(board[nx][ny]);
-			dfs(nx, ny, d, sx, sy);
-			set.remove(board[nx][ny]);
+		if (limit(nx, ny) && !vis[board[nx][ny]])  {
+			vis[board[nx][ny]] = true;
+			dfs(nx, ny, d, sx, sy, cnt + 1);
+			vis[board[nx][ny]] = false;
 		}
 	}
 }
