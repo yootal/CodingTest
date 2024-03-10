9bf551553fc7d0856d13f0431dda138e5f828ffb
yootal <92325898+yootal@users.noreply.github.com>
2024-03-10 23:04:14 +0900

    [Gold IV] Title: 감시, Time: 216 ms, Memory: 18732 KB -BaekjoonHub

--- 백준/Gold/15683. 감시/감시.java
+++ 백준/Gold/15683. 감시/감시.java
@@ -9,6 +9,16 @@ public class Main {
 	static int N, M, ans;
 	static ArrayList<Camera> camera;
 
+	static class Camera {
+		int x, y, mode;
+
+		public Camera(int x, int y, int mode) {
+			this.x = x;
+			this.y = y;
+			this.mode = mode;
+		}
+	}
+
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -41,41 +51,41 @@ public class Main {
 		Camera cur = camera.get(cnt);
 		if (cur.mode == 1) {
 			for (int i = 0; i < 4; i++) {
-				erase(cur.x, cur.y, i);
+				go(cur.x, cur.y, i);
 				solve(cnt + 1);
 				back(cur.x, cur.y, i);
 			}
 		} else if (cur.mode == 2) {
 			for (int i = 0; i < 2; i++) {
-				erase(cur.x, cur.y, i);
-				erase(cur.x, cur.y, (i + 2) % 4);
+				go(cur.x, cur.y, i);
+				go(cur.x, cur.y, (i + 2) % 4);
 				solve(cnt + 1);
 				back(cur.x, cur.y, i);
 				back(cur.x, cur.y, (i + 2) % 4);
 			}
 		} else if (cur.mode == 3) {
 			for (int i = 0; i < 4; i++) {
-				erase(cur.x, cur.y, i);
-				erase(cur.x, cur.y, (i + 1) % 4);
+				go(cur.x, cur.y, i);
+				go(cur.x, cur.y, (i + 1) % 4);
 				solve(cnt + 1);
 				back(cur.x, cur.y, i);
 				back(cur.x, cur.y, (i + 1) % 4);
 			}
 		} else if (cur.mode == 4) {
 			for (int i = 0; i < 4; i++) {
-				erase(cur.x, cur.y, i);
-				erase(cur.x, cur.y, (i + 1) % 4);
-				erase(cur.x, cur.y, (i + 2) % 4);
+				go(cur.x, cur.y, i);
+				go(cur.x, cur.y, (i + 1) % 4);
+				go(cur.x, cur.y, (i + 2) % 4);
 				solve(cnt + 1);
 				back(cur.x, cur.y, i);
 				back(cur.x, cur.y, (i + 1) % 4);
 				back(cur.x, cur.y, (i + 2) % 4);
 			}
 		} else {
-			erase(cur.x, cur.y, 0);
-			erase(cur.x, cur.y, 1);
-			erase(cur.x, cur.y, 2);
-			erase(cur.x, cur.y, 3);
+			go(cur.x, cur.y, 0);
+			go(cur.x, cur.y, 1);
+			go(cur.x, cur.y, 2);
+			go(cur.x, cur.y, 3);
 			solve(cnt + 1);
 			back(cur.x, cur.y, 0);
 			back(cur.x, cur.y, 1);
@@ -84,23 +94,13 @@ public class Main {
 		}
 	}
 
-	static class Camera {
-		int x, y, mode;
-
-		public Camera(int x, int y, int mode) {
-			this.x = x;
-			this.y = y;
-			this.mode = mode;
-		}
-	}
-
-	static void erase(int x, int y, int d) {
+	static void go(int x, int y, int d) {
 		if (!cameraCheck[x][y])
 			office[x][y]--;
 		int nx = x + dx[d];
 		int ny = y + dy[d];
 		if (nx >= 0 && nx < N && ny >= 0 && ny < M && office[nx][ny] != 6) {
-			erase(nx, ny, d);
+			go(nx, ny, d);
 		}
 	}
 
