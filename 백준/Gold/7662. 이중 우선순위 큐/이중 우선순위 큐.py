444fae9c5cad13013ef1a97a494fbc5eb36eaf91
yootal <92325898+yootal@users.noreply.github.com>
2024-03-13 10:18:52 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 6980 ms, Memory: 294516 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,4 +1,5 @@
-import sys, heapq
+import sys
+from heapq import heappush, heappop
 input = sys.stdin.readline
 
 t = int(input())
@@ -10,27 +11,27 @@ for _ in range(t):
         command, value = input().rstrip().split()
         value = int(value)
         if command == 'I':
-            heapq.heappush(min_heap,(value, i))
-            heapq.heappush(max_heap,(-value, i))
+            heappush(min_heap,(value, i))
+            heappush(max_heap,(-value, i))
             visited[i] = True
         if command == 'D':
             if value == 1:
                 while max_heap and not visited[max_heap[0][1]]:
-                    heapq.heappop(max_heap)
+                    heappop(max_heap)
                 if max_heap:
                     visited[max_heap[0][1]] = False
-                    heapq.heappop(max_heap)
+                    heappop(max_heap)
             elif value == -1:
                 while min_heap and not visited[min_heap[0][1]]:
-                    heapq.heappop(min_heap)
+                    heappop(min_heap)
                 if min_heap:
                     visited[min_heap[0][1]] = False
-                    heapq.heappop(min_heap)
+                    heappop(min_heap)
                         
     while min_heap and not visited[min_heap[0][1]]:
-        heapq.heappop(min_heap)
+        heappop(min_heap)
     while max_heap and not visited[max_heap[0][1]]:
-        heapq.heappop(max_heap)
+        heappop(max_heap)
     if max_heap and min_heap:
         print(-max_heap[0][0], min_heap[0][0]) 
     else: 
