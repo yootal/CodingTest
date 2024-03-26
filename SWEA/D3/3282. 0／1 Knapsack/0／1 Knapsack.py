5f7a0beb5998e40027b791cd869097c5140c8278
yootal <92325898+yootal@users.noreply.github.com>
2024-03-26 13:30:24 +0900

    [D3] Title: 0/1 Knapsack, Time: 185 ms, Memory: 62,708 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.py
@@ -1,17 +1,13 @@
-t = int(input())
-for case in range(1,t+1):
-    n,k = map(int,input().split())
-    things = []
-    for _ in range(n):
-        v,c = map(int,input().split())
-        things.append((v,c))
-    dp = [[0] * (k+1) for _ in range(n+1)]
-    for i in range(1,n+1):
-        for j in range(1,k+1):
-            volume = things[i-1][0]
-            cost = things[i-1][1]
-            if j < volume:
-                dp[i][j] = dp[i-1][j]
+for t in range(int(input())):
+    n, k = map(int, input().split())
+    dp = [[0] * (k + 1) for _ in range(n + 1)]
+    info = [tuple(map(int, input().split())) for _ in range(n)]
+    for i in range(1, n + 1):
+        size = info[i - 1][0]
+        value = info[i - 1][1]
+        for j in range(1, k + 1):
+            if j < size:
+                dp[i][j] = dp[i - 1][j]
             else:
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-volume] + cost)
-    print(f"#{case} {dp[n][k]}")
+                dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - size] + value)
+    print(f"#{t + 1} {dp[n][k]}")
