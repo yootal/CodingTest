abc4773b514ff0fc39ad339c8b550d0c3044c53b
yootal <92325898+yootal@users.noreply.github.com>
2024-03-27 13:30:11 +0900

    [D2] Title: 스도쿠 검증, Time: 149 ms, Memory: 58,764 KB -BaekjoonHub

--- SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.py
@@ -4,26 +4,25 @@ def check():
         col_check = set()
         for j in range(9):
             if board[i][j] in row_check:
-                return 0 
+                return 0
             else:
                 row_check.add(board[i][j])
             if board[j][i] in col_check:
                 return 0
             else:
                 col_check.add(board[j][i])
-    for i in range(0,9,3):
-        for j in range(0,9,3):
+    for i in range(0, 9, 3):
+        for j in range(0, 9, 3):
             part_check = set()
             for i2 in range(3):
                 for j2 in range(3):
-                    if board[i+i2][j+j2] in part_check:
+                    if board[i + i2][j + j2] in part_check:
                         return 0
                     else:
-                        part_check.add(board[i+i2][j+j2])
+                        part_check.add(board[i + i2][j + j2])
     return 1
 
-t = int(input())
-for case in range(1,t+1):
-    board = [list(map(int,input().split())) for _ in range(9)]
+
+for case in range(1, int(input()) + 1):
+    board = [list(map(int, input().split())) for _ in range(9)]
     print(f'#{case} {check()}')
-    
