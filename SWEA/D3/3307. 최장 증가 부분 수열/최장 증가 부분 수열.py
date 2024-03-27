338edfe5a4154f4596c116a316e3fce6ab1c105d
yootal <92325898+yootal@users.noreply.github.com>
2024-03-27 13:21:15 +0900

    [D3] Title: 최장 증가 부분 수열, Time: 175 ms, Memory: 60,100 KB -BaekjoonHub

--- SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
+++ SWEA/D3/3307. 최장 증가 부분 수열/최장 증가 부분 수열.py
@@ -1,10 +1,12 @@
-t = int(input())
-for case in range(1,t+1):
+from bisect import bisect_left
+
+for t in range(1, int(input()) + 1):
     n = int(input())
-    num = [0] + list(map(int,input().split()))
-    dp = [0] * (n+1)
-    for i in range(1,n+1):
-        for j in range(i):
-            if num[j] < num[i]:
-                dp[i] = max(dp[i],dp[j] + 1)
-    print(f"#{case} {max(dp)}")
+    num = list(map(int, input().split()))
+    ans = [0]
+    for x in num:
+        if ans[-1] < x:
+            ans.append(x)
+        else:
+            ans[bisect_left(ans, x)] = x
+    print(f'#{t} {len(ans)-1}')
