dee541aace9a4078faf6e7a50fe373f80db4c33a
yootal <92325898+yootal@users.noreply.github.com>
2024-04-01 15:24:00 +0900

    [D3] Title: [Professional] 조합, Time: 272 ms, Memory: 49,584 KB -BaekjoonHub

--- SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.py
+++ SWEA/D3/5607. ［Professional］ 조합/［Professional］ 조합.py
@@ -1,25 +1,47 @@
-def factorial(num):
-    n = 1
-    for i in range(2,num+1):
-        n = (n * i) % mod
-    return n
+import java.io.*;
+import java.util.*;
 
-def mod_division(n,k):
-    if k == 0:
-        return 1
-    elif k == 1:
-        return n
-    tmp = mod_division(n,k//2)
-    if k % 2 == 1:
-        return tmp * tmp * n % mod
-    else:
-        return tmp * tmp % mod
+public class Solution {
+	static long mod = 1234567891;
 
-mod = 1234567891
-t = int(input())
-for case in range(1,t+1):
-    n,r = map(int,input().split())
-    top = factorial(n)
-    bottom = factorial(n-r) * factorial(r) % mod
-    ans = top * mod_division(bottom,mod-2) % mod
-    print(f"#{case} {ans}")
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			long N = Long.parseLong(st.nextToken());
+			long R = Long.parseLong(st.nextToken());
+			long top = factorial(N);
+			long bottom = (factorial(N - R) % mod) * (factorial(R) % mod) % mod;
+			sb.append("#").append(tc + " ").append((top % mod) * (modDivision(bottom, mod - 2) % mod) % mod)
+					.append("\n");
+		}
+		System.out.print(sb);
+	}
+
+	static long factorial(long num) {
+		long n = 1;
+		for (int i = 2; i <= num; i++) {
+			n *= i;
+			n %= mod;
+		}
+		return n;
+	}
+
+	static long modDivision(long n, long r) {
+		if (r == 0)
+			return 1;
+		else if (r == 1) {
+			return n;
+		}
+		long temp = modDivision(n, r / 2);
+		if (r % 2 == 1) {
+			return ((temp * temp % mod) * n % mod) % mod;
+		} else {
+			return temp * temp % mod;
+		}
+	}
+}
