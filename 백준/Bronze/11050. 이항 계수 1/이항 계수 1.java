3ac7348d036f033cbf52fdd972ca30e92a5daac3
yootal <92325898+yootal@users.noreply.github.com>
2024-04-02 09:23:46 +0900

    [Bronze I] Title: 이항 계수 1, Time: 76 ms, Memory: 11512 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -9,16 +9,13 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		int n = Integer.parseInt(st.nextToken());
 		int k = Integer.parseInt(st.nextToken());
-		int[][] dp = new int[n + 1][n + 1];
+		int[][] dp = new int[n + 1][k + 1];
 		for (int i = 0; i <= n; i++) {
-			dp[i][1] = i;
-			dp[i][0] = 1;
-			dp[i][i] = 1;
-			if (i >= 2) {
-				for (int j = 1; j < i; j++) {
-					// i-1에서 j개 다고르나 아니면 j-1개 고르나
-					dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
-				}
+			for (int j = 0, end = Math.min(i, k); j <= end; ++j) {
+				if (j == 0 || j == i)
+					dp[i][j] = 1;
+				else
+					dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
 			}
 		}
 		System.out.println(dp[n][k]);
