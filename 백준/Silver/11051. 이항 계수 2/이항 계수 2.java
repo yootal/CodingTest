b7b9b85b88ce92fb0af7fd748ccad7182c534a08
yootal <92325898+yootal@users.noreply.github.com>
2024-04-02 09:28:40 +0900

    [Silver II] Title: 이항 계수 2, Time: 92 ms, Memory: 15596 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int mod = 10007;
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[][] dp = new int[n + 1][k + 1];
+		for (int i = 0; i <= n; i++) {
+			for (int j = 0, end = Math.min(i, k); j <= end; ++j) {
+				if (j == 0 || j == i)
+					dp[i][j] = 1;
+				else
+					dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) % mod;
+			}
+		}
+		System.out.println(dp[n][k]);
+	}
+}
