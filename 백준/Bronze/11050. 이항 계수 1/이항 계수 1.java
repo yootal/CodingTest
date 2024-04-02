12d0e46a322e5897aca68db4881ff9d501fa4576
yootal <92325898+yootal@users.noreply.github.com>
2024-04-02 09:20:05 +0900

    [Bronze I] Title: 이항 계수 1, Time: 80 ms, Memory: 11516 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -3,27 +3,24 @@ import java.util.*;
 
 public class Main {
 
-	// nCk
 	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-
-//		int[][] B = new int[N + 1][N + 1];
-		int[][] B = new int[N + 1][K + 1];
-
-		for (int i = 0; i <= N; i++) {
-			for (int j = 0, end = Math.min(i, K); j <= end; ++j) {
-
-				if (j == 0 || j == i)
-					B[i][j] = 1;
-
-				// nCk = (n-1)C(k-1) + (n-1)C(k)
-				else
-					B[i][j] = B[i - 1][j - 1] + B[i - 1][j];
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int[][] dp = new int[n + 1][n + 1];
+		for (int i = 0; i <= n; i++) {
+			dp[i][1] = i;
+			dp[i][0] = 1;
+			dp[i][i] = 1;
+			if (i >= 2) {
+				for (int j = 1; j < i; j++) {
+					// i-1에서 j개 다고르나 아니면 j-1개 고르나
+					dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+				}
 			}
 		}
-		System.out.println(B[N][K]);
+		System.out.println(dp[n][k]);
 	}
 }
