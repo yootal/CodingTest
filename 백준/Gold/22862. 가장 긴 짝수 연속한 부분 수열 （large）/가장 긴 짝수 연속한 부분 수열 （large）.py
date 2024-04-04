4b69c4a63e4b31ee37b8ad431009b642bf7f75a8
yootal <92325898+yootal@users.noreply.github.com>
2024-04-05 08:11:02 +0900

    [Gold V] Title: 가장 긴 짝수 연속한 부분 수열 (large), Time: 1144 ms, Memory: 155584 KB -BaekjoonHub

--- 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.py
+++ 백준/Gold/22862. 가장 긴 짝수 연속한 부분 수열 （large）/가장 긴 짝수 연속한 부분 수열 （large）.py
@@ -1,21 +1,33 @@
 import sys
 input = sys.stdin.readline
-    
-n,k = map(int,input().split())
-num = list(map(int,input().split()))
 
-ans = 0
-cnt = 0
+n,k = map(int,input().split())
+s = list(map(int,input().split()))
+
 en = 0
-for st in range(n):
-    while en < n and cnt <= k:
-        if cnt >= k and num[en] % 2 == 1:
-            break
-        if num[en] % 2 == 1:
-            cnt += 1
-        en += 1 
-    ans = max(ans, en - st - cnt)
-    if num[st] % 2 == 1:
-        cnt -= 1
+ans = 0
+if s[0] % 2 == 1:
+    k -= 1
+    len = 0
+else:
+    len = 1
     
+for st in range(n):
+    while en + 1 < n and (s[en+1] % 2 == 0 or k > 0):
+        en += 1
+        if s[en] % 2 == 0:
+            len += 1
+        else:
+            k -= 1
+    
+    ans = max(ans,len)
+    if en == n - 1:
+        break
+
+    if s[st] % 2 == 0:
+        len -= 1
+    else:
+        k += 1
+
 print(ans)
+            
