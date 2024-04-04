e5828cea529bc4c46f4ca962e1444f8ddba2e075
yootal <92325898+yootal@users.noreply.github.com>
2024-04-04 16:28:37 +0900

    [D4] Title: 장훈이의 높은 선반, Time: 333 ms, Memory: 62,552 KB -BaekjoonHub

--- SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
+++ SWEA/D4/1486. 장훈이의 높은 선반/장훈이의 높은 선반.py
@@ -1,13 +1,41 @@
-from itertools import combinations
+import java.io.*;
+import java.util.*;
 
-t = int(input())
-for case in range(1, t + 1):
-    n, b = map(int, input().split())
-    s = list(map(int, input().split()))
-    ans = 1e9
-    for i in range(1, n + 1):
-        for comb in combinations(s, i):
-            total = sum(comb)
-            if total >= b:
-                ans = min(ans, total)
-    print(f'#{case} {ans - b}')
+public class Solution {
+	static int n, b, ans, num[];
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			b = Integer.parseInt(st.nextToken());
+			num = new int[n];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < n; i++) {
+				num[i] = Integer.parseInt(st.nextToken());
+			}
+			ans = Integer.MAX_VALUE;
+			select(0, 0);
+			sb.append("#").append(tc).append(" ").append(ans - b).append("\n");
+		}
+		System.out.print(sb);
+	}
+
+	static void select(int cur, int total) {
+		if (total > ans)
+			return;
+		if (cur == n) {
+			if (total >= b) {
+				ans = Math.min(ans, total);
+			}
+			return;
+		}
+		select(cur + 1, total + num[cur]);
+		select(cur + 1, total);
+	}
+}
