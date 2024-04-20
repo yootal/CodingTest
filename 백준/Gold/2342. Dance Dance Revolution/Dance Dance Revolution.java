8bb3317f6b50e44f97fd903071dd085315795aa1
yootal <92325898+yootal@users.noreply.github.com>
2024-04-20 14:40:47 +0900

    [Gold III] Title: Dance Dance Revolution, Time: 404 ms, Memory: 50680 KB -BaekjoonHub

--- 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
+++ 백준/Gold/2342. Dance Dance Revolution/Dance Dance Revolution.java
@@ -3,7 +3,6 @@ import java.util.*;
 
 public class Main {
 	static int dp[][][];
-	static ArrayList<Integer> num;
 	static final int force[][] = { 
 			{ 1, 2, 2, 2, 2 }, 
 			{ 2, 1, 3, 4, 3 }, 
@@ -15,26 +14,46 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		dp = new int[100001][5][5];
+		for (int i = 0; i <= 100000; i++) {
+			for (int j = 0; j < 5; j++) {
+				for (int k = 0; k < 5; k++) {
+					dp[i][j][k] = 100000 * 4;
+				}
+			}
+		}
+		dp[0][0][0] = 0;
+		int idx = 1, cur = 0;
+		
+		// BottomUp
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		num = new ArrayList<>();
 		while (st.hasMoreTokens()) {
-			int cur = Integer.parseInt(st.nextToken());
+			cur = Integer.parseInt(st.nextToken());
 			if (cur == 0)
 				break;
-			num.add(cur);
+			for (int i = 0; i < 5; i++) {
+				if (i == cur) // 양발 불가능
+					continue;
+				for (int j = 0; j < 5; j++) {
+					dp[idx][i][cur] = Math.min(dp[idx][i][cur], dp[idx - 1][i][j] + force[j][cur]);
+				}
+			}
+			for (int j = 0; j < 5; j++) {
+				if (j == cur)
+					continue;
+				for (int i = 0; i < 5; i++) {
+					dp[idx][cur][j] = Math.min(dp[idx][cur][j], dp[idx - 1][i][j] + force[i][cur]);
+				}
+			}
+			idx++;
 		}
-		dp = new int[100000][5][5];
-		dance(0, 0, 0);
-		System.out.println(dp[0][0][0]);
-	}
-
-	static int dance(int idx, int x, int y) {
-		if (idx == num.size())
-			return 0;
-		if (dp[idx][x][y] != 0)
-			return dp[idx][x][y];
-		int nxt = num.get(idx);
-		dp[idx][x][y] = Math.min(dance(idx + 1, x, nxt) + force[y][nxt], dance(idx + 1, nxt, y) + force[x][nxt]);
-		return dp[idx][x][y];
+		idx--;
+		int ans = Integer.MAX_VALUE;
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				ans = Math.min(ans, dp[idx][i][j]);
+			}
+		}
+		System.out.println(ans);
 	}
 }
