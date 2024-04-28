6651e141e53db560e145a31320ed2c57a6b81ecd
yootal <92325898+yootal@users.noreply.github.com>
2024-04-28 15:56:49 +0900

    [Bronze V] Title: 이 별은 무슨 색일까, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30676. 이 별은 무슨 색일까/이 별은 무슨 색일까.py
@@ -0,0 +1,15 @@
+x=int(input())
+if 620<=x<=780:
+    print("Red")
+elif 590<=x<620:
+    print("Orange")
+elif 570<=x<590:
+    print("Yellow")
+elif 495<=x< 570:
+    print("Green")
+elif 450<=x<495:
+    print("Blue")
+elif 425<=x<450:
+    print("Indigo")
+elif 380<=x<425:
+    print("Violet")
