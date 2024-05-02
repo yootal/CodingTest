ea869473a542c781e72c582ba87d38bfd3a2ac48
yootal <92325898+yootal@users.noreply.github.com>
2024-05-02 11:25:40 +0900

    [Gold IV] Title: 최단경로, Time: 732 ms, Memory: 113972 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,6 +2,23 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+	static class Node implements Comparable<Node> {
+		int vertex, weight;
+		Node link;
+
+		public Node(int vertex, int weight, Node link) {
+			this.vertex = vertex;
+			this.weight = weight;
+			this.link = link;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(weight, o.weight);
+		}
+
+	}
+
 	public static void main(String[] args) throws Exception {
 		//System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -9,10 +26,7 @@ class Main {
 		int V = Integer.parseInt(st.nextToken());
 		int E = Integer.parseInt(st.nextToken());
 		int K = Integer.parseInt(br.readLine());
-		ArrayList<int[]>[] graph = new ArrayList[V + 1];
-		for (int i = 1; i <= V; i++) {
-			graph[i] = new ArrayList<>();
-		}
+		Node[] nodeList = new Node[V + 1];
 		int[] dist = new int[V + 1];
 		Arrays.fill(dist, Integer.MAX_VALUE);
 		for (int i = 0; i < E; i++) {
@@ -20,7 +34,7 @@ class Main {
 			int u = Integer.parseInt(st.nextToken());
 			int v = Integer.parseInt(st.nextToken());
 			int w = Integer.parseInt(st.nextToken());
-			graph[u].add(new int[] { v, w });
+			nodeList[u] = new Node(v, w, nodeList[u]);
 		}
 		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1], o2[1]));
 		dist[K] = 0;
@@ -29,10 +43,10 @@ class Main {
 			int[] cur = pq.poll();
 			if (cur[1] != dist[cur[0]])
 				continue;
-			for (int[] nxt : graph[cur[0]]) {
-				if (cur[1] + nxt[1] < dist[nxt[0]]) {
-					dist[nxt[0]] = cur[1] + nxt[1];
-					pq.offer(new int[] { nxt[0], dist[nxt[0]] });
+			for (Node nxt = nodeList[cur[0]]; nxt != null; nxt = nxt.link) {
+				if (dist[nxt.vertex] > dist[cur[0]] + nxt.weight) {
+					dist[nxt.vertex] = dist[cur[0]] + nxt.weight;
+					pq.offer(new int[] { nxt.vertex, dist[nxt.vertex] });
 				}
 			}
 		}
