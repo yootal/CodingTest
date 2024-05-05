cdc3efd3bae0a918086b26daf6f81b3bb739354d
yootal <92325898+yootal@users.noreply.github.com>
2024-05-05 12:30:41 +0900

    [Gold V] Title: 용액, Time: 284 ms, Memory: 31980 KB -BaekjoonHub

---
+++ 백준/Gold/2467. 용액/용액.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int num[] = new int[N];
+		for (int i = 0; i < N; i++) {
+			num[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(num);
+		int ans[] = new int[2];
+		int pre = Integer.MAX_VALUE;
+		int s = 0, e = N - 1;
+		while (s < e) {
+			int cur = num[s] + num[e];
+			if (pre > Math.abs(cur)) {
+				pre = Math.abs(cur);
+				ans[0] = num[s];
+				ans[1] = num[e];
+			}
+			if (cur < 0)
+				s++;
+			else
+				e--;
+		}
+		System.out.println(ans[0] + " " + ans[1]);
+	}
+}
