e7d8ab633b12ea54a63ddca7eca07f740077a97f
yootal <92325898+yootal@users.noreply.github.com>
2024-05-16 08:40:59 +0900

    [Silver IV] Title: 암기왕, Time: 3356 ms, Memory: 188316 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.py
+++ 백준/Silver/2776. 암기왕/암기왕.py
@@ -1,15 +1,17 @@
 from bisect import bisect_left
+from sys import stdin
 
-T = int(input())
-for _ in range(T):
-    N = int(input())
-    num1 = list(map(int, input().split()))
+inp = stdin.readline
+
+for _ in range(int(inp())):
+    N = int(inp())
+    num1 = list(map(int, inp().split()))
     num1.sort()
-    M = int(input())
-    num2 = list(map(int, input().split()))
-    for i in range(len(num2)):
+    M = int(inp())
+    num2 = list(map(int, inp().split()))
+    for i in range(M):
         idx = bisect_left(num1, num2[i])
-        if idx < len(num1) and num2[i] == num1[bisect_left(num1, num2[i])]:
+        if idx < N and num2[i] == num1[idx]:
             print(1)
         else:
             print(0)
