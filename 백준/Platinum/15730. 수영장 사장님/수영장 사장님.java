0042b5baf04e945593bfd1041e58b160771b5388
yootal <92325898+yootal@users.noreply.github.com>
2024-05-30 19:35:43 +0900

    [Platinum V] Title: 수영장 사장님, Time: 308 ms, Memory: 19392 KB -BaekjoonHub

--- 백준/Platinum/15730. 수영장 사장님/수영장 사장님.java
+++ 백준/Platinum/15730. 수영장 사장님/수영장 사장님.java
@@ -14,7 +14,6 @@ class Main {
 		int M = Integer.parseInt(st.nextToken()) + 2;
 		int block[][] = new int[N][M]; // 블록 높이
 		int water[][] = new int[N][M]; // 물 높이
-		boolean vis[][] = new boolean[N][M];
 		for (int i = 1; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j < M - 1; j++) {
@@ -22,18 +21,18 @@ class Main {
 			}
 		}
 		// 물 높이 초기화
-		for (int i = 0; i < N; i++) Arrays.fill(water[i], Integer.MAX_VALUE);
+		for (int i = 0; i < N; i++)
+			Arrays.fill(water[i], Integer.MAX_VALUE);
 		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
 		water[0][0] = 0;
 		pq.offer(new int[] { 0, 0, 0 }); // 테두리 높이 0 시작
 		while (!pq.isEmpty()) {
 			int cur[] = pq.poll();
-			if (vis[cur[0]][cur[1]]) break;
-			vis[cur[0]][cur[1]] = true;
 			for (int d = 0; d < 4; d++) {
 				int nx = cur[0] + dx[d];
 				int ny = cur[1] + dy[d];
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M) continue;
+				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+					continue;
 				// 1. 현 위치 물 높이와 주변 블록의 높이중 최대값 선택
 				int h = Math.max(water[cur[0]][cur[1]], block[nx][ny]);
 				// 2. 주변의 물 높이보다 h 작으면 갱신
