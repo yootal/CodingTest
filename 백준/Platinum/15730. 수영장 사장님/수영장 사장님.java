afa92ffbe6f43e16a8f7f09e92741be2cec3c719
yootal <92325898+yootal@users.noreply.github.com>
2024-05-30 20:03:13 +0900

    [Platinum V] Title: 수영장 사장님, Time: 180 ms, Memory: 13192 KB -BaekjoonHub

--- 백준/Platinum/15730. 수영장 사장님/수영장 사장님.java
+++ 백준/Platinum/15730. 수영장 사장님/수영장 사장님.java
@@ -2,54 +2,56 @@ import java.io.*;
 import java.util.*;
 
 class Main {
+	static final int dx[] = { -1, 0, 1, 0 };
+	static final int dy[] = { 0, 1, 0, -1 };
+	static int N, M, block[][], water[][];
 
 	public static void main(String[] args) throws Exception {
-		final int dx[] = { -1, 0, 1, 0 };
-		final int dy[] = { 0, 1, 0, -1 };
 		//System.setIn(new FileInputStream("res/input.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		// 테두리 만들고 시작
-		int N = Integer.parseInt(st.nextToken()) + 2;
-		int M = Integer.parseInt(st.nextToken()) + 2;
-		int block[][] = new int[N][M]; // 블록 높이
-		int water[][] = new int[N][M]; // 물 높이
+		N = Integer.parseInt(st.nextToken()) + 2;
+		M = Integer.parseInt(st.nextToken()) + 2;
+		block = new int[N][M];
+		water = new int[N][M];
 		for (int i = 1; i < N - 1; i++) {
 			st = new StringTokenizer(br.readLine());
 			for (int j = 1; j < M - 1; j++) {
 				block[i][j] = Integer.parseInt(st.nextToken());
+				water[i][j] = Integer.MAX_VALUE;
 			}
 		}
-		// 물 높이 초기화
-		for (int i = 0; i < N; i++)
-			Arrays.fill(water[i], Integer.MAX_VALUE);
-		PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[2], o2[2]));
-		water[0][0] = 0;
-		pq.offer(new int[] { 0, 0, 0 }); // 테두리 높이 0 시작
-		while (!pq.isEmpty()) {
-			int cur[] = pq.poll();
-			for (int d = 0; d < 4; d++) {
-				int nx = cur[0] + dx[d];
-				int ny = cur[1] + dy[d];
-				if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-					continue;
-				// 1. 현 위치 물 높이와 주변 블록의 높이중 최대값 선택
-				int h = Math.max(water[cur[0]][cur[1]], block[nx][ny]);
-				// 2. 주변의 물 높이보다 h 작으면 갱신
-				// 블록이 더 크면 어차피 계산시 0 나오고
-				// 물 높이가 더 크면 해당 위치의 물 높이 기록 -> 이후 블록 높이 빼서 물 계산
-				if (water[nx][ny] > h) {
-					water[nx][ny] = h;
-					pq.offer(new int[] { nx, ny, h });
-				}
-			}
-		}
+		fill();
 		int ans = 0;
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
+		for (int i = 1; i < N - 1; i++) {
+			for (int j = 1; j < M - 1; j++) {
 				ans += water[i][j] - block[i][j];
 			}
 		}
 		System.out.println(ans);
+	}
+
+	static void fill() {
+		while (true) {
+			boolean check = true;
+			for (int i = 1; i < N - 1; i++) {
+				for (int j = 1; j < M - 1; j++) {
+					if (block[i][j] == water[i][j])
+						continue;
+					int minH = Integer.MAX_VALUE;
+					for (int d = 0; d < 4; d++) {
+						int nx = i + dx[d];
+						int ny = j + dy[d];
+						minH = Math.min(minH, water[nx][ny]);
+					}
+					if (water[i][j] > minH) {
+						water[i][j] = Math.max(block[i][j], minH);
+						check = false;
+					}
+				}
+			}
+			if (check)
+				break;
+		}
 	}
 }
