e5bd293ca4845d0bca4d4a9cd9285418f04a9452
yootal <92325898+yootal@users.noreply.github.com>
2024-06-01 17:36:45 +0900

    [Platinum V] Title: 전개도, Time: 80 ms, Memory: 11548 KB -BaekjoonHub

---
+++ 백준/Platinum/2642. 전개도/전개도.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+	static final int dx[] = { -1, 0, 1, 0 };
+	static final int dy[] = { 0, 1, 0, -1 };
+	static int[][] block, point;
+	static boolean vis[][], tempVis[][];
+	static int pair, opposite;
+
+	public static void main(String[] args) throws Exception {
+		//System.setIn(new FileInputStream("res/input.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		block = new int[6][6];
+		vis = new boolean[6][6];
+		point = new int[6][2];
+		int idx = 0;
+		for (int i = 0; i < 6; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < 6; j++) {
+				block[i][j] = Integer.parseInt(st.nextToken());
+				if (block[i][j] != 0) {
+					point[idx][0] = i;
+					point[idx][1] = j;
+					idx++;
+				}
+			}
+		}
+		pair = 0;
+		for (int i = 0; i < 6; i++) {
+			int cur[] = point[i];
+			if (!vis[cur[0]][cur[1]]) {
+				tempVis = new boolean[6][6];
+				dfs(cur[0], cur[1], 0, 0, block[cur[0]][cur[1]]);
+			}
+		}
+		if (pair != 3) {
+			System.out.println(0);
+		} else {
+			System.out.println(opposite);
+		}
+	}
+
+	static void dfs(int x, int y, int cnt, int pre, int from) {
+		if (cnt == 0 || cnt == 2) {
+			if (!vis[x][y])
+				vis[x][y] = true;
+			else
+				return;
+		}
+		// 카운트가 2개면 짝 성립
+		if (cnt == 2) {
+			pair++;
+			if (from == 1)
+				opposite = block[x][y];
+			if (block[x][y] == 1)
+				opposite = from;
+			return;
+		}
+		tempVis[x][y] = true;
+		for (int d = 0; d < 4; d++) {
+			int nx = x + dx[d];
+			int ny = y + dy[d];
+			if (nx >= 0 && nx < 6 && ny >= 0 && ny < 6 && block[nx][ny] != 0 && !tempVis[nx][ny]) {
+				if (cnt == 0) {
+					// 시작 블록이면 방향 등록
+					dfs(nx, ny, cnt + 1, d, from);
+				} else if (pre == d) {
+					// 같은 방향이면 카운트 +1
+					dfs(nx, ny, cnt + 1, pre, from);
+				} else {
+					// 다른 방향이면 이동만
+					dfs(nx, ny, cnt, pre, from);
+				}
+			}
+		}
+	}
+}
