61ee66a9377340b1f483f534483ee77ae4fefb5a
yootal <92325898+yootal@users.noreply.github.com>
2024-06-11 19:12:06 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,4 @@
+s = { "Algorithm": "204", "DataAnalysis": "207", "ArtificialIntelligence": "302", "CyberSecurity": "B101", "Network": "303", "Startup": "501", "TestStrategy": "105" }
+inp = [input() for _ in range(int(input()))]
+for x in inp:
+  print(s[x])
