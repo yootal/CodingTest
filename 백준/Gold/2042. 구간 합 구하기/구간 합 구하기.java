101ef22621b655b9ad851b637bc198ec8f99dd85
유경헌 <92325898+yootal@users.noreply.github.com>
2024-10-19 02:01:04 +0900

    [Gold I] Title: 구간 합 구하기, Time: 464 ms, Memory: 117212 KB -BaekjoonHub

--- 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
+++ 백준/Gold/2042. 구간 합 구하기/구간 합 구하기.java
@@ -1,72 +1,74 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N, M, K;
-	static long tree[];
+    static long[] num, tree;
 
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		int h = (int) Math.ceil(Math.log(N) / Math.log(2));
-		int treeSize = 1 << (h + 1);
-		int startIdx = treeSize / 2 - 1;
-		tree = new long[treeSize + 1];
-		for (int i = startIdx + 1; i <= startIdx + N; i++) {
-			tree[i] = Long.parseLong(br.readLine());
-		}
-		init(treeSize);
-		for (int i = 0; i < M + K; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			long c = Long.parseLong(st.nextToken());
-			if (a == 1) {
-				change(startIdx + b, c);
-			} else {
-				b += startIdx;
-				c += startIdx;
-				sb.append(calcSum(b, (int) c)).append("\n");
-			}
-		}
-		System.out.print(sb);
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        M += K;
+        num = new long[N];
+        for (int i = 0; i < N; i++) {
+            num[i] = Long.parseLong(br.readLine());
+        }
+        int h = (int) Math.ceil(Math.log(N) / Math.log(2));
+        int tree_size = (1 << (h + 1));
+        tree = new long[tree_size];
+        init(1, 0, N - 1);
+        StringBuilder sb = new StringBuilder();
+        while (M-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int cmd = Integer.parseInt(st.nextToken());
+            if (cmd == 1) {
+                int index = Integer.parseInt(st.nextToken());
+                long val = Long.parseLong(st.nextToken());
+                update(1, 0, N - 1, index - 1, val);
+            } else {
+                int left = Integer.parseInt(st.nextToken());
+                int right = Integer.parseInt(st.nextToken());
+                sb.append(query(1, 0, N - 1, left - 1, right - 1)).append("\n");
+            }
+        }
+        System.out.println(sb);
+    }
 
-	static void init(int i) {
-		while (i != 1) {
-			tree[i / 2] += tree[i];
-			i--;
-		}
-	}
+    static void init(int node, int start, int end) {
+        if (start == end) {
+            tree[node] = num[start];
+        } else {
+            init(node * 2, start, (start + end) / 2);
+            init(node * 2 + 1, (start + end) / 2 + 1, end);
+            tree[node] = tree[node * 2] + tree[node * 2 + 1];
+        }
+    }
 
-	static void change(int idx, long v) {
-		long diff = v - tree[idx];
-		while (idx > 0) {
-			tree[idx] = tree[idx] + diff;
-			idx /= 2;
-		}
-	}
+    static long query(int node, int start, int end, int left, int right) {
+        if (left > end || right < start) {
+            return 0;
+        }
+        if (left <= start && end <= right) {
+            return tree[node];
+        }
+        long leftSum = query(node * 2, start, (start + end) / 2, left, right);
+        long rightSum = query(node * 2 + 1, (start + end) / 2 + 1, end, left, right);
+        return leftSum + rightSum;
+    }
 
-	static long calcSum(int s, int e) {
-		long total = 0;
-		while (s <= e) {
-			if (s % 2 == 1) {
-				total += tree[s];
-				s++;
-			}
-			if (e % 2 == 0) {
-				total += tree[e];
-				e--;
-			}
-			s = s / 2;
-			e = e / 2;
-		}
-		return total;
-	}
-
+    static void update(int node, int start, int end, int index, long val) {
+        if (index < start || index > end) {
+            return;
+        }
+        if (start == end) {
+            num[index] = val;
+            tree[node] = val;
+            return;
+        }
+        update(node * 2, start, (start + end) / 2, index, val);
+        update(node * 2 + 1, (start + end) / 2 + 1, end, index, val);
+        tree[node] = tree[node * 2] + tree[node * 2 + 1];
+    }
 }
