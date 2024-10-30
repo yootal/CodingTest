ffb43adacb28d48e6be481f142b33f880af33c18
유경헌 <92325898+yootal@users.noreply.github.com>
2024-10-30 09:29:47 +0900

    [Platinum V] Title: 주유소, Time: 320 ms, Memory: 45020 KB, Score: 34 point -BaekjoonHub

---
+++ 백준/Platinum/13308. 주유소/주유소.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        ArrayList<int[]>[] graph = new ArrayList[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
+        int[] price = new int[N + 1];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            price[i] = Integer.parseInt(st.nextToken());
+        }
+        int[][] dist = new int[N + 1][2501];
+        for (int i = 1; i <= N; i++) {
+            Arrays.fill(dist[i], (int) 1e9);
+        }
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            graph[a].add(new int[]{b, c});
+            graph[b].add(new int[]{a, c});
+        }
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
+        dist[1][price[1]] = 0;
+        pq.offer(new int[]{1, dist[1][price[1]], price[1]}); // node, total, minCost
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            if (cur[0] == N) {
+                System.out.println(cur[1]);
+                break;
+            }
+            for (int[] nxt : graph[cur[0]]) {
+                int nxtTotal = cur[1] + nxt[1] * cur[2];
+                if (dist[nxt[0]][cur[2]] > nxtTotal) {
+                    dist[nxt[0]][cur[2]] = nxtTotal;
+                    pq.offer(new int[]{nxt[0], nxtTotal, Math.min(cur[2], price[nxt[0]])});
+                }
+            }
+        }
+    }
+}
