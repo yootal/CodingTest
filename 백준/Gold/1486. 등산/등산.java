43d161b5547984cb4540deea359836c7bbe4e338
유경헌 <92325898+yootal@users.noreply.github.com>
2025-04-24 19:55:48 +0900

    [Gold II] Title: 등산, Time: 260 ms, Memory: 21004 KB -BaekjoonHub

--- 백준/Gold/1486. 등산/등산.java
+++ 백준/Gold/1486. 등산/등산.java
@@ -6,7 +6,8 @@ public class Main {
     static int N, M, T, D;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int[][] mountain, dist;
+    static int[][] mountain;
+    static int[][][] dist;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,40 +29,14 @@ public class Main {
                 }
             }
         }
-        dist = new int[N][M];
-        for (int i = 0; i < N; i++) {
-            Arrays.fill(dist[i], Integer.MAX_VALUE);
-        }
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
-        pq.offer(new int[]{0, 0, 0});
-        dist[0][0] = 0;
-        while (!pq.isEmpty()) {
-            int[] cur = pq.poll();
-            int x = cur[0];
-            int y = cur[1];
-            int t = cur[2];
-            if (dist[x][y] != t) continue;
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-                if (limit(nx, ny) && Math.abs(mountain[x][y] - mountain[nx][ny]) <= T) {
-                    int diff = calc(mountain[x][y], mountain[nx][ny]);
-                    if (dist[nx][ny] > t + diff) {
-                        dist[nx][ny] = t + diff;
-                        pq.offer(new int[]{nx, ny, dist[nx][ny]});
-                    }
-                }
+        dist = new int[N][M][52];
+        dijkstra();
+        for (int i = 51; i >= 0; i--) {
+            if (dist[0][0][i] <= D) {
+                System.out.println(i);
+                System.exit(0);
             }
         }
-        int top = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if (dist[i][j] != Integer.MAX_VALUE && dijkstra(i, j)) {
-                    top = Math.max(top, mountain[i][j]);
-                }
-            }
-        }
-        System.out.println(top);
     }
 
     static boolean limit(int x, int y) {
@@ -76,32 +51,34 @@ public class Main {
         }
     }
 
-    static boolean dijkstra(int i, int j) {
-        int[][] dist2 = new int[N][M];
-        for (int k = 0; k < N; k++) {
-            Arrays.fill(dist2[k], Integer.MAX_VALUE);
+    static void dijkstra() {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                Arrays.fill(dist[i][j], Integer.MAX_VALUE);
+            }
         }
-        PriorityQueue<int[]> pq2 = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
-        pq2.offer(new int[]{i, j, 0});
-        dist2[i][j] = 0;
-        while (!pq2.isEmpty()) {
-            int[] cur = pq2.poll();
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[2]));
+        dist[0][0][mountain[0][0]] = 0;
+        pq.offer(new int[]{0, 0, 0, mountain[0][0]});
+        while (!pq.isEmpty()) {
+            int[] cur = pq.poll();
             int x = cur[0];
             int y = cur[1];
             int t = cur[2];
-            if (dist2[x][y] != t) continue;
+            int top = cur[3];
+            if (dist[x][y][top] != t) continue;
             for (int d = 0; d < 4; d++) {
                 int nx = x + dx[d];
                 int ny = y + dy[d];
                 if (limit(nx, ny) && Math.abs(mountain[x][y] - mountain[nx][ny]) <= T) {
                     int diff = calc(mountain[x][y], mountain[nx][ny]);
-                    if (dist2[nx][ny] > t + diff) {
-                        dist2[nx][ny] = t + diff;
-                        pq2.offer(new int[]{nx, ny, dist2[nx][ny]});
+                    int nt = Math.max(top, mountain[nx][ny]);
+                    if (dist[nx][ny][nt] > t + diff) {
+                        dist[nx][ny][nt] = t + diff;
+                        pq.offer(new int[]{nx, ny, dist[nx][ny][nt], nt});
                     }
                 }
             }
         }
-        return dist2[0][0] != Integer.MAX_VALUE && dist[i][j] + dist2[0][0] <= D;
     }
 }
