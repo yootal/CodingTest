2b0d066d2645a1e4ff8f4b09a50cfa137b6a70be
유경헌 <92325898+yootal@users.noreply.github.com>
2025-06-14 01:42:54 +0900

    [Gold IV] Title: 플로이드, Time: 384 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.py
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -3,24 +3,23 @@ input = sys.stdin.readline
 
 n = int(input())
 m = int(input())
-graph = [[sys.maxsize] * (n) for _ in range(n)]
+inf = sys.maxsize
+graph = [[inf] * (n+1) for _ in range(n+1)]
 
 for _ in range(m):
-    s,e,c = map(int,input().split())
-    graph[s-1][e-1] = min(graph[s-1][e-1],c)
-
-for i in range(n):
+    a,b,c = map(int,input().split())
+    graph[a][b] = min(graph[a][b],c)
+    
+for i in range(1,n+1):
     graph[i][i] = 0
 
-for k in range(n):
-    for i in range(n):
-        for j in range(n):
-            graph[i][j] = min(graph[i][j], graph[i][k] + graph[k][j])
-            
-for i in range(n):
-    for j in range(n):
-        if graph[i][j] == sys.maxsize:
-            print(0,end = " ")
-        else:
-            print(graph[i][j],end = " ")
+for k in range(1,n+1): 
+    for i in range(1,n+1): 
+        for j in range(1,n+1): 
+            if graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+
+for row in graph[1:]:
+    for col in row[1:]:
+        print(col if col != inf else 0,end=" ")
     print()
