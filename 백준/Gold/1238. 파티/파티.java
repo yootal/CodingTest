ecbea3fd2cf071b1a4d9e4d6cfccb397b7780107
유경헌 <92325898+yootal@users.noreply.github.com>
2025-08-08 13:18:58 +0900

    [Gold III] Title: 파티, Time: 288 ms, Memory: 21348 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.java
+++ 백준/Gold/1238. 파티/파티.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
     static int N, M, X;
-    static ArrayList<int[]>[] graph;
+    static ArrayList<int[]>[] graph, graphReverse;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,8 +12,10 @@ public class Main {
         M = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
         graph = new ArrayList[N + 1];
+        graphReverse = new ArrayList[N + 1];
         for (int i = 1; i <= N; i++) {
             graph[i] = new ArrayList<>();
+            graphReverse[i] = new ArrayList<>();
         }
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
@@ -21,26 +23,20 @@ public class Main {
             int e = Integer.parseInt(st.nextToken());
             int t = Integer.parseInt(st.nextToken());
             graph[s].add(new int[]{e, t});
+            graphReverse[e].add(new int[]{s, t});
         }
+        int[] dist = dijkstra(X, graph);
+        int[] dist2 = dijkstra(X, graphReverse);
         int[] round = new int[N + 1];
-        for (int i = 1; i <= N; i++) {
-            int[] dist = dijkstra(i);
-            if (i == X) {
-                for (int j = 1; j <= N; j++) {
-                    round[j] += dist[j];
-                }
-            } else {
-                round[i] += dist[X];
-            }
-        }
         int ans = 0;
         for (int i = 1; i <= N; i++) {
+            round[i] = dist[i] + dist2[i];
             ans = Math.max(ans, round[i]);
         }
         System.out.println(ans);
     }
 
-    static int[] dijkstra(int s) {
+    static int[] dijkstra(int s, ArrayList<int[]>[] graph) {
         int[] dist = new int[N + 1];
         Arrays.fill(dist, Integer.MAX_VALUE);
         dist[s] = 0;
