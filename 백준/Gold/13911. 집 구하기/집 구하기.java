510899fa54277acd7f39e05870ed803f4cc61ed2
유경헌 <92325898+yootal@users.noreply.github.com>
2025-08-08 13:46:29 +0900

    [Gold II] Title: 집 구하기, Time: 920 ms, Memory: 129904 KB -BaekjoonHub

--- 백준/Gold/13911. 집 구하기/집 구하기.java
+++ 백준/Gold/13911. 집 구하기/집 구하기.java
@@ -2,16 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
+    static int V, E;
     static ArrayList<int[]>[] graph;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
-        graph = new ArrayList[V + 3];
-        for (int i = 1; i <= V + 2; i++) {
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+        graph = new ArrayList[V + 1];
+        for (int i = 1; i <= V; i++) {
             graph[i] = new ArrayList<>();
         }
         for (int i = 0; i < E; i++) {
@@ -23,48 +23,46 @@ public class Main {
             graph[v].add(new int[]{u, w});
         }
         boolean[] check = new boolean[V + 1];
-        int mac = V + 1;
         st = new StringTokenizer(br.readLine());
         int M = Integer.parseInt(st.nextToken());
         int x = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
+        int[] mac = new int[M];
         for (int i = 0; i < M; i++) {
-            int m = Integer.parseInt(st.nextToken());
-            check[m] = true;
-            graph[mac].add(new int[]{m, 0});
+            mac[i] = Integer.parseInt(st.nextToken());
+            check[mac[i]] = true;
         }
-        int star = V + 2;
         st = new StringTokenizer(br.readLine());
         int S = Integer.parseInt(st.nextToken());
         int y = Integer.parseInt(st.nextToken());
+        int[] sta = new int[S];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < S; i++) {
-            int s = Integer.parseInt(st.nextToken());
-            check[s] = true;
-            graph[star].add(new int[]{s, 0});
+            sta[i] = Integer.parseInt(st.nextToken());
+            check[sta[i]] = true;
         }
-        int[] mDist = new int[V + 3];
-        Arrays.fill(mDist, Integer.MAX_VALUE);
-        dijkstra(mDist, mac);
-        int[] sDist = new int[V + 3];
-        Arrays.fill(sDist, Integer.MAX_VALUE);
-        dijkstra(sDist, star);
         int ans = Integer.MAX_VALUE;
+        int[] distMac = dijkstra(mac);
+        int[] distSta = dijkstra(sta);
         for (int i = 1; i <= V; i++) {
-            if (!check[i] && mDist[i] <= x && sDist[i] <= y) {
-                ans = Math.min(ans, mDist[i] + sDist[i]);
+            if (!check[i] && distMac[i] <= x && distSta[i] <= y) {
+                ans = Math.min(ans, distMac[i] + distSta[i]);
             }
         }
-        System.out.println(ans != Integer.MAX_VALUE ? ans : -1);
+        System.out.println(ans == Integer.MAX_VALUE ? -1 : ans);
     }
 
-    static void dijkstra(int[] dist, int start) {
-        dist[start] = 0;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(o -> o[1]));
-        pq.offer(new int[]{start, 0});
+    static int[] dijkstra(int[] start) {
+        int[] dist = new int[V + 1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(arr -> arr[1]));
+        for (int point : start) {
+            dist[point] = 0;
+            pq.offer(new int[]{point, dist[point]});
+        }
         while (!pq.isEmpty()) {
             int[] cur = pq.poll();
-            if (dist[cur[0]] != cur[1]) continue;
+            if (cur[1] != dist[cur[0]]) continue;
             for (int[] nxt : graph[cur[0]]) {
                 if (dist[nxt[0]] > cur[1] + nxt[1]) {
                     dist[nxt[0]] = cur[1] + nxt[1];
@@ -72,5 +70,6 @@ public class Main {
                 }
             }
         }
+        return dist;
     }
 }
