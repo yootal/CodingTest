e8ccee916ee1cc661ed42ead81cb9e7b0a7d676a
유경헌 <92325898+yootal@users.noreply.github.com>
2025-08-08 12:50:22 +0900

    [Gold III] Title: 파티, Time: 996 ms, Memory: 34568 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -1,33 +1,39 @@
-import sys
-from heapq import heappush, heappop
+from sys import stdin,maxsize
 from collections import defaultdict
-input = sys.stdin.readline
-inf = sys.maxsize
-
-def calc_dist(st,ed):
-    sdt = [inf] * (n+1)
-    sdt[st] = 0
-    q = []
-    heappush(q,(sdt[st],st)) # 거리, 시작점
-    while q:
-        dis,cur = heappop(q)
-        if sdt[cur] != dis:
-            continue
-        for d,nxt in graph[cur]:
-            if sdt[nxt] <= sdt[cur] + d:
-                continue
-            sdt[nxt] = sdt[cur] + d
-            heappush(q,(sdt[nxt],nxt))
-    return sdt[ed]
+from heapq import heappush, heappop
+input = stdin.readline
 
 n,m,x = map(int,input().split())
 graph = defaultdict(list)
 for _ in range(m):
-    u,v,w = map(int,input().split())
-    graph[u].append((w,v))
+    s,e,w = map(int,input().split())
+    graph[s].append((w,e))
     
-dist_list = []
-for i in range(1,n+1):
-    dist_list.append(calc_dist(i,x) + calc_dist(x,i))
-            
-print(max(dist_list))
+def dijkstra(st,en):
+    sdt = [maxsize] * (n+1)
+    sdt[st] = 0
+    hq = []
+    heappush(hq,(sdt[st],st))
+    while hq:
+        t1,cur = heappop(hq)
+        if sdt[cur] != t1:
+            continue
+        for t2,nxt in graph[cur]:
+            if sdt[nxt] > sdt[cur] + t2:
+                sdt[nxt] = sdt[cur] + t2
+                heappush(hq,(sdt[nxt],nxt))
+    if st == en:
+        return sdt
+    else:
+        return sdt[en]
+        
+round = [0] * (n+1)
+for st in range(1,n+1):
+    if st == x:
+        dists = dijkstra(st,x)
+        for st in range(1,n+1):
+            round[st] += dists[st]
+    else:
+        round[st] += dijkstra(st,x)
+        
+print(max(round))
