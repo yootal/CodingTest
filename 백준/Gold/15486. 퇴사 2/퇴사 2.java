c71a9c52b4f4c05b9b8e984ee45f0b246ff50c0a
유경헌 <92325898+yootal@users.noreply.github.com>
2025-08-09 15:34:53 +0900

    [Gold V] Title: 퇴사 2, Time: 712 ms, Memory: 330776 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,28 +1,42 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
+    static class Info {
+        int t, p;
 
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		int n = Integer.parseInt(br.readLine());
-		int advice[][] = new int[n + 1][2];
-		int dp[] = new int[n + 1];
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			advice[i][0] = Integer.parseInt(st.nextToken());
-			advice[i][1] = Integer.parseInt(st.nextToken());
-		}
-		for (int i = 0; i <= n; i++) {
-			int idx = i + advice[i][0];
-			if (i > 0)
-				dp[i] = Math.max(dp[i], dp[i - 1]);
-			if (idx <= n) {
-				dp[idx] = Math.max(dp[idx], dp[i] + advice[i][1]);
-			}
-		}
-		System.out.println(dp[n]);
-	}
+        public Info(int t, int p) {
+            this.t = t;
+            this.p = p;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        Info[] infos = new Info[N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int t = Integer.parseInt(st.nextToken());
+            int p = Integer.parseInt(st.nextToken());
+            infos[i] = new Info(t, p);
+        }
+        int[] dp = new int[N + 1];
+        int max = 0;
+        for (int i = 0; i < N; i++) {
+            if (max < dp[i]) {
+                max = dp[i];
+            }
+            Info info = infos[i];
+            int nxt = i + info.t;
+            if (nxt <= N)
+                dp[nxt] = Math.max(dp[nxt], max + info.p);
+        }
+        int ans = 0;
+        for (int i = 0; i <= N; i++) {
+            ans = Math.max(ans, dp[i]);
+        }
+        System.out.println(ans);
+    }
 }
