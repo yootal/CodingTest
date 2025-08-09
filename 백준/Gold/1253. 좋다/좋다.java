849789eedf5aaf7d557e97b15424e610d390e6f9
유경헌 <92325898+yootal@users.noreply.github.com>
2025-08-09 17:28:52 +0900

    [Gold IV] Title: 좋다, Time: 120 ms, Memory: 12760 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -1,41 +1,37 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
-//		System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int N = Integer.parseInt(br.readLine());
-		int[] num = new int[N];
-		StringTokenizer stt = new StringTokenizer(br.readLine());
-		for (int i = 0; i < N; i++) {
-			num[i] = Integer.parseInt(stt.nextToken());
-		}
-		Arrays.sort(num);
-		int ans = 0;
-		for (int i = 0; i < N; i++) {
-			int l = 0;
-			int r = N - 1;
-			while (l < r) {
-				if (num[l] + num[r] < num[i]) {
-					l++;
-				} else if (num[l] + num[r] > num[i]) {
-					r--;
-				} else {
-					if (l != i && r != i) {
-						ans++;
-						break;
-					}
-					else if(l == i) {
-						l++;
-					}
-					else if(r == i) {
-						r--;
-					}
-				}
-			}
-		}
-		System.out.println(ans);
-	}
 
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] num = new int[N];
+        for (int i = 0; i < N; i++) {
+            num[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(num);
+        int cnt = 0;
+        for (int i = 0; i < N; i++) {
+            int s = 0, e = N - 1;
+            while (s < e) {
+                if (num[s] + num[e] < num[i]) {
+                    s++;
+                } else if (num[s] + num[e] > num[i]) {
+                    e--;
+                } else {
+                    if (s != i && e != i) {
+                        cnt++;
+                        break;
+                    } else if (s == i) {
+                        s++;
+                    } else {
+                        e--;
+                    }
+                }
+            }
+        }
+        System.out.println(cnt);
+    }
 }
