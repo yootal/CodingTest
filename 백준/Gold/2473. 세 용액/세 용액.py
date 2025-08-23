ae1d23dd129142e76931f505f9d41b88f5516caf
유경헌 <92325898+yootal@users.noreply.github.com>
2025-08-23 09:47:32 +0900

    [Gold III] Title: 세 용액, Time: 4660 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,37 +1,27 @@
 import sys
 input = sys.stdin.readline
-
+    
 n = int(input())
-solutions = list(map(int,input().split()))
-solutions.sort()
+sol = list(map(int,input().split()))
+sol.sort()
 
 ans = sys.maxsize
-ans_base = 0
-ans_st = 0
-ans_en = 0
+a1,a2,a3 = 0,0,0
 
 for i in range(n-2):
-    for j in range(i+1,n-1):
-        base = solutions[i]
-        current = solutions[j]
-        st = j + 1
-        en = n 
+    st = i + 1
+    en = n - 1
+    while st < en:
+        s = sol[i] + sol[st] + sol[en]
+        if ans > abs(s):
+            a1 = sol[i]
+            a2 = sol[st]
+            a3 = sol[en]
+            ans = abs(s)
         
-        while st < en:
-            mid = (st + en) // 2
-            check = base + current + solutions[mid]
+        if s < 0:
+            st += 1
+        else:
+            en -= 1
             
-            if abs(check) < ans:
-                ans = abs(check)
-                ans_base = i
-                ans_left = j
-                ans_right = mid
-                if check == 0:
-                    break
-                
-            if check < 0:
-                st = mid + 1
-            else:
-                en = mid
-
-print(solutions[ans_base], solutions[ans_left], solutions[ans_right])
+print(a1,a2,a3)
