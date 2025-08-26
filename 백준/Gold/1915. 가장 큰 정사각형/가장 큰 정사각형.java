e48ada405a2a4fce120b6f8e609aad024472182e
유경헌 <92325898+yootal@users.noreply.github.com>
2025-08-26 13:09:25 +0900

    [Gold IV] Title: 가장 큰 정사각형, Time: 200 ms, Memory: 21924 KB -BaekjoonHub

--- 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
+++ 백준/Gold/1915. 가장 큰 정사각형/가장 큰 정사각형.java
@@ -3,24 +3,23 @@ import java.util.*;
 
 public class Main {
 
-	public static void main(String[] args) throws Exception {
-		//System.setIn(new FileInputStream("res/input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int n = Integer.parseInt(st.nextToken());
-		int m = Integer.parseInt(st.nextToken());
-		int dp[][] = new int[n][m];
-		int ans = 0;
-		for (int i = 0; i < n; i++) {
-			String row = br.readLine();
-			for (int j = 0; j < m; j++) {
-				dp[i][j] = (int) row.charAt(j) - '0';
-				if (dp[i][j] == 1 && j > 0 && i > 0) {
-					dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + dp[i][j];
-				}
-				ans = Math.max(ans, dp[i][j]);
-			}
-		}
-		System.out.println((int) Math.pow(ans, 2));
-	}
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n][m];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            String row = br.readLine();
+            for (int j = 0; j < m; j++) {
+                dp[i][j] = (int) row.charAt(j) - '0';
+                if (dp[i][j] == 1 && i > 0 && j > 0) {
+                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1;
+                }
+                ans = Math.max(ans, dp[i][j]);
+            }
+        }
+        System.out.println((int) Math.pow(ans, 2));
+    }
 }
