04929c53462881fbe586e91181988362a4e50898
유경헌 <92325898+yootal@users.noreply.github.com>
2025-10-20 23:08:22 +0900

    [level 4] Title: 무지의 먹방 라이브, Time: 291.57 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/4/42891. 무지의 먹방 라이브/무지의 먹방 라이브.java
@@ -0,0 +1,64 @@
+import java.util.*;
+
+class Solution {
+    static int[] tf;
+    public int solution(int[] food_times, long k) {
+        int answer = -1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1])); 
+        int len = food_times.length;
+        tf = new int[len];
+        for(int i = 0 ; i < len ; i++){
+            tf[i] = food_times[i];
+        }
+        Arrays.sort(tf);
+        long sum = 0;
+        for(int i = 0 ; i < food_times.length ; i++){
+            int cur = food_times[i];
+            sum += cur;
+            pq.offer(new int[]{i,cur});
+        }
+        if(sum <= k) return -1;
+        int cycle = 0;
+        while(!pq.isEmpty() && k - (long) len * (pq.peek()[1] - cycle) > 0){
+            int out = pq.peek()[1];
+            while(!pq.isEmpty() && pq.peek()[1] == out){
+                int[] temp = pq.poll();
+                food_times[temp[0]] = 0;
+            }
+            int cnt = count(out);
+            k -= (long) len * (out - cycle);
+            len -= cnt;
+            cycle += (out - cycle);
+        }
+        long pos = k % len;
+        for (int i = 0; i < food_times.length; i++) {
+            if (food_times[i] == 0) continue;
+            if (pos == 0) return i + 1;
+            pos--;
+        }
+        for(int i = 0 ; i < food_times.length ; i++){
+            if(food_times[i] == 0) continue;
+            k--;
+            if(k == -1) answer = i + 1;
+        }   
+        return answer;
+    }
+    
+    static int count(int x){
+        int lo = -1, hi = tf.length;
+        while(lo + 1 < hi){
+            int mid = (lo + hi) / 2;
+            if(tf[mid] <= x) lo = mid;
+            else hi = mid;
+        }
+        int upper = hi;
+        lo = -1; hi = tf.length;
+        while(lo + 1 < hi){
+            int mid = (lo + hi) / 2;
+            if(tf[mid] < x) lo = mid;
+            else hi = mid;
+        }
+        int lower = hi;
+        return upper - lower;
+    }
+}
